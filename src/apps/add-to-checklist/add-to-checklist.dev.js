33ab8762b35bd8140a7daf48c8e5eeb10b226e4f
Lasse Borly <lasseborly@gmail.com>
2019-11-23 02:20:23 +0100

    Let's remove unuused files for now.

--- src/apps/add-to-checklist/add-to-checklist.dev.js
+++ src/apps/add-to-checklist/add-to-checklist.dev.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { withKnobs, text } from "@storybook/addon-knobs";
 import AddToChecklist from "./add-to-checklist.entry.js";
-import "./add-to-checklist.scss";
 
 export default {
   title: "Apps|Add to Checklist",
--- src/apps/add-to-checklist/add-to-checklist.test.js
+++
@@ -1,9 +0,0 @@
-describe("Add to checklist", function() {
-  it("successfully loads", function() {
-    cy.visit("/");
-    cy.get("#explorerapps-add-to-checklist--entry").click();
-    cy.get("#storybook-preview-iframe")
-      .iframe()
-      .find("Tilføj til min huskeliste");
-  });
-});
