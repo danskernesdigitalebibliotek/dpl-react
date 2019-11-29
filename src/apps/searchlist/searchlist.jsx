ba2c896e04afa5e86bb09c539ecb8a30e9923b3a
Lasse Borly <lasseborly@gmail.com>
2019-11-29 13:25:25 +0100

    Push corrections

--- src/apps/searchlist/searchlist.jsx
+++ src/apps/searchlist/searchlist.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Searchlist ({ items }) {
+function Searchlist({ items }) {
   return (
     <ul className="list">
       {items.map(item => (
