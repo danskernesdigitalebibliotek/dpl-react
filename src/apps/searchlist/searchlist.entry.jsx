769897ec6fd6ae7a47d6fb29b75aaead55ae9485
Lasse Borly <lasseborly@gmail.com>
2019-12-11 20:34:59 +0100

    Initial layout and link func.
    We can now link to a previous search and the conditional layout is in order.
    
    We are still missing remove and see new materials.

--- src/apps/searchlist/searchlist.entry.jsx
+++ src/apps/searchlist/searchlist.entry.jsx
@@ -1,22 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 import Searchlist from "./searchlist";
 import FollowSearches from "../../core/FollowSearches";
 
 const client = new FollowSearches();
 
-function SearchlistEntry() {
+function SearchlistEntry({
+  newButtonText,
+  removeButtonText,
+  statusText,
+  searchUrl
+}) {
+  const [searches, setSearches] = useState([]);
+  const [loading, setLoading] = useState("inactive");
   useEffect(function getSearches() {
+    setLoading("active");
     client
       .getSearches()
       .then(function onSuccess(result) {
-        console.log(result);
+        setSearches(result);
+        setLoading("finished");
       })
-      .catch(function onError(err) {
-        console.error(err);
+      .catch(function onError() {
+        setLoading("failed");
       });
   }, []);
-  return <Searchlist />;
+  return (
+    <Searchlist
+      loading={loading}
+      searches={searches}
+      newButtonText={newButtonText}
+      removeButtonText={removeButtonText}
+      statusText={statusText}
+      searchUrl={searchUrl}
+    />
+  );
 }
+
+SearchlistEntry.propTypes = {
+  newButtonText: PropTypes.string,
+  removeButtonText: PropTypes.string,
+  statusText: PropTypes.string,
+  searchUrl: PropTypes.string
+};
+
+SearchlistEntry.defaultProps = {
+  newButtonText: "Nye materialer",
+  removeButtonText: "Fjern fra listen",
+  statusText: ":hit_count nye materialer siden den",
+  searchUrl: "https://lollandbib.dk/search/ting/:query"
+};
 
 export default SearchlistEntry;
--- src/apps/searchlist/searchlist.jsx
+++ src/apps/searchlist/searchlist.jsx
@@ -1,36 +1,130 @@
 import React from "react";
 import PropTypes from "prop-types";
+import dayjs from "dayjs";
+import ListItem from "../../components/list-item/list-item";
+import Skeleton, { getList } from "../../components/atoms/skeleton/skeleton";
+import UnorderedList from "../../components/atoms/list/list";
+import Button from "../../components/atoms/button/button";
 
-function Searchlist({ items }) {
+/**
+ * Provided a string with tags noted inside of it with ":" in front
+ * of the keyname we replace said tags with it's value.
+ *
+ * @param {object} options
+ * @param {string} options.text
+ * @param {object} options.tag corresponsing keys to what's assigned in the text must be present here.
+ * @returns {string} text with the values of the tags inserted.
+ */
+function replaceTags({ text = "", tags = {} } = {}) {
+  return Object.keys(tags).reduce(function replaceTag(acc, tag) {
+    return acc.replace(`:${tag}`, tags[tag]);
+  }, text);
+}
+
+function SkeletonElement(_, index) {
   return (
-    <ul className="list">
-      {items.map(item => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <ListItem
+      key={index}
+      aside={
+        <>
+          <Skeleton width="130px" height="50px" />
+          <Skeleton width="130px" height="50px" />
+        </>
+      }
+    >
+      <div className="ddb-searchlist__container">
+        <Skeleton height="30px" width="95px" mb="12px" />
+        <Skeleton width="60px" mb="12px" />
+        <Skeleton width="145px" />
+      </div>
+    </ListItem>
   );
 }
 
-Searchlist.defaultProps = {
-  items: [
-    {
-      id: 1,
-      text: "Harry Potter"
-    },
-    {
-      id: 2,
-      text: "Tolkien"
-    }
-  ]
-};
+function Searchlist({
+  searches,
+  loading,
+  newButtonText,
+  removeButtonText,
+  searchUrl,
+  statusText
+}) {
+  if (loading === "active") {
+    return <UnorderedList>{getList(8).map(SkeletonElement)}</UnorderedList>;
+  }
+
+  return (
+    <UnorderedList>
+      {searches.map(search => (
+        <ListItem
+          key={search.id}
+          childrenClass="ddb-searchlist__container"
+          aside={
+            <>
+              {search.hit_count > 0 && (
+                <Button
+                  className="ddb-searchlist__new-button"
+                  variant="charcoal"
+                >
+                  {newButtonText}
+                </Button>
+              )}
+              <Button>{removeButtonText}</Button>
+            </>
+          }
+        >
+          <h2 className="ddb-searchlist__header">
+            <a
+              href={replaceTags({
+                text: searchUrl,
+                tags: {
+                  query: search.query
+                }
+              })}
+            >
+              {search.title}
+            </a>
+          </h2>
+          <p className="ddb-searchlist__query">{search.query}</p>
+          {search.hit_count > 0 && (
+            <p className="ddb-searchlist__status">
+              {replaceTags({
+                text: statusText,
+                tags: {
+                  hit_count: search.hit_count
+                }
+              })}{" "}
+              <time dateTime={search.last_seen}>
+                {dayjs(search.last_seen).format("DD/MM-YYYY")}
+              </time>
+              .
+            </p>
+          )}
+        </ListItem>
+      ))}
+    </UnorderedList>
+  );
+}
 
 Searchlist.propTypes = {
-  items: PropTypes.arrayOf(
+  loading: PropTypes.oneOf(["inactive", "active", "finished", "failed"]),
+  statusText: PropTypes.string.isRequired,
+  newButtonText: PropTypes.string.isRequired,
+  removeButtonText: PropTypes.string.isRequired,
+  searchUrl: PropTypes.string.isRequired,
+  searches: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number,
-      text: PropTypes.string
+      id: PropTypes.number.isRequired,
+      hit_count: PropTypes.number.isRequired,
+      last_seen: PropTypes.string.isRequired,
+      query: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired
     })
-  )
+  ).isRequired
+};
+
+Searchlist.defaultProps = {
+  loading: "inactive"
 };
 
 export default Searchlist;
