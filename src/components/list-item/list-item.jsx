c7d280f0d0996233be369128d7b92a3248a4a359
Lasse Borly <lasseborly@gmail.com>
2019-12-13 11:18:05 +0100

    Extend list-item.
    We need to access almost all of the classNames on the list-item.

--- src/components/list-item/list-item.jsx
+++ src/components/list-item/list-item.jsx
@@ -1,29 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function ListItem({ children, childrenClass, aside, asideClass }) {
+function ListItem({
+  className,
+  children,
+  childrenClass,
+  containerClass,
+  aside,
+  asideClass,
+  footer,
+  footerClass
+}) {
   return (
-    <li className="ddb-list-item">
-      <section className="ddb-list-item__container">
+    <li className={`ddb-list-item ${className}`}>
+      <section className={`ddb-list-item__container ${containerClass}`}>
         <article className={`ddb-list-item__children ${childrenClass}`}>
           {children}
         </article>
         <aside className={`ddb-list-item__aside ${asideClass}`}>{aside}</aside>
       </section>
+      {footer && (
+        <section className={`ddb-list-item__footer ${footerClass}`}>
+          {footer}
+        </section>
+      )}
     </li>
   );
 }
 
 ListItem.propTypes = {
+  className: PropTypes.string,
+  containerClass: PropTypes.string,
   children: PropTypes.node.isRequired,
   childrenClass: PropTypes.string,
   aside: PropTypes.node.isRequired,
-  asideClass: PropTypes.string
+  asideClass: PropTypes.string,
+  footer: PropTypes.node,
+  footerClass: PropTypes.string
 };
 
 ListItem.defaultProps = {
+  className: "",
+  containerClass: "",
   childrenClass: "",
-  asideClass: ""
+  asideClass: "",
+  footer: undefined,
+  footerClass: ""
 };
 
 export default ListItem;
