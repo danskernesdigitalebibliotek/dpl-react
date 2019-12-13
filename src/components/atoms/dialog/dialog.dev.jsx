53cfdf602151c9f7a3fd39f24b0490d94aae7f4c
Lasse Borly <lasseborly@gmail.com>
2019-12-13 13:49:38 +0100

    Add dropdown storybook dialog.

--- src/components/atoms/dialog/dialog.dev.jsx
+++ src/components/atoms/dialog/dialog.dev.jsx
@@ -13,10 +13,28 @@ export function Simple() {
     <div>
       <Button onClick={open}>Open Dialog</Button>
       <Dialog isOpen={showDialog} onDismiss={close}>
+        <p>Hello there. I am a dialog</p>
         <Button className="close-button" onClick={close}>
           Close
         </Button>
+      </Dialog>
+    </div>
+  );
+}
+
+export function Dropdown() {
+  const [showDialog, setShowDialog] = useState(false);
+  const open = () => setShowDialog(true);
+  const close = () => setShowDialog(false);
+
+  return (
+    <div>
+      <Button onClick={open}>Open Dialog</Button>
+      <Dialog dropDown isOpen={showDialog} onDismiss={close}>
         <p>Hello there. I am a dialog</p>
+        <Button className="close-button" onClick={close}>
+          Close
+        </Button>
       </Dialog>
     </div>
   );
