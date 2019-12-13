d9bdbfda74de99488d44dedbbb2dcd064ad19fec
Lasse Borly <lasseborly@gmail.com>
2019-12-13 11:15:53 +0100

    Extract a simple material view.
    We need to show a material on both the searchlist and the checklist.

---
+++ src/components/simple-material/simple-material.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import PropTypes from "prop-types";
+import urlPropType from "url-prop-type";
+import replaceTags from "../../core/replaceTags";
+
+function SimpleMaterial({
+  item,
+  materialUrl,
+  authorUrl,
+  ofText,
+  className,
+  dataClass,
+  style
+}) {
+  return (
+    <section className={`ddb-simple-material ${className}`} style={style}>
+      <figure className="ddb-simple-material__cover">
+        <a
+          href={replaceTags({
+            text: materialUrl,
+            tags: {
+              pid: item.pid
+            }
+          })}
+        >
+          <img src={item.coverUrl} alt={item.title} />
+        </a>
+      </figure>
+      <div className={`ddb-simple-material__data ${dataClass}`}>
+        {item.type}
+        <a
+          href={replaceTags({
+            text: materialUrl,
+            tags: {
+              pid: item.pid
+            }
+          })}
+        >
+          <h2>{item.title}</h2>
+        </a>
+        <p className="ddb-simple-material__author-year">
+          {`${ofText} `}
+          {item.creators.map((creator, index) => {
+            return (
+              <span key={creator}>
+                <a
+                  href={replaceTags({
+                    text: authorUrl,
+                    tags: {
+                      author: creator
+                    }
+                  })}
+                >
+                  {creator}
+                </a>
+                {item.creators[index + 1] ? ", " : " "}
+              </span>
+            );
+          })}
+          ({item.year})
+        </p>
+      </div>
+    </section>
+  );
+}
+
+SimpleMaterial.propTypes = {
+  ofText: PropTypes.string,
+  className: PropTypes.string,
+  dataClass: PropTypes.string,
+  materialUrl: urlPropType.isRequired,
+  authorUrl: urlPropType.isRequired,
+  item: PropTypes.shape({
+    pid: PropTypes.string.isRequired,
+    creators: PropTypes.arrayOf(PropTypes.string),
+    title: PropTypes.string,
+    type: PropTypes.string,
+    year: PropTypes.string,
+    coverUrl: urlPropType
+  }).isRequired,
+  style: PropTypes.objectOf(PropTypes.any)
+};
+
+SimpleMaterial.defaultProps = {
+  ofText: "Af",
+  className: "",
+  dataClass: "",
+  style: {}
+};
+
+export default SimpleMaterial;
