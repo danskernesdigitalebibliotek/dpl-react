f264531593c3ca341a21f8f5694e3869f5a5df7a
Lasse Borly <lasseborly@gmail.com>
2019-12-13 13:44:45 +0100

    Correct button centering

--- src/components/atoms/button/button.dev.jsx
+++ src/components/atoms/button/button.dev.jsx
@@ -3,13 +3,30 @@ import Button from "./button";
 
 export default { title: "Atoms|Button" };
 
-export function WithText() {
+export function LeftAligned() {
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Button align="left">Grey</Button>
+      <Button align="left" variant="black">
+        Black
+      </Button>
+      <Button align="left" variant="secondary">
+        Secondary
+      </Button>
+      <Button align="left" variant="charcoal">
+        Charcoal
+      </Button>
+    </div>
+  );
+}
+
+export function CenterAligned() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <Button>Grey</Button>
       <Button variant="black">Black</Button>
       <Button variant="secondary">Secondary</Button>
       <Button variant="charcoal">Charcoal</Button>
-    </>
+    </div>
   );
 }
