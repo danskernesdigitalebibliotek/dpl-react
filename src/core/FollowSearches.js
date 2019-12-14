2376252ccb27236f27eecff1b08fc6da31493867
Lasse Borly <lasseborly@gmail.com>
2019-12-15 00:00:49 +0100

    Add fields to FollowSearches.
    This adds fields to getResultsForSearch.

--- src/core/FollowSearches.js
+++ src/core/FollowSearches.js
@@ -90,21 +90,29 @@ class FollowSearches {
    * @param {object} options
    * @param {string} options.listName
    * @param {number} options.searchId
+   * @param {string[]} options.fields https://raw.githubusercontent.com/DBCDK/serviceprovider/master/doc/work-context.jsonld
    * @returns {Promise<Material[]>} materials pids of the search.
    * @memberof FollowSearches
    */
-  async getResultsForSearch({ listName = "default", searchId } = {}) {
+  async getResultsForSearch({
+    listName = "default",
+    searchId,
+    fields = ["pid", "title"]
+  } = {}) {
     if (!searchId) {
       throw Error("searchId must be provided");
     }
-
-    const raw = await fetch(`${this.baseUrl}/list/${listName}/${searchId}`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${this.token}`
+    const formattedFields = fields.map(encodeURIComponent).join(",");
+    const raw = await fetch(
+      `${this.baseUrl}/list/${listName}/${searchId}?fields=${formattedFields}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${this.token}`
+        }
       }
-    });
+    );
 
     const response = await raw.json();
     return response.materials;
