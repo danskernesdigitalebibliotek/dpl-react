807cfabd58c94f496720ed752b6522690dd8e55d
Thomas Fini Hansen <xen@xen.dk>
2020-09-18 13:23:52 +0200

    Simplify hasToken

--- src/core/token.js
+++ src/core/token.js
@@ -31,7 +31,7 @@ export function setToken(type, value) {
  * @returns {boolean}
  */
 export function hasToken(type) {
-  return type in tokens && tokens[type];
+  return Boolean(tokens[type]);
 }
 
 /**
