f4735437d48d304570ae2f199de23ea5be1bff07
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-03-24 16:01:52 +0100

    Upgrading storybook to version 6.4.9 Had to change webpack rules in order to prevent unwnted eslint warnings

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,13 +5,32 @@ const customWebpack = require("../webpack.config.js");
 // https://storybook.js.org/docs/configurations/custom-webpack-config/#full-control-mode
 module.exports = async ({ config }) => {
   const custom = customWebpack(undefined, { mode: "development" });
+
+  // Remove first rule since we don't need that.
+  const [excludeRule, ...globalRules] = custom.module.rules;
+
   const rules = [
-    ...custom.module.rules,
+    ...globalRules,
     // We need to make use of css modules in our stories.
     {
       test: /\.scss$/,
       use: ["style-loader", "postcss-loader"],
       include: path.resolve(__dirname, "../")
+    },
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"],
+      include: path.resolve(__dirname, "../")
+    },
+    // For some reason we need to split up babel-loader and eslint-loader
+    // in order to get rid of eslint warnings like
+    // [these](https://github.com/storybookjs/storybook/issues/10878).
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ["eslint-loader"],
+      include: path.resolve(__dirname, "../src")
     }
   ];
   const plugins = [...config.plugins];
