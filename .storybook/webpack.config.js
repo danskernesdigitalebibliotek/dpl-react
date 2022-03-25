11f8f60c2fda49841f3daeafe44750e022b9108f
Kasper Garnaes <kasper.garnaes@gmail.com>
2022-03-25 14:06:01 +0100

    Move modifications to webpack rules to root config
    This way we do not have to remove and readd rules which are already
    configured.
    
    The configuration means that we will only run eslint on files in the
    src folder. We do not lint JS files in other subfolders. That is
    unfortunate but there does not seem to be a away to only avoid the
    linting files generated by Storybook >6.0.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,31 +6,13 @@ const customWebpack = require("../webpack.config.js");
 module.exports = async ({ config }) => {
   const custom = customWebpack(undefined, { mode: "development" });
 
-  // Remove first rule since we don't need that.
-  const [excludeRule, ...globalRules] = custom.module.rules;
-
   const rules = [
-    ...globalRules,
+    ...custom.module.rules,
     // We need to make use of css modules in our stories.
     {
       test: /\.scss$/,
       use: ["style-loader", "postcss-loader"],
       include: path.resolve(__dirname, "../")
-    },
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: ["babel-loader"],
-      include: path.resolve(__dirname, "../")
-    },
-    // For some reason we need to split up babel-loader and eslint-loader
-    // in order to get rid of eslint warnings like
-    // [these](https://github.com/storybookjs/storybook/issues/10878).
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: ["eslint-loader"],
-      include: path.resolve(__dirname, "../src")
     }
   ];
   const plugins = [...config.plugins];
