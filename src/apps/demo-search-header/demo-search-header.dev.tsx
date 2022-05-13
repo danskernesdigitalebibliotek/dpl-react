b8dd9bfcb80f63c1944bb578ee02ea29fae56fd0
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-05-13 12:36:44 +0200

    Library token component and story added
    Both the component and the story are meant
    only to be used in storybook context.

--- src/apps/demo-search-header/demo-search-header.dev.tsx
+++ src/apps/demo-search-header/demo-search-header.dev.tsx
@@ -1,7 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
-import WarningIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/basic/icon-warning.svg";
-import { setToken, TOKEN_LIBRARY_KEY } from "../../core/token";
 import DemoSearchHeader, {
   DemoSearchHeaderEntryProps
 } from "./demo-search-header.entry";
