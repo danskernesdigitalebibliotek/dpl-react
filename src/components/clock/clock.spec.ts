69fa7c6adbcea868d1f6c34975cccb5c748a6b09
Sine Jespersen <sinejespersen@gmail.com>
2022-05-17 14:22:06 +0200

    rename tests

--- src/components/clock/clock.spec.ts
+++ src/components/clock/clock.spec.ts
@@ -1,4 +1,4 @@
-describe("Hello World", () => {
+describe("Clock tests", () => {
   it("Renders clock eleven thirty", () => {
     const ElevenThirty = new Date(
       "Mon May 16 2022 11:30:00 GMT+0200 (Central European Summer Time)"
@@ -14,7 +14,9 @@ describe("Hello World", () => {
         "transform",
         "matrix(6.12323e-17, 1, -1, 6.12323e-17, 0, 0)"
       );
+
     cy.get(".capitalize").should("contain", "mandag, maj 16");
+
     cy.get(".needle")
       .last()
       .should(
@@ -23,6 +25,7 @@ describe("Hello World", () => {
         "matrix(-0.258819, -0.965926, 0.965926, -0.258819, 0, 0)"
       );
   });
+
   it("Renders clock fifteen fifteen", () => {
     const FifteenFifteen = new Date(
       "Tue May 17 2022 15:15:00 GMT+0200 (Central European Summer Time)"
@@ -31,9 +34,11 @@ describe("Hello World", () => {
     cy.visit("/iframe.html?path=/story/components-clock--clock-now");
 
     cy.get(".capitalize").should("contain", "tirsdag, maj 17");
+
     cy.get(".needle")
       .first()
       .should("have.css", "transform", "matrix(1, 0, 0, 1, 0, 0)");
+
     cy.get(".needle")
       .last()
       .should(
