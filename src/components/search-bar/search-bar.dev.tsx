f922ef82c843b93227776ea61c6ebf30a4310e60
Adam Antal <adam@reload.dk>
2022-05-20 14:05:06 +0200

    Move the storyless header component down from app level and document it
    The header component ia refered to from the search bar component. This
    meant refering to something from a component level upwards to app level,
    and was thus moved down from the app level into the search bar component
    folder, where it is called.

--- src/components/search-bar/search-bar.dev.tsx
+++ src/components/search-bar/search-bar.dev.tsx
@@ -1,6 +1,6 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React, { useState } from "react";
-import Header from "../../apps/search-header/header";
+import Header from "./header";
 import SearchBar, { SearchBarProps } from "./search-bar";
 
 export default {
