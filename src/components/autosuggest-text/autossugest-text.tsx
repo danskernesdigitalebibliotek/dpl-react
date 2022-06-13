5b7cdd0a74e2e70b26c42d2c55e9cd6a5e340b74
Adam Antal <adam@reload.dk>
2022-06-13 09:54:13 +0200

    Introduce the autosuggest-text subcomponent
    This component is going to be set into the main autosuggest component as
    one of the three parts that make it up. It shows a list of item that are
    strings. As we use Downshift to tackle the autosuggest, this component
    utilizes some of downshift props to manage overall autosuggest state.

--- src/components/autosuggest-text/autossugest-text.tsx
+++ src/components/autosuggest-text/autossugest-text.tsx
@@ -3,23 +3,58 @@ import { SuggestionsFromQueryStringQuery } from "../../core/dbc-gateway/generate
 
 export interface AutosuggestTextProps {
   responseData: SuggestionsFromQueryStringQuery["suggest"]["result"];
-  currentQ: string;
+  currentQ: string | undefined;
+  // TODO: find out what type this can be from downshifts official types
+  highlightedIndex: any;
+  getItemProps: any;
 }
 
 export const AutosuggestText: React.FC<AutosuggestTextProps> = ({
-  responseData
+  responseData,
+  highlightedIndex,
+  getItemProps
 }) => {
+  // now we need to make a string item for downshift
+  // @ts-expect-error TODO: objectItem needs a type
+  function itemToString(objectItem) {
+    switch (objectItem.__typename) {
+      case "Creator":
+        return objectItem.name;
+      case "Subject":
+        return objectItem.value;
+      default:
+        return objectItem.title;
+    }
+  }
+
   return (
     <>
-      {responseData.map((item) => {
+      {responseData.map((item, index) => {
+        /* eslint-disable react/no-array-index-key */
+        // TODO: find a way to index the <li> without using index
         return (
-          <li className="autosuggest-text-container__item text-body-medium-regular">
-            {item.__typename === "Creator" ? `${item.name} (forfatter)` : null}
-            {item.__typename === "Subject" ? `${item.value} (emne)` : null}
-            {item.__typename === "Work"
-              ? `${item.title} (manifestation)`
-              : null}
-          </li>
+          <>
+            {/* eslint-disable react/jsx-props-no-spreading */}
+            {/* console.log({ ...getItemProps({ item, index }) }) */}
+            <li
+              className="autosuggest-text-container__item text-body-medium-regular"
+              key={index}
+              style={
+                highlightedIndex === index ? { backgroundColor: "#EEE9E5" } : {}
+              }
+              {...getItemProps({ item: itemToString(item), index })}
+            >
+              {/* eslint-enable react/jsx-props-no-spreading */}
+              {/* eslint-enable react/no-array-index-key */}
+              {item.__typename === "Creator"
+                ? `${item.name} (forfatter)`
+                : null}
+              {item.__typename === "Subject" ? `${item.value} (emne)` : null}
+              {item.__typename === "Work"
+                ? `${item.title} (manifestation)`
+                : null}
+            </li>
+          </>
         );
       })}
     </>
