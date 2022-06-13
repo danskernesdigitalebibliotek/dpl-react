2c3e1c0a54ee9f717838fa0193d28b622acc1970
Adam Antal <adam@reload.dk>
2022-06-13 10:00:56 +0200

    Insert Autosuggest-text into the Autosuggest component
    The autosuggest component was updated with all the Downshift props
    necessary, and the story was adjusted so that it can also stand on its
    own - without being set in the hsearch-header.
    As of currently, we still need to address some eslint errors and TS
    types for Downshift props.

--- src/components/autosuggest/autosuggest.dev.tsx
+++ src/components/autosuggest/autosuggest.dev.tsx
@@ -10,6 +10,60 @@ export default {
       name: "Query",
       defaultValue: "Adam",
       control: { type: "text" }
+    },
+    data: {
+      name: "Data",
+      defaultValue: {
+        suggest: {
+          result: [
+            {
+              __typename: "Creator",
+              name: "Adam August"
+            },
+            {
+              __typename: "Creator",
+              name: "Douglas Adams"
+            },
+            {
+              __typename: "Creator",
+              name: "Adam Neutzsky-Wulff"
+            },
+            {
+              __typename: "Creator",
+              name: "Andrew Adamson"
+            },
+            {
+              __typename: "Creator",
+              name: "Adam Wallensten"
+            }
+          ]
+        }
+      },
+      control: { type: "object" }
+    },
+    isLoading: {
+      name: "Is Loading",
+      defaultValue: false,
+      control: { type: "boolean" }
+    },
+    status: {
+      name: "Status of the transaction",
+      defaultValue: "success",
+      control: { type: "text" }
+    },
+    isOpen: {
+      name: "Is dropdown open",
+      defaultValue: true,
+      control: { type: "boolean" }
+    },
+    getMenuProps: {
+      control: { type: null }
+    },
+    getItemProps: {
+      control: { type: null }
+    },
+    highlightedIndex: {
+      control: { type: null }
     }
   }
 } as ComponentMeta<typeof Autosuggest>;
@@ -17,6 +71,14 @@ export default {
 export const Default: ComponentStory<typeof Autosuggest> = (
   args: AutosuggestProps
 ) => {
+  // The constants below are given to the story in order for it to now throw
+  // errors. Downshift dropdown menu and menu items require we pass these down
+  const getMenuProps = () => {
+    return null;
+  };
+  const getItemProps = () => {
+    return null;
+  };
   return (
     // The markup below is used to give the autosuggest contextual elements
     // that its design depends on (so that the story looks presentable).
@@ -41,7 +103,11 @@ export const Default: ComponentStory<typeof Autosuggest> = (
         placeholder="I am here just for context."
         className="header__menu-search-input text-body-medium-regular"
       />
-      <Autosuggest {...args} />
+      <Autosuggest
+        {...args}
+        getMenuProps={getMenuProps}
+        getItemProps={getItemProps}
+      />
     </div>
   );
 };
--- src/components/autosuggest/autosuggest.tsx
+++ src/components/autosuggest/autosuggest.tsx
@@ -1,43 +1,53 @@
 import React from "react";
-import {
-  SuggestionsFromQueryStringQuery,
-  useSuggestionsFromQueryStringQuery
-} from "../../core/dbc-gateway/generated/graphql";
 import { AutosuggestText } from "../autosuggest-text/autossugest-text";
 
 export interface AutosuggestProps {
-  q: string;
+  q: string | undefined;
+  data: any | undefined;
+  isLoading: boolean;
+  status: string;
+  // TODO: find out what type this can be from downshifts official types
+  getMenuProps: any;
+  highlightedIndex: any;
+  getItemProps: any;
+  isOpen: any;
 }
 
 export const Autosuggest: React.FC<AutosuggestProps> = ({
-  q = "placeholder"
+  q = "query",
+  data,
+  isLoading,
+  status,
+  getMenuProps,
+  highlightedIndex,
+  getItemProps,
+  isOpen
 }) => {
-  const {
-    data,
-    isLoading,
-    status
-  }: {
-    data: SuggestionsFromQueryStringQuery | undefined;
-    isLoading: boolean;
-    status: string;
-  } = useSuggestionsFromQueryStringQuery({ q });
+  return (
+    <>
+      {/* console.log({ ...getMenuProps() }) */}
+      {/* eslint-disable react/jsx-props-no-spreading */}
+      {/* The downshift combobox works this way by design */}
+      <ul
+        className="autosuggest-text-container"
+        {...getMenuProps()}
+        style={!isOpen ? { display: "none" } : {}}
+      >
+        {/* eslint-enable react/jsx-props-no-spreading */}
 
-  if (isLoading) {
-    return <ul className="autosuggest-text-container">Loading</ul>;
-  }
+        {isLoading && <div>Loading</div>}
 
-  // if the service cannot find any suggestions
-  if (data && data.suggest.result.length < 1) {
-    return <ul className="autosuggest-text-container">Ingen suggestions</ul>;
-  }
+        {data && data.suggest.result.length < 1 && null}
 
-  if (data && status === "success") {
-    return (
-      <ul className="autosuggest-text-container">
-        <AutosuggestText responseData={data.suggest.result} currentQ={q} />
+        {data && status === "success" && isOpen && (
+          <AutosuggestText
+            responseData={data.suggest.result}
+            currentQ={q}
+            highlightedIndex={highlightedIndex}
+            getItemProps={getItemProps}
+          />
+        )}
       </ul>
-    );
-  }
-
-  return null;
+    </>
+  );
 };
