d3d843a94b566fb4199932b560ce64a30989fbef
Kasper Birch <kasperbirch1@gmail.com>
2022-06-15 14:13:47 +0200

    Refactories orval code generator
    - New file structure
    - handling of user token
    - script for orval codegen

---
+++ src/core/material-list-api/model/index.ts
@@ -0,0 +1,6 @@
+export * from "./listId";
+export * from "./itemId";
+export * from "./legacyUserId";
+export * from "./list";
+export * from "./collectionIdsParameter";
+export * from "./getListParams";
