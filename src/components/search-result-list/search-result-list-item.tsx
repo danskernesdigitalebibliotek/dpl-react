fd952652563a85a115e69ac9944ac2beb02c87b8
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-06-15 18:03:02 +0200

    Add basic rendering of search result list and items
    with the proper classes

--- src/components/search-result-list/search-result-list-item.tsx
+++ src/components/search-result-list/search-result-list-item.tsx
@@ -1,13 +1,47 @@
 import React from "react";
+import { SearchWithPaginationQuery } from "../../core/dbc-gateway/generated/graphql";
 
 export interface SearchResultListItemProps {
-  title: string;
+  item: SearchWithPaginationQuery["search"]["works"][0];
 }
 
+// TODO: SVG icons. Why are they not a part of the design system as assets?
+// TODO: The material item link should point at something.
 const SearchResultListItem: React.FC<SearchResultListItemProps> = ({
-  title
+  item
 }) => {
-  return <div>{title}</div>;
+  const author = item.creators?.[0]?.name;
+  return (
+    <a href="/" className="search-result-item arrow arrow__hover--right-small">
+      <div className="search-result-item__cover">
+        <div className="material-container">
+          <a href="/" className="material material--small bg-identity-tint-120">
+            &nbsp;
+          </a>
+        </div>
+      </div>
+      <div className="search-result-item__text">
+        <h2 className="search-result-item__title text-header-h4">
+          {item.title}
+        </h2>
+        {author && <p className="text-small-caption">Af {author}</p>}
+      </div>
+      <svg
+        width="61"
+        height="9"
+        viewBox="0 0 61 9"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path className="arrow__body" d="M60 4.5H0" stroke="black" />
+        <path
+          className="arrow__head"
+          d="M60.3537 4.85355C60.5489 4.65829 60.5489 4.34171 60.3537 4.14645L57.1717 0.96447C56.9764 0.769208 56.6598 0.769208 56.4646 0.96447C56.2693 1.15973 56.2693 1.47631 56.4646 1.67157L59.293 4.5L56.4646 7.32843C56.2693 7.52369 56.2693 7.84027 56.4646 8.03553C56.6598 8.2308 56.9764 8.2308 57.1717 8.03553L60.3537 4.85355ZM60.0001 4H57.0001V5H60.0001V4Z"
+          fill="black"
+        />
+      </svg>
+    </a>
+  );
 };
 
 export default SearchResultListItem;
