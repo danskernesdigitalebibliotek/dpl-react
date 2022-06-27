81c9d49a270d1b0d943d193ea1733f4caa753c63
Jesper Kristensen <cableman@linuxdev.dk>
2022-06-27 12:55:24 +0200

    5463: Added CORS headers to mocked FBS requests

--- src/apps/hello-world/hello-world.tsx
+++ src/apps/hello-world/hello-world.tsx
@@ -2,8 +2,14 @@ import * as React from "react";
 import { Hello } from "../../components/hello/hello";
 import { useText } from "../../core/utils/text";
 
+import { getLoansV2 } from "../../core/fbs/fbs";
+
 const HelloWorld: React.FC = () => {
   const t = useText();
+
+  const test = getLoansV2();
+  console.log(test);
+
   return (
     <article>
       <h2>{t("titleText")}</h2>
