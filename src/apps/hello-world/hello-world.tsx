39e9fff36cbaa1d944c88e60f3347862d5048659
Jesper Kristensen <cableman@linuxdev.dk>
2022-06-27 13:04:06 +0200

    5463: Code clean up

--- src/apps/hello-world/hello-world.tsx
+++ src/apps/hello-world/hello-world.tsx
@@ -2,14 +2,8 @@ import * as React from "react";
 import { Hello } from "../../components/hello/hello";
 import { useText } from "../../core/utils/text";
 
-import { getLoansV2 } from "../../core/fbs/fbs";
-
 const HelloWorld: React.FC = () => {
   const t = useText();
-
-  const test = getLoansV2();
-  console.log(test);
-
   return (
     <article>
       <h2>{t("titleText")}</h2>
