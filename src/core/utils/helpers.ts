fd00d6c2776b34fe91134cef0edd691c931ff027
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-07-05 20:16:37 +0200

    Add function that can extract params
    Either from props or from url parameters

--- src/core/utils/helpers.ts
+++ src/core/utils/helpers.ts
@@ -18,3 +18,15 @@ export const convertPostIdToFaustId = (postId: Pid): FaustId | null => {
   const matches = postId.match(/^[0-9]+-[a-z]+:([0-9]+)$/);
   return matches?.[1] ? (matches?.[1] as FaustId) : null;
 };
+
+// Get params if they are defined as props use those
+// otherwise try to fetch them from the url.
+export const getParams = <T, K extends keyof T>(props: T) => {
+  const params = {} as T;
+
+  Object.entries(props).forEach(([property, value]) => {
+    params[property as K] = value || (getUrlQueryParam(property) as string);
+  });
+
+  return params;
+};
