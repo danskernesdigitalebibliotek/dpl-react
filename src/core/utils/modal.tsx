3bf820744652b6d5d8b1b88b5c0a4985f8b5360e
Sine Jespersen <sinejespersen@gmail.com>
2022-07-11 13:12:26 +0200

    5387: remove div from modal

--- src/core/utils/modal.tsx
+++ src/core/utils/modal.tsx
@@ -73,9 +73,7 @@ function Modal({
         <img src={CloseIcon} alt="" />
         {/* alt="": Hidden from screen readers, because the aria-label is sufficient */}
       </button>
-      <div className="modal-loan__container" style={{ overflow: "scroll" }}>
         {children}
-      </div>
     </div>
   );
 }
