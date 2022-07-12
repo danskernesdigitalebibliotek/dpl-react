b2226f639d7e10fa8735e6c8a4a9b3864d410453
Sine Jespersen <sinejespersen@gmail.com>
2022-07-12 11:49:32 +0200

    5387: fix test path

--- src/apps/demo-modal/demo-modal.test.ts
+++ src/apps/demo-modal/demo-modal.test.ts
@@ -1,6 +1,6 @@
 describe("Opens demo modal", () => {
   it("Opens and closes modal", () => {
-    cy.visit("/iframe.html?path=/story/demo-modal--app");
+    cy.visit("/iframe.html?path=/story/apps-demo-modal--app");
     // Url parameters are passed in, so we expect the first modal to be open on navigation to the url
     cy.get(".modal").find("h1").should("contain", "demo-modal-one");
     cy.get(".modal")
