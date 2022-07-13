d27095a3d4e4f1ca7f8c6ed311636e5c1e7e8164
kasperbirch1 <kasperbirch1@gmail.com>
2022-07-13 12:15:50 +0200

    Get select values from props instead of placeholder data
    Weeks and years now come from props. if one of them is not present it will not be displayed

--- src/components/material/MaterialPeriodikumSelect.tsx
+++ src/components/material/MaterialPeriodikumSelect.tsx
@@ -1,44 +1,52 @@
 import React from "react";
 import { useText } from "../../core/utils/text";
 
-const MaterialPeriodikumSelect: React.FC = () => {
-  const t = useText();
+// create interface for MaterialPeriodikumSelectProps widt a list of strings
+interface MaterialPeriodikumSelectProps {
+  years?: string[];
+  weeks?: string[];
+}
 
-  const placeholderData = {
-    year: ["2017", "2018", "2019", "2020", "2021", "2022"],
-    weeks: ["30", "31", "32", "33", "34", "35"]
-  };
+const MaterialPeriodikumSelect: React.FC<MaterialPeriodikumSelectProps> = ({
+  years,
+  weeks
+}) => {
+  const t = useText();
 
   return (
     <div className="text-small-caption material-periodikum ">
-      <div className="material-periodikum-select">
-        {/* This is because the design requires label and select input to be separated */}
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor="year">{t("Årgang")}</label>
-        <div className="material-periodikum-select__border-container">
-          <select id="year">
-            {placeholderData.year.map((year) => (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            ))}
-          </select>
+      {years && (
+        <div className="material-periodikum-select">
+          {/* This is because the design requires label and select input to be separated */}
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="year">{t("Årgang")}</label>
+          <div className="material-periodikum-select__border-container">
+            <select id="year">
+              {years.map((year) => (
+                <option key={year} value={year}>
+                  {year}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="material-periodikum-select">
-        {/* This is because the design requires label and select input to be separated */}
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor="weeks">{t("Uge")}</label>
-        <div className="material-periodikum-select__border-container">
-          <select id="weeks">
-            {placeholderData.weeks.map((week) => (
-              <option key={week} value={week}>
-                {week}
-              </option>
-            ))}
-          </select>
+      )}
+      {weeks && (
+        <div className="material-periodikum-select">
+          {/* This is because the design requires label and select input to be separated */}
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="weeks">{t("Uge")}</label>
+          <div className="material-periodikum-select__border-container">
+            <select id="weeks">
+              {weeks.map((week) => (
+                <option key={week} value={week}>
+                  {week}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
