5dec404468359aa2a31760a698b54adb7d80b2af
kasperbirch1 <kasperbirch1@gmail.com>
2022-07-15 11:43:34 +0200

    Change name from MaterialSeriesLine to HorizontalTermLine

--- src/components/horizontal-term-line/HorizontalTermLine.tsx
+++ src/components/horizontal-term-line/HorizontalTermLine.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 
-export interface SeriesLineProps {
+export interface HorizontalTermLineProps {
   title: string;
   subTitle?: string;
   linkList: string[];
 }
 
-const SeriesLine: React.FC<SeriesLineProps> = ({
+const HorizontalTermLine: React.FC<HorizontalTermLineProps> = ({
   title,
   subTitle,
   linkList
 }) => {
   return (
-    <div className="text-small-caption material-series-line">
+    <div className="text-small-caption horizontal-term-line">
       <p className="text-label-semibold">
         {`${title}`}{" "}
         {subTitle && <span className="text-small-caption">{subTitle} </span>}
       </p>
-      <ul className="material-series-line__list">
+      <ul className="horizontal-term-line__list">
         {linkList?.map((item) => (
           <li>
             <a href="/" className="link-tag">
@@ -30,4 +30,4 @@ const SeriesLine: React.FC<SeriesLineProps> = ({
   );
 };
 
-export default SeriesLine;
+export default HorizontalTermLine;
