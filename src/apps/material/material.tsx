0a1088c1f510ee50358d53795b8752258755de28
kasperbirch1 <kasperbirch1@gmail.com>
2022-08-02 14:21:19 +0200

    Add MaterialMainfestationItem component
    a reusable component instead of hard-coded markup

--- src/apps/material/material.tsx
+++ src/apps/material/material.tsx
@@ -7,6 +7,7 @@ import MaterialHeader from "../../components/material/MaterialHeader";
 import { useGetMaterialQuery } from "../../core/dbc-gateway/generated/graphql";
 import { Pid } from "../../core/utils/types/ids";
 import MaterialDescription from "../../components/material/MaterialDescription";
+import MaterialMainfestationItem from "../../components/material/MaterialMainfestationItem";
 
 export interface MaterialProps {
   pid: Pid;
@@ -17,6 +18,7 @@ const Material: React.FC<MaterialProps> = ({ pid, searchUrl }) => {
   const { data, isLoading } = useGetMaterialQuery({
     pid
   });
+  console.log("🚀 ~ file: material.tsx ~ line 20 ~ data", data);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -47,51 +49,8 @@ const Material: React.FC<MaterialProps> = ({ pid, searchUrl }) => {
             alt="expand-icon"
           />
         </summary>
-        <div className="material-manifestation-item">
-          <div className="material-manifestation-item__availability">
-            <div className="pagefold-parent--xsmall availability-label text-label availability-label--unselected">
-              <div className="pagefold-triangle--xsmall--success pagefold-triangle--xsmall" />
-              <img
-                className="availability-label--check available"
-                src="icons/collection/Check.svg"
-                alt="check-icon"
-              />
-              <p className="text-label-semibold ml-24">BOG</p>
-              <div className="availability-label--divider ml-4" />
-              <p className="text-label-normal ml-4 mr-8">Hjemme</p>
-            </div>
-          </div>
-          <div className="material-manifestation-item__cover">
-            <div className="material-container">
-              <span className="material material--small bg-identity-tint-120">
-                <img src="images/book_cover_3.jpg" alt="I will be replaced" />
-              </span>
-            </div>
-          </div>
-          <div className="material-manifestation-item__text">
-            <h2 className="material-manifestation-item__text__title text-header-h4">
-              Title
-            </h2>
-            <p className="text-small-caption">Af Author (2022)</p>
-            <div className="material-manifestation-item__text__details">
-              <p className="link-tag text-small-caption">
-                Detaljer om materialet
-              </p>
-              <img src={ExpandIcon} alt="ExpandMore-icon" />
-            </div>
-          </div>
-          <div className="material-manifestation-item__reserve">
-            <button
-              type="button"
-              className="btn-primary btn-filled btn-small arrow__hover--right-small"
-            >
-              RESERVER
-            </button>
-            <span className="link-tag text-small-caption material-manifestation-item__reserve__find">
-              Find på hylden
-            </span>
-          </div>
-        </div>
+        <MaterialMainfestationItem />
+        <MaterialMainfestationItem />
       </details>
       <details className="disclosure text-body-large">
         <summary className="disclosure__headline text-body-large">
