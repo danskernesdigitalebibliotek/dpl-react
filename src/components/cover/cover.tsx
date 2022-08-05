1fd3ae3c1919463393833ff45b364fa1bf7da4d4
kasperbirch1 <kasperbirch1@gmail.com>
2022-08-05 15:49:04 +0200

    Change className from material to cover
    Material has changed its name to Cover in the dpl-design-system

--- src/components/cover/cover.tsx
+++ src/components/cover/cover.tsx
@@ -46,23 +46,19 @@ export const Cover = ({
   };
 
   const classes = {
-    wrapper: clsx(
-      `material material--${size}`,
-      tintClasses[tint || "default"],
-      {
-        material__animate: animate
-      }
-    )
+    wrapper: clsx(`cover cover--${size}`, tintClasses[tint || "default"], {
+      cover__animate: animate
+    })
   };
 
   const coverUrl = data?.[0]?.imageUrls?.[`${dataSize}`]?.url;
   const image = coverUrl && <img src={coverUrl} alt={description || ""} />;
 
   return (
-    <div className="material-container">
+    <div className="cover-container">
       {/**
        * Images inside links must have an non-empty alt text to meet accessibility requirements.
-       * Only render the material as a link if we have both an url and a description.
+       * Only render the cover as a link if we have both an url and a description.
        */}
       {url && description ? (
         <LinkNoStyle href={url} className={classes.wrapper}>
