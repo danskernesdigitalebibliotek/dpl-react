7a436e68f693ab29c4c415912815e63b8bfa6798
kasperbirch1 <kasperbirch1@gmail.com>
2022-08-05 11:03:34 +0200

    Change logic to not show key without data
    Fields must not show if there is no data or if the data is undefined

--- src/components/list-description/list-description.tsx
+++ src/components/list-description/list-description.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { FC } from "react";
 
-//  TODO Move this to a global file
-export function generateId(index: number | string) {
-  const randomId = Math.random().toString(36);
-  return randomId.concat(index.toString());
-}
+// export function generateId(index: number | string) {
+//   const randomId = Math.random().toString(36);
+//   return randomId.concat(index.toString());
+// }
 
 export type ListData = {
   [k: string]: { value: string; type: "standard" | "link" };
@@ -17,10 +16,15 @@ const ListDescription: React.FC<{ data: ListData; className?: string }> = ({
 }) => {
   return (
     <dl className={`list-description ${className ?? ""}`}>
-      {Object.keys(data).map((key, index) => {
+      {Object.keys(data).map((key) => {
         const { value, type } = data[key as keyof ListData];
+
+        // Check if value is null or undefined
+        if (value === "null" || value === "undefined" || undefined || null) {
+          return null;
+        }
         return (
-          <div key={generateId(index)}>
+          <div key={key}>
             <dt>{key}:</dt>
             <dd>
               {type === "standard" && value}
