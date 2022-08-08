4e3111168199923d4c5c80eb4ed040cef25c6a98
Adam Antal <adoantal@gmail.com>
2022-08-08 09:44:11 +0200

    Add Review Hearts component
    This component will be reused in multiple of the review types. It shows
    the heart rating given to a work from the reviewer based on a string
    input (e.g. "5/6")

---
+++ src/components/material/ReviewHearts.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import HeartIconFilled from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/basic/icon-heart-filled.svg";
+import HeartIconGrey from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/basic/icon-heart-grey.svg";
+
+export interface ReviewHeartsProps {
+  amountOfHearts: string;
+}
+
+const ReviewHearts: React.FC<ReviewHeartsProps> = ({ amountOfHearts }) => {
+  const heartArray = amountOfHearts.split("/");
+  const filledHeartsArray = Array.from(
+    { length: Number(heartArray[0]) },
+    (_, i) => i + 1
+  );
+  const emptyHeartsArray = Array.from(
+    { length: Number(heartArray[1]) - Number(heartArray[0]) },
+    (_, i) => i + 1
+  );
+  return (
+    <div
+      className="mb-4"
+      role="figure"
+      aria-label="Rating of this item is 5 out of 6"
+    >
+      {filledHeartsArray.map(() => {
+        return <img src={HeartIconFilled} className="review__heart" alt="" />;
+      })}
+      {emptyHeartsArray.map(() => {
+        return <img src={HeartIconGrey} className="review__heart" alt="" />;
+      })}
+    </div>
+  );
+};
+
+export default ReviewHearts;
