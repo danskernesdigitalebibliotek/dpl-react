c01f654f5abf351bc72a24eff160f0d7c68dbbfc
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-08-12 15:35:14 +0200

    Adding cypress tests for search result
    - Introducing usage of local images in fixtures. Will also increase
      speed
    - Adding fixture data for a cover
    - Adding fixture data for the search result test itself

---
+++ src/apps/search-result/search-result.test.ts
@@ -0,0 +1,123 @@
+const coverUrlPattern = /^https:\/\/res\.cloudinary\.com\/.*\.(jpg|jpeg|png)$/;
+
+describe("Search Result", () => {
+  it("Should render the search results as expected", () => {
+    cy.visit(
+      "/iframe.html?id=apps-search-result--search-result&args=pageSizeDesktop:2;pageSizeMobile:2"
+    );
+
+    cy.log("Check length of search result list:");
+    cy.get(".search-result-page__list").find("li").should("have.length", 2);
+
+    cy.log("Do the search results have images?");
+    cy.get(".search-result-page__list .search-result-item img")
+      .should("have.attr", "src")
+      .and("match", coverUrlPattern);
+
+    cy.log("Does the search result have favourite buttons?");
+    cy.get(
+      ".search-result-page__list .search-result-item .button-favourite"
+    ).should("have.attr", "aria-label", "Add to favorites");
+
+    cy.log("Does the search result have a series line?");
+    cy.get(
+      ".search-result-page__list .search-result-item .horizontal-term-line"
+    ).should(
+      "contain.text",
+      "Nr. 1  i serienDummy Some SeriesNr. 1  i serienDummy Some Series"
+    );
+
+    cy.log("Does the search result have titles?");
+    cy.get(".search-result-page__list .search-result-item h2").should(
+      "contain.text",
+      "Dummy Some Title: Full"
+    );
+
+    cy.log("Does the search result have authors?");
+    cy.get(
+      ".search-result-page__list .search-result-item p.text-small-caption"
+    ).should("contain.text", "Af Dummy Jens Jensen (Dummy 1839)");
+
+    cy.log(
+      "Does a search result have the expected number of availibility labels?"
+    );
+    cy.get(
+      ".search-result-page__list :nth-child(1) .search-result-item .search-result-item__availability"
+    )
+      .find("a")
+      .should("have.length", 20);
+
+    cy.log("Do we have a pager?");
+    cy.get(".search-result-pager__title").should(
+      "contain.text",
+      "Viser 2 ud af 9486 resultater"
+    );
+
+    cy.log("Do we have some pager info?");
+    cy.get(".search-result-pager button").should("contain.text", "VIS FLERE");
+  });
+
+  it("Shows more items if asked to", () => {
+    cy.visit(
+      "/iframe.html?id=apps-search-result--search-result&args=pageSizeDesktop:2;pageSizeMobile:2"
+    );
+
+    cy.log("Show more results.");
+    cy.get(".search-result-pager button").click();
+
+    cy.log(
+      "Check length of search result list since it should be twice as long."
+    );
+    cy.get(".search-result-page__list").find("li").should("have.length", 4);
+
+    cy.log("The pager info should also have beenb updated.");
+    cy.get(".search-result-pager__title").should(
+      "contain.text",
+      "Viser 4 ud af 9486 resultater"
+    );
+  });
+
+  beforeEach(() => {
+    // Intercept graphql search query.
+    cy.fixture("search-result/fbi-api.json")
+      .then((result) => {
+        cy.intercept("POST", "**/opac/graphql**", result);
+      })
+      .as("Graphql search query");
+    // Intercept all images from Cloudinary.
+    cy.intercept(
+      {
+        url: coverUrlPattern
+      },
+      {
+        fixture: "images/harry-cover.jpg"
+      }
+    ).as("Harry Potter cover");
+    // Intercept covers.
+    cy.fixture("cover.json")
+      .then((result) => {
+        cy.intercept("GET", "**/covers**", result);
+      })
+      .as("Cover service");
+    // Intercept availability service.
+    cy.intercept("GET", "**/availability/v3**", {
+      statusCode: 200,
+      body: [
+        {
+          recordId: "99999999",
+          reservable: true,
+          available: true,
+          reservations: 5
+        }
+      ]
+    }).as("Availability service");
+
+    // Intercept material list service.
+    cy.intercept("HEAD", "**/list/default/**", {
+      statusCode: 404,
+      body: {}
+    }).as("Material list service");
+  });
+});
+
+export {};
