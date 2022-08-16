513ff6be37f46ce3bfc2f2b416d2f3697223d67f
kasperbirch1 <kasperbirch1@gmail.com>
2022-08-16 11:25:06 +0200

    Add cypress test of material app
    This test uses mockup data and checks if data is rendered in the right places

---
+++ src/apps/material/material.test.ts
@@ -0,0 +1,69 @@
+describe("Material", () => {
+  it("Should render the material page as expected", () => {
+    cy.log("Does the Material have title?");
+    cy.contains("Dummy Some Title: Full");
+
+    cy.log("Check that cover has a src");
+    cy.get("img").should(
+      "have.attr",
+      "src",
+      "https:res.cloudinary.com/dandigbib/image/upload/t_ddb_cover_large/v1605727140/bogportalen.dk/9781848485532.jpg"
+    );
+
+    cy.log("Does the material have favourite buttons?");
+    cy.get(".button-favourite").should(
+      "have.attr",
+      "aria-label",
+      "Add to favorites"
+    );
+
+    cy.log("Does the material have horizontal lines?");
+    cy.contains("Nr 1 i serien");
+    cy.contains("Dummy Some Series");
+
+    cy.log("Does the material have authors?");
+    cy.contains("Af Dummy Jens Jensen");
+
+    cy.log("Does a material have a availibility label");
+    cy.contains("Dummy bog");
+    cy.contains("unavailable");
+
+    cy.log("Open material details");
+    cy.get("details").click({ multiple: true });
+
+    cy.log("Does the material have a editions with a buttton to reserve");
+    cy.contains("reserver");
+  });
+
+  beforeEach(() => {
+    cy.visit("/iframe.html?args=&id=apps-material--material");
+
+    // Intercept graphql search query.
+    cy.fixture("material/fbi-api.json")
+      .then((result) => {
+        cy.intercept("POST", "**/opac/graphql**", result);
+      })
+      .as("Graphql search query");
+
+    // Intercept covers.
+    cy.fixture("material/cover.json")
+      .then((result) => {
+        cy.intercept("GET", "**/api/v2/covers?**", result);
+      })
+      .as("Cover service");
+
+    // Intercept availability's service.
+    cy.fixture("material/availability.json")
+      .then((result) => {
+        cy.intercept("GET", "**/availability/v3?recordid=**", result);
+      })
+      .as("Availability service");
+
+    // Intercept like button
+    cy.intercept("HEAD", "**/list/default/**", {
+      statusCode: 404
+    }).as("Favorite list service");
+  });
+});
+
+export {};
