c61dc93442a72feab6c4559d3949fe0bf4f4efee
kasperbirch1 <kasperbirch1@gmail.com>
2022-08-17 17:11:49 +0200

    Add setQueryParametersInUrl
    a function that can add or overwrite URL parameters

--- src/core/utils/helpers/url.ts
+++ src/core/utils/helpers/url.ts
@@ -20,6 +20,17 @@ export const getUrlQueryParam = (param: string): null | string => {
   return queryParams.get(param);
 };
 
+export const setQueryParametersInUrl = (parameters: {
+  [key: string]: string;
+}) => {
+  const processedUrl = new URL(getCurrentLocation());
+  Object.keys(parameters).forEach((key) => {
+    processedUrl.searchParams.set(key, parameters[key]);
+  });
+
+  window.history.replaceState(null, "", processedUrl);
+};
+
 export const redirectTo = (url: URL): void => {
   window.location.replace(url);
 };
