203a4ab3bbc8ae9d4aee681d54d5fc5c31c47a30
Adam Antal <adoantal@gmail.com>
2022-08-26 13:26:52 +0200

    Add inText text prop to search header app entry and story arguments

--- src/apps/search-header/search-header.entry.tsx
+++ src/apps/search-header/search-header.entry.tsx
@@ -17,6 +17,7 @@ export interface SearchHeaderTextProps {
   musicCategoryText: string;
   gameCategoryText: string;
   animatedSeriesCategoryText: string;
+  inText: string;
 }
 
 export interface SearchHeaderUrlProps {
