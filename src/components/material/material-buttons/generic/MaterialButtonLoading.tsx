e725dcdf3a4afffe1ed1430d0eeb6d74789a4593
Adam Antal <adoantal@gmail.com>
2022-08-30 15:34:09 +0200

    Make it possible to distinguish generic material reserve buttons
    ...based on usage location.

--- src/components/material/material-buttons/generic/MaterialButtonLoading.tsx
+++ src/components/material/material-buttons/generic/MaterialButtonLoading.tsx
@@ -3,8 +3,28 @@ import { FC } from "react";
 import { useText } from "../../../../core/utils/text";
 import { Button } from "../../../Buttons/Button";
 
-const MaterialButtonLoading: FC = () => {
+export interface MaterialButtonLoadingProps {
+  isOnEditionCard?: boolean;
+}
+
+const MaterialButtonLoading: FC<MaterialButtonLoadingProps> = ({
+  isOnEditionCard
+}) => {
   const t = useText();
+
+  if (isOnEditionCard) {
+    return (
+      <Button
+        label={t("loadingText")}
+        buttonType="none"
+        variant="filled"
+        disabled
+        collapsible={false}
+        size="small"
+      />
+    );
+  }
+
   return (
     <Button
       label={t("loadingText")}
