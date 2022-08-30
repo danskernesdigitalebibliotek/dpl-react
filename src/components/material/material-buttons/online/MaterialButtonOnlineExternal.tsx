ddcad3b2eff4ade06f7a8b150854844a72d41e12
Adam Antal <adoantal@gmail.com>
2022-08-30 15:35:16 +0200

    Make it possible to distinguish online material reserve buttons
    ..based on usage location.

--- src/components/material/material-buttons/online/MaterialButtonOnlineExternal.tsx
+++ src/components/material/material-buttons/online/MaterialButtonOnlineExternal.tsx
@@ -7,14 +7,32 @@ import { Button } from "../../../Buttons/Button";
 export interface MaterialButtonOnlineExternalProps {
   externalUrl: string;
   origin: string;
+  isOnEditionCard?: boolean;
 }
 
 const MaterialButtonOnlineExternal: FC<MaterialButtonOnlineExternalProps> = ({
   externalUrl = "https://google.com",
-  origin
+  origin,
+  isOnEditionCard
 }) => {
   const t = useText();
   const externalLinkObject = new URL(externalUrl);
+
+  if (isOnEditionCard) {
+    return (
+      <LinkNoStyle url={externalLinkObject}>
+        <Button
+          label={`${t("goToText")} ${origin}`}
+          buttonType="external-link"
+          variant="filled"
+          disabled={false}
+          collapsible={false}
+          size="small"
+          classNames="invert"
+        />
+      </LinkNoStyle>
+    );
+  }
 
   return (
     <LinkNoStyle url={externalLinkObject}>
--- src/components/material/material-buttons/online/MaterialButtonOnlineInfomediaArticle.tsx
+++ src/components/material/material-buttons/online/MaterialButtonOnlineInfomediaArticle.tsx
@@ -5,11 +5,12 @@ import { Button } from "../../../Buttons/Button";
 
 export interface MaterialButtonOnlineInfomediaArticleProps {
   infomediaArticleId: string;
+  isOnEditionCard?: boolean;
 }
 
 const MaterialButtonOnlineInfomediaArticle: FC<
   MaterialButtonOnlineInfomediaArticleProps
-> = ({ infomediaArticleId }) => {
+> = ({ infomediaArticleId, isOnEditionCard }) => {
   // TODO: A logged in user with municipality registration can access this.
   const isRegistered = true;
   const t = useText();
@@ -22,6 +23,22 @@ const MaterialButtonOnlineInfomediaArticle: FC<
   const onClick = (articleId: string) => {
     // TODO: view the article in full text
   };
+
+  if (isOnEditionCard) {
+    return (
+      <Button
+        label={t("readArticleText")}
+        buttonType="none"
+        variant="filled"
+        disabled={false}
+        collapsible={false}
+        size="small"
+        onClick={() => {
+          onClick(infomediaArticleId);
+        }}
+      />
+    );
+  }
 
   return (
     <Button
