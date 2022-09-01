71cc143ca201d5634811c001feb93a2c7dc72b45
Sine Jespersen <sinejespersen@gmail.com>
2022-09-01 12:55:28 +0200

    5387: arrow function always returns

--- src/components/utils/in-viewport.tsx
+++ src/components/utils/in-viewport.tsx
@@ -4,12 +4,14 @@ function useIsInViewport(offset = 0): [boolean, RefObject<HTMLInputElement>] {
   const currentElement = useRef<HTMLInputElement>(null);
   const [isVisible, setIsVisible] = useState(true);
   const getVisibility = useCallback(() => {
+    let returnOffset = false;
     if (currentElement.current) {
       const { top } = currentElement.current.getBoundingClientRect();
       // Offset is the pixels up to the of the element
-      return top + offset >= 0 && top - offset <= window.innerHeight;
+
+      returnOffset = top + offset >= 0 && top - offset <= window.innerHeight;
     }
-    return false;
+    return returnOffset;
   }, [offset]);
 
   useEffect(() => {
