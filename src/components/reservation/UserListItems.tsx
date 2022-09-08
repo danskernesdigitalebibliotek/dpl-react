5078ed863e87a7f4e94d82921cffa316187120b5
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-09-08 15:16:50 +0200

    Clean up data checking
    and move patron (user) dependant items into a separate component

---
+++ src/components/reservation/UserListItems.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import Location from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/Location.svg";
+import Subtitles from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/Subtitles.svg";
+import Message from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/Message.svg";
+import LoanHistory from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/LoanHistory.svg";
+import { FC } from "react";
+import { useText } from "../../core/utils/text";
+import ReservationFormListItem from "./ReservationFormListItem";
+import {
+  getNoInterestAfter,
+  getPreferredLocation
+} from "../../apps/material/helper";
+import { AgencyBranch, PatronV5 } from "../../core/fbs/model";
+
+export interface UserListItemsProps {
+  patron: PatronV5;
+  branchData: AgencyBranch[];
+}
+
+const UserListItems: FC<UserListItemsProps> = ({
+  patron: {
+    defaultInterestPeriod,
+    preferredPickupBranch,
+    phoneNumber,
+    emailAddress
+  },
+  branchData
+}) => {
+  const t = useText();
+  return (
+    <>
+      {defaultInterestPeriod && (
+        <ReservationFormListItem
+          icon={LoanHistory}
+          title={t("haveNoInterestAfterText")}
+          text={getNoInterestAfter(defaultInterestPeriod)}
+        />
+      )}
+      {preferredPickupBranch && branchData && (
+        <ReservationFormListItem
+          icon={Location}
+          title={t("pickupLocationText")}
+          text={getPreferredLocation(preferredPickupBranch, branchData)}
+        />
+      )}
+      {phoneNumber && (
+        <ReservationFormListItem
+          icon={Subtitles}
+          title={t("receiveSmsWhenMaterialReadyText")}
+          text={phoneNumber}
+        />
+      )}
+      {emailAddress && (
+        <ReservationFormListItem
+          icon={Message}
+          title={t("receiveEmailWhenMaterialReadyText")}
+          text={emailAddress}
+        />
+      )}
+    </>
+  );
+};
+
+export default UserListItems;
