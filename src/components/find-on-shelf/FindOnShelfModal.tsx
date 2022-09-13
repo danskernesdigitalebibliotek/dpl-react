f873fc256bf1da9b352439aa92894fa04b772ab7
Adam Antal <adoantal@gmail.com>
2022-09-13 16:11:53 +0200

    Show real holding branch names for find on shelf modal disclosures

--- src/components/find-on-shelf/FindOnShelfModal.tsx
+++ src/components/find-on-shelf/FindOnShelfModal.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { FC } from "react";
 import { ManifestationsSimpleFieldsFragment } from "../../core/dbc-gateway/generated/graphql";
+import { useGetHoldingsV3 } from "../../core/fbs/fbs";
 import {
   convertPostIdToFaustId,
   creatorsToString,
@@ -32,6 +33,16 @@ const FindOnShelfModal: FC<FindOnShelfModalProps> = ({ manifestation }) => {
       flattenCreators(filterCreators(creators, ["Person"])),
       t
     ) || t("creatorsAreMissingText");
+  const { data, isError, isLoading } = useGetHoldingsV3({
+    recordid: [faustId]
+  });
+
+  if (isError || !data) {
+    // TODO: handle error once we have established a way to do it
+    return null;
+  }
+
+  const { holdings } = data[0];
 
   return (
     <Modal
@@ -44,12 +55,27 @@ const FindOnShelfModal: FC<FindOnShelfModalProps> = ({ manifestation }) => {
         <h1 className="text-header-h2 modal-find-on-shelf__headline">
           {mainTitle} / {author}
         </h1>
-        <div className="text-small-caption modal-find-on-shelf__caption">
-          8 biblioteker har materialet
-        </div>
-        <Disclosure title="Xyz" faustId={faustId}>
-          I will one day be a list of items.
-        </Disclosure>
+        {isLoading && (
+          <p className="text-body-large ml-16 mt-96">{t("loadingText")}</p>
+        )}
+        {!isLoading && (
+          <>
+            <div className="text-small-caption modal-find-on-shelf__caption">
+              {data[0].holdings.length} biblioteker har materialet
+            </div>
+            {holdings.map((holding) => {
+              return (
+                <Disclosure
+                  key={holding.branch.branchId}
+                  title={holding.branch.title}
+                  faustId={faustId}
+                >
+                  I will one day be a list of items.
+                </Disclosure>
+              );
+            })}
+          </>
+        )}
       </>
     </Modal>
   );
