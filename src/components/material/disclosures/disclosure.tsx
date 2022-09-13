15eedcf13c7999f73bb5eb8f9f98c118e70a5a9f
Adam Antal <adoantal@gmail.com>
2022-09-13 14:02:34 +0200

    Show availability label in disclosure if a Faust ID is provided
    Allows us to show a new kind of disclosure in the "Find on shelf" modal
    on the material page. This disclosure doesn't have an icon and shows an
    availability button.

--- src/components/material/disclosures/disclosure.tsx
+++ src/components/material/disclosures/disclosure.tsx
@@ -1,19 +1,36 @@
 import ExpandMoreIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/ExpandMore.svg";
 import React, { FC, ReactNode } from "react";
+import { FaustId } from "../../../core/utils/types/ids";
+import { AvailabilityLabel } from "../../availability-label/availability-label";
 
 export interface DisclosureProps {
-  mainIconPath: string;
+  mainIconPath?: string;
   title: string;
   children?: ReactNode;
   disclosureIconExpandAltText?: string;
+  faustId?: FaustId;
 }
 
-const Disclosure: FC<DisclosureProps> = ({ title, children, mainIconPath }) => {
+const Disclosure: FC<DisclosureProps> = ({
+  title,
+  children,
+  mainIconPath,
+  faustId
+}) => {
   return (
     <details className="disclosure text-body-large">
       <summary className="disclosure__headline text-body-large">
+        {mainIconPath && (
           <div className="disclosure__icon bg-identity-tint-120">
-        {title}
+            <img className="invert" src={mainIconPath} alt="" />
+          </div>
+        )}
+        <span
+          className={`disclosure__headline__text${faustId ? "-shorter" : ""}`}
+        >
+          {title}
+        </span>
+        {faustId && <AvailabilityLabel faustIds={[faustId]} />}
         <img
           className="disclosure__expand noselect"
           src={ExpandMoreIcon}
