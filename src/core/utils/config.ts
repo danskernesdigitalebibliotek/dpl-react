07fac3ac22eb6b3fb94c66dcba50da46eb6c23bc
Sine Jespersen <sinejespersen@gmail.com>
2022-09-15 13:10:39 +0200

    remove unused code

--- src/core/utils/config.ts
+++ src/core/utils/config.ts
@@ -1,11 +1,4 @@
-import { RootState, useSelector } from "../store";
 import withConfigSuffix from "./withConfigSuffix";
-
-export type UseConfigFunction = (key: string) => string;
-export const useConfig = (): UseConfigFunction => {
-  const { data } = useSelector((state: RootState) => state.config);
-  return (key: string) => data?.[key] ?? key;
-};
 
 export const withConfig = <T>(Component: React.ComponentType<T>) => {
   return withConfigSuffix(Component);
