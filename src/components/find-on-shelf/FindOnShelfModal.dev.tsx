a04e012c388d17ad6a4ea80eb6a5b61ea0fe4dc7
Adam Antal <adoantal@gmail.com>
2022-09-21 16:31:30 +0200

    Make a Find on shelf modal story so that we can test its functionality
    We don't have the final data for the modal so the work is mostly
    abstract. With this story for the modal we can manipulate the data
    received so that we avoid "blindfolded" coding.

---
+++ src/components/find-on-shelf/FindOnShelfModal.dev.tsx
@@ -0,0 +1,116 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import { convertPostIdToFaustId } from "../../core/utils/helpers/general";
+import { Pid } from "../../core/utils/types/ids";
+import MaterialButtonsFindOnShelf from "../material/material-buttons/physical/MaterialButtonsFindOnShelf";
+import FindOnShelfModal, { FindOnShelfModalProps } from "./FindOnShelfModal";
+
+export default {
+  title: "Components / Find On Shelf Modal",
+  component: FindOnShelfModal,
+  argTypes: {
+    manifestations: {
+      name: "Manifestations",
+      defaultValue: [
+        {
+          pid: "870970-basis:52557240",
+          genreAndForm: ["slægtsromaner"],
+          source: ["Bibliotekskatalog"],
+          titles: {
+            main: ["De syv søstre"],
+            original: ["The seven sisters"]
+          },
+          publicationYear: {
+            display: "2016"
+          },
+          materialTypes: [
+            {
+              specific: "bog"
+            }
+          ],
+          creators: [
+            {
+              display: "Lucinda Riley",
+              __typename: "Person"
+            }
+          ],
+          hostPublication: null,
+          languages: {
+            main: [
+              {
+                display: "dansk"
+              }
+            ]
+          },
+          identifiers: [
+            {
+              value: "9788763844116"
+            }
+          ],
+          contributors: [
+            {
+              display: "Ulla Lauridsen"
+            }
+          ],
+          edition: {
+            summary: "1. udgave, 3. oplag (2018)"
+          },
+          audience: {
+            generalAudience: []
+          },
+          physicalDescriptions: [
+            {
+              numberOfPages: null
+            }
+          ],
+          accessTypes: [
+            {
+              code: "PHYSICAL"
+            }
+          ],
+          access: [
+            {
+              __typename: "InterLibraryLoan",
+              loanIsPossible: true
+            }
+          ]
+        }
+      ],
+      control: { type: "object" }
+    },
+    workTitles: {
+      name: "Work title(s)",
+      defaultValue: ["Title 1", "Title 2"],
+      control: { type: "object" }
+    },
+    authors: {
+      name: "Work title(s)",
+      defaultValue: [
+        { __typename: "Person", display: "author 1" },
+        { __typename: "Person", display: "author 2" },
+        { __typename: "Corporation", display: "author 3" }
+      ],
+      control: { type: "object" }
+    },
+    pid: {
+      name: "Work title(s)",
+      defaultValue: "870970-basis:52557240",
+      control: { type: "text" }
+    }
+  }
+} as ComponentMeta<typeof FindOnShelfModal>;
+
+export const Default: ComponentStory<typeof FindOnShelfModal> = (
+  args: FindOnShelfModalProps
+) => {
+  const { pid } = args;
+  return (
+    <>
+      <MaterialButtonsFindOnShelf
+        size="small"
+        faustIds={[convertPostIdToFaustId(pid as Pid)]}
+      />
+      <FindOnShelfModal {...args} />{" "}
+    </>
+  );
+};
