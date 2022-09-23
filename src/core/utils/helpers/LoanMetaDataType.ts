f9154c2d516ca5e6b2a6d81a93d0b3bcee0e5a04
Sine Jespersen <sinejespersen@gmail.com>
2022-09-23 15:10:57 +0200

    5379: fixes after rebase

--- src/core/utils/helpers/LoanMetaDataType.ts
+++
@@ -1,9 +0,0 @@
-export interface LoanMetaDataType {
-  dueDate: string | null | undefined;
-  loanDate: string | null | undefined;
-  id: string;
-  isRenewable: boolean;
-  materialItemNumber: string | null | undefined;
-  renewalStatusList: string[];
-  loanType: string | null;
-}
