c815bb82aecf634e45d0de58b2eab01bf4f17698
Sine Jespersen <sinejespersen@gmail.com>
2022-10-06 08:50:23 +0200

    5379: move helpers to list mapper file

---
+++ src/core/utils/helpers/list-mapper.ts
@@ -0,0 +1,161 @@
+import { LoanV2, RenewedLoanV2 } from "../../fbs/model";
+import { FaustId } from "../types/ids";
+import { store } from "../../store";
+import { GetMaterialManifestationQuery } from "../../dbc-gateway/generated/graphql";
+import { BasicDetailsType } from "../types/basic-details-type";
+import { Product, Loan } from "../../publizon/model";
+import { LoanType } from "../types/loan-type";
+
+// Creates a "by author, author and author"-string
+// String interpolation todo?
+export const getContributors = (creators: string[] | undefined) => {
+  const {
+    text: { data: texts }
+  } = store.getState();
+
+  let returnContentString = "";
+  if (creators && creators.length > 0) {
+    if (creators.length === 1) {
+      returnContentString = `${texts.materialByAuthorText} ${creators.join(
+        ", "
+      )}`;
+    } else {
+      returnContentString = `${texts.materialByAuthorText} ${creators
+        .slice(0, -1)
+        .join(", ")} ${texts.materialAndAuthorText} ${creators.slice(-1)}`;
+    }
+  }
+  return returnContentString;
+};
+
+//
+function getYearFromDataString(date?: string) {
+  if (date) {
+    return new Date(date).getFullYear();
+  }
+  return "";
+}
+
+// Loan is a loan from Publizon, and is the equivalent
+// to the LoanV2 type in FBS. These are mapped to the same
+// so digital/physical loans/reservations can use the same components,
+// as their UI is often quite similar
+export const mapPublizonLoanToLoanType = (list: Loan[]): LoanType[] => {
+  return list.map(({ loanExpireDateUtc, orderDateUtc, libraryBook }) => {
+    return {
+      dueDate: loanExpireDateUtc,
+      loanDate: orderDateUtc,
+      isRenewable: false,
+      materialItemNumber: libraryBook?.identifier || "",
+      renewalStatusList: [],
+      loanType: null,
+      identifier: libraryBook?.identifier || null,
+      faust: null
+    };
+  });
+};
+
+// LoanV2 is a loan from FBS, and is the equivalent
+// to the Loan type in Publizon. These are mapped to the same
+// so digital/physical loans/reservations can use the same components,
+// as their UI is often quite similar
+export const mapFBSLoanToLoanType = (list: LoanV2[]): LoanType[] => {
+  return list.map(({ loanDetails, isRenewable, renewalStatusList }) => {
+    return {
+      dueDate: loanDetails.dueDate,
+      loanDate: loanDetails.loanDate,
+      renewalStatusList,
+      isRenewable,
+      materialItemNumber: loanDetails.materialItemNumber,
+      loanType: loanDetails.loanType,
+      identifier: null,
+      faust: (loanDetails.recordId as FaustId) || null
+    };
+  });
+};
+
+// RenewedLoanV2 is a renewed loan from FBS, and for the moment has no
+// equivalent in publizon. It is mapped to loan type, because the only
+// thing relevant in the UI is that the loan has been renewed, and is
+// Not renewable any longer.
+export const mapFBSRenewedLoanToLoanType = (
+  list: RenewedLoanV2[]
+): LoanType[] => {
+  return list.map(({ loanDetails }) => {
+    return {
+      dueDate: loanDetails.dueDate,
+      loanDate: loanDetails.loanDate,
+      renewalStatusList: [],
+      isRenewable: false,
+      materialItemNumber: loanDetails.materialItemNumber,
+      loanType: loanDetails.loanType,
+      identifier: null,
+      faust: (loanDetails.recordId as FaustId) || null
+    };
+  });
+};
+
+// Product is a material from Publizon, and is the equivalent
+// to a manifestation from FBI. These are mapped to the same
+// so digital/physical loans/reservations can use the same components,
+// as their UI is often quite similar
+export const mapProductToBasicDetailsType = (material: Product) => {
+  const {
+    publicationDate,
+    title,
+    description,
+    productType,
+    contributors,
+    externalProductId
+  } = material;
+
+  const {
+    text: { data: texts }
+  } = store.getState();
+
+  const digitalProductType: { [key: number]: string } = {
+    1: texts.publizonEbookText,
+    2: texts.publizonAudioBookText,
+    4: texts.publizonAudioBookText
+  };
+
+  return {
+    title,
+    year: getYearFromDataString(publicationDate),
+    description,
+    materialType: productType ? digitalProductType[productType] : "",
+    externalProductId: externalProductId?.id,
+    authors: getContributors(
+      contributors?.map(({ firstName, lastName }) => `${firstName} ${lastName}`)
+    )
+  } as BasicDetailsType;
+};
+
+// Manifestation is a material manifestation from FBI, and is the equivalent
+// to the Product type in Publizon. These are mapped to the same
+// so digital/physical loans/reservations can use the same components,
+// as their UI is often quite similar
+export const mapManifestationToBasicDetailsType = (
+  material: GetMaterialManifestationQuery
+) => {
+  const { hostPublication, abstract, titles, pid, materialTypes, creators } =
+    material?.manifestation || {};
+
+  const description = abstract ? abstract[0] : "";
+  const {
+    main: [mainText]
+  } = titles || { main: [] };
+  const { year: yearObject } = hostPublication || {};
+  const { year } = yearObject || {};
+
+  return {
+    authors: getContributors(creators?.map(({ display }) => display)),
+    pid,
+    title: mainText,
+    year,
+    description,
+    materialType: materialTypes ? materialTypes[0].specific : undefined
+  } as BasicDetailsType;
+};
+
+export default {};
