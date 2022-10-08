dbd75d8f4c522db5860663cdd817b8c34655d25e
Sine Jespersen <sinejespersen@gmail.com>
2022-10-08 21:12:19 +0200

    5381: add save buttons

--- src/apps/reservation-list/modal/physical-list-details.tsx
+++ src/apps/reservation-list/modal/physical-list-details.tsx
@@ -1,4 +1,10 @@
-import React, { FC, useEffect, useState } from "react";
+import React, {
+  FC,
+  useEffect,
+  useState,
+  useCallback,
+  ChangeEvent
+} from "react";
 import EbookIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/Ebook.svg";
 import ExpandIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/ExpandMore.svg";
 import LocationIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/Location.svg";
@@ -8,11 +14,12 @@ import LoansIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icon
 import { useText } from "../../../core/utils/text";
 import { ReservationType } from "../../../core/utils/types/reservation-type";
 import { MaterialProps } from "../../loan-list/materials/utils/material-fetch-hoc";
-import { useGetBranches } from "../../../core/fbs/fbs";
+import { useGetBranches, useUpdateReservations } from "../../../core/fbs/fbs";
 import { getPreferredBranch } from "../../../components/reservation/helper";
 import { AgencyBranch } from "../../../core/fbs/model";
 import { formatDate } from "../../loan-list/utils/helpers";
 import ListDetails from "../../../components/list-details/list-details";
+import { Button } from "../../../components/Buttons/Button";
 
 export interface PhysicalListDetailsProps {
   reservation: ReservationType;
@@ -25,6 +32,8 @@ const PhysicalListDetails: FC<PhysicalListDetailsProps & MaterialProps> = ({
   const branchResponse = useGetBranches();
   const [pickupBranchFetched, setPickupBranchFetched] = useState<string>("");
   const [branches, setBranches] = useState<AgencyBranch[]>([]);
+  const [selectedBranch, setSelectedBranch] = useState<AgencyBranch>();
+  const { mutate } = useUpdateReservations();
 
   const {
     numberInQueue,
@@ -32,8 +41,12 @@ const PhysicalListDetails: FC<PhysicalListDetailsProps & MaterialProps> = ({
     expiryDate,
     pickupDeadline,
     dateOfReservation,
-    pickupNumber
+    pickupNumber,
+    reservationId
   } = reservation;
+  const [selectedExpiryDate, setSelectedExpiryDate] = useState<string>(
+    expiryDate || ""
+  );
 
   useEffect(() => {
     if (branchResponse.data && pickupBranch) {
@@ -41,8 +54,65 @@ const PhysicalListDetails: FC<PhysicalListDetailsProps & MaterialProps> = ({
       setPickupBranchFetched(
         getPreferredBranch(pickupBranch, branchResponse.data as AgencyBranch[])
       );
+      // selected branch
+      const selected: AgencyBranch[] = branchResponse.data.filter(
+        ({ branchId }) => branchId === pickupBranch
+      );
+      if (selected.length > 0) {
+        setSelectedBranch(selected[0]);
+      }
     }
   }, [branchResponse.data, pickupBranch]);
+
+  const changeSelectedBranch = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      const newBranch = branches.filter(
+        ({ branchId }) => branchId === e.target.value
+      );
+      if (newBranch.length > 0) {
+        setSelectedBranch(newBranch[0]);
+      }
+    },
+    [branches]
+  );
+
+  const save = useCallback(() => {
+    if (reservationId) {
+      mutate(
+        {
+          data: {
+            reservations: [
+              {
+                expiryDate: selectedExpiryDate,
+                pickupBranch: selectedBranch?.branchId,
+                reservationId
+              }
+            ]
+          }
+        },
+        {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          onSuccess: (result) => {
+            // todo
+          },
+          // todo error handling, missing in figma
+          onError: () => {}
+        }
+      );
+    }
+  }, [mutate, reservationId, selectedBranch, selectedExpiryDate]);
+
+  const cancel = useCallback(() => {
+    // Reset branch to original branch
+    const originalBranch = branches.filter(
+      ({ branchId }) => branchId === pickupBranch
+    );
+    if (originalBranch.length > 0) {
+      setSelectedBranch(originalBranch[0]);
+    }
+    // Reset expiry date to original expiryDate
+    setSelectedExpiryDate(expiryDate || "");
+  }, [branches, expiryDate, pickupBranch]);
 
   return (
     <>
@@ -63,14 +133,17 @@ const PhysicalListDetails: FC<PhysicalListDetailsProps & MaterialProps> = ({
           labels={[pickupBranchFetched, pickupNumber || ""]}
         >
           <div className="dropdown">
-            <select className="dropdown__select">
+            <select
+              className="dropdown__select"
+              onChange={(e) => changeSelectedBranch(e)}
+            >
               {/* todo when does the value change */}
               {branches.map(({ title: branchTitle, branchId }) => (
                 <option
                   key={branchId}
-                  selected={branchId === pickupBranch}
+                  selected={selectedBranch?.branchId === branchId}
                   className="dropdown__option"
-                  value={branchTitle}
+                  value={branchId}
                 >
                   {branchTitle}
                 </option>
@@ -91,7 +164,8 @@ const PhysicalListDetails: FC<PhysicalListDetailsProps & MaterialProps> = ({
           <div className="dropdown">
             <input
               className="dropdown__select"
-              value={expiryDate}
+              value={selectedExpiryDate}
+              onChange={(e) => setSelectedExpiryDate(e.target.value)}
               type="date"
             />
           </div>
@@ -111,6 +185,20 @@ const PhysicalListDetails: FC<PhysicalListDetailsProps & MaterialProps> = ({
           labels={[formatDate(dateOfReservation)]}
         />
       )}
+      <div className="modal-details__buttons">
+        <button type="button" className="link-tag mx-16" onClick={cancel}>
+          {t("reservationDetailsCancelText")}
+        </button>
+        <Button
+          label={t("reservationDetailsSaveText")}
+          buttonType="none"
+          variant="filled"
+          disabled={false}
+          onClick={save}
+          collapsible={false}
+          size="small"
+        />
+      </div>
     </>
   );
 };
