edbe238558484442c76c394bde19df888399dceb
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-10-11 17:40:51 +0200

    Add support for dpl cms api base url in fetcher
    It has been implemented the same way as it has been done with the FBS
    and the Publizon fetchers

--- src/core/utils/helpers/fetcher.ts
+++ src/core/utils/helpers/fetcher.ts
@@ -1,16 +1,21 @@
 import { store } from "../../store";
 
+type Api = "publizon" | "fbs" | "dplCms";
+type ApiBaseUrl = `${Api}BaseUrl`;
+
 interface ConfigPossibilities {
-  fbs: string;
-  publizon: string;
+  fbs: ApiBaseUrl;
+  publizon: ApiBaseUrl;
+  dplCms: ApiBaseUrl;
 }
 
 export const configTypes: ConfigPossibilities = {
   fbs: "fbsBaseUrl",
-  publizon: "publizonBaseUrl"
+  publizon: "publizonBaseUrl",
+  dplCms: "dplCmsBaseUrl"
 } as const;
 
-export const getFectcherUrl = (endpoint: string) => {
+export const getFetcherUrl = (endpoint: string) => {
   const {
     url: { data: appUrls }
   } = store.getState();
