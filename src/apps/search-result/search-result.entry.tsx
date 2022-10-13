af07a1344e34fd7dff2b835e93ce726f380a5a8a
kasperbirch1 <kasperbirch1@gmail.com>
2022-10-13 11:20:35 +0200

    Add blacklistedBranchesConfig + branchesConfig
    In the future, this will come from cms, so it has therefore moved to storybook props

--- src/apps/search-result/search-result.entry.tsx
+++ src/apps/search-result/search-result.entry.tsx
@@ -27,6 +27,7 @@ interface SearchResultEntryUrlProps {
 interface SearchResultEntryConfigProps {
   blacklistedAvailabilityBranchesConfig: string;
   blacklistedPickupBranchesConfig?: string;
+  blacklistedSearchBranchesConfig?: string;
   branchesConfig: string;
 }
 
