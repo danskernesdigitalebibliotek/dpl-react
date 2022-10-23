49657f9ebe80f53893a44c21131396e2ab5bebbf
Sine Jespersen <sinejespersen@gmail.com>
2022-10-23 20:28:34 +0200

    5387: add link to material overdue

--- src/apps/loan-list/materials/stackable-material/material-overdue-link.tsx
+++ src/apps/loan-list/materials/stackable-material/material-overdue-link.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { Link } from "../../../../components/atoms/link";
 import { useUrls } from "../../../../core/utils/url";
 import { materialIsOverdue } from "../../utils/helpers";
 
