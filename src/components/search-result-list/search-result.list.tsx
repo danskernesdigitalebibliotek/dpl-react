f51f39e368dda3e28b060a9310126bdff618a25e
Adam Antal <adoantal@gmail.com>
2022-10-25 12:39:07 +0200

    Make an API call but fake the response data for campaigns
    This is a temporary solution just so that I could implement the
    campaigns showing.

--- src/components/search-result-list/search-result.list.tsx
+++ src/components/search-result-list/search-result.list.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useCampaignMatchPOST } from "../../core/dpl-cms/dpl-cms";
+import { CampaignMatchPOST200 } from "../../core/dpl-cms/model";
 import { getCoverTint } from "../../core/utils/helpers/general";
 import { Work } from "../../core/utils/types/entities";
 import Campaign from "../campaign/campaign";
@@ -7,6 +9,96 @@ import SearchResultListItem from "./search-result-list-item/search-result-list-i
 export interface SearchResultListProps {
   resultItems: Work[];
 }
+const SearchResultList: React.FC<SearchResultListProps> = ({ resultItems }) => {
+  const { mutate } = useCampaignMatchPOST();
+  const [campaignData, setCampaignData] = useState<CampaignMatchPOST200 | null>(
+    null
+  );
+  useEffect(() => {
+    mutate(
+      {
+        data: [
+          {
+            name: "mainLanguages",
+            values: [
+              {
+                key: "eng",
+                term: "Engelsk",
+                score: 30805
+              },
+              {
+                key: "dan",
+                term: "Dansk",
+                score: 7543
+              },
+              {
+                key: "ger",
+                term: "Tysk",
+                score: 2465
+              },
+              {
+                key: "mul",
+                term: "flere sprog",
+                score: 1510
+              },
+              {
+                key: "swe",
+                term: "Svensk",
+                score: 1140
+              }
+            ]
+          },
+          {
+            name: "materialTypes",
+            values: [
+              {
+                key: "bog",
+                term: "bog",
+                score: 17832
+              },
+              {
+                key: "artikel",
+                term: "artikel",
+                score: 11768
+              },
+              {
+                key: "cd (musik)",
+                term: "cd (musik)",
+                score: 2988
+              },
+              {
+                key: "grammofonplade",
+                term: "grammofonplade",
+                score: 2591
+              },
+              {
+                key: "ebog",
+                term: "e-bog",
+                score: 2538
+              }
+            ]
+          }
+        ]
+      },
+      {
+        onSuccess: (data) => {
+          setCampaignData(data);
+        },
+        onError: () => {
+          setCampaignData({
+            data: {
+              text: "Have you ever wanted to not be alone? This is a fake campaign, so try it today! Good luck, and don't f it up *wink* 🏆",
+              image: {
+                url: "https://picsum.photos/id/777/300/200",
+                alt: "I am an image."
+              },
+              url: "http://localhost/?path=/story/apps-search-result--search-result"
+            }
+          });
+        }
+      }
+    );
+  }, [mutate]);
 
   return (
     <ul className="search-result-page__list my-32">
