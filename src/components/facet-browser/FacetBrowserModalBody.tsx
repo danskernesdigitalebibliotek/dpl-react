207487654860e57b8d17f87ef74bfa5523f060f1
kasperbirch1 <kasperbirch1@gmail.com>
2022-10-26 13:17:56 +0200

    Add functionality to use terms from facet browser to search with
    The following has been added:
    - useFilterHandler is a hook that returns a state for filters and a filterHandler function to add and remove
    
    - formatFilters is a function that formats filters so that they can be sent with searches
    
    - add prefix + postfix to facet name in Disclosure ``` 
    title={t(`facet${capitalizeFirstLetter(name)}Text`)}  ```

--- src/components/facet-browser/FacetBrowserModalBody.tsx
+++ src/components/facet-browser/FacetBrowserModalBody.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import {
+  FilterItemTerm,
+  TagOnclickHandler
+} from "../../apps/search-result/types";
 import { FacetResult } from "../../core/dbc-gateway/generated/graphql";
+import { capitalizeFirstLetter } from "../../core/utils/helpers/general";
 import { useText } from "../../core/utils/text";
 import { Button } from "../Buttons/Button";
 import Disclosure from "../material/disclosures/disclosure";
@@ -7,11 +12,13 @@ import Tag from "../tag/Tag";
 
 interface FacetBrowserModalBodyProps {
   facets: FacetResult[];
+  filterHandler: TagOnclickHandler;
+  filters: { [key: string]: { [key: string]: FilterItemTerm } };
 }
 
 const FacetBrowserModalBody: React.FunctionComponent<
   FacetBrowserModalBodyProps
-> = ({ facets }) => {
+> = ({ facets, filterHandler, filters }) => {
   const t = useText();
   return (
     <section className="facet-browser">
@@ -27,7 +34,8 @@ const FacetBrowserModalBody: React.FunctionComponent<
           </button>
         )}
       </header>
-      {facets.map(({ name, values }) => {
+      {facets.map((facet) => {
+        const { name, values } = facet;
         // Remove facets disclosures with no tags
         if (values.length === 0) return null;
 
@@ -36,14 +44,32 @@ const FacetBrowserModalBody: React.FunctionComponent<
             key={name}
             fullWidth
             removeHeadlinePadding
-            title={t(`${name}Text`)}
+            title={t(`facet${capitalizeFirstLetter(name)}Text`)}
           >
             <div className="facet-browser__facet-group">
-              {values.map(({ term, score }) => (
-                <Tag>
-                  {term} ({score})
-                </Tag>
-              ))}
+              {values.map((termItem) => {
+                const { term } = termItem;
+
+                const selected = Boolean(filters[name] && filters[name][term]);
+
+                return (
+                  <Tag
+                    key={term}
+                    onClick={() =>
+                      filterHandler({
+                        filterItem: {
+                          facet: name,
+                          term: termItem
+                        },
+                        action: selected ? "remove" : "add"
+                      })
+                    }
+                    selected={selected}
+                  >
+                    {termItem.term} ({termItem.score})
+                  </Tag>
+                );
+              })}
             </div>
 
             {/* TODO: It will be used in the future to check if there is more tags to show */}
--- src/core/utils/helpers/general.ts
+++ src/core/utils/helpers/general.ts
@@ -262,4 +262,8 @@ export const pageSizeGlobal = (
   return pageSize;
 };
 
+export const capitalizeFirstLetter = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 export default {};
