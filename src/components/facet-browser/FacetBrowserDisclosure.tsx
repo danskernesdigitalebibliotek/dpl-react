3dde8eb5f5f1154c7af438d6bf1e4db733dfecc5
kasperbirch1 <kasperbirch1@gmail.com>
2022-10-28 06:17:54 +0200

    Add FacetBrowserDisclosure
    Behavior for details / summary open attribute means that we could not get our logic to work. So we make our own with div's elements

---
+++ src/components/facet-browser/FacetBrowserDisclosure.tsx
@@ -0,0 +1,80 @@
+import ExpandMoreIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/ExpandMore.svg";
+import clsx from "clsx";
+import React, { FC, ReactNode, useCallback } from "react";
+import { useText } from "../../core/utils/text";
+import Pagefold from "../pagefold/Pagefold";
+
+export interface FacetBrowserDisclosureProps {
+  mainIconPath?: string;
+  title: string;
+  children?: ReactNode;
+  isAvailable?: boolean;
+  fullWidth?: boolean;
+  showContent?: boolean;
+  removeHeadlinePadding?: boolean;
+  onClick?: () => void;
+}
+
+const FacetBrowserDisclosure: FC<FacetBrowserDisclosureProps> = ({
+  title,
+  children,
+  mainIconPath,
+  isAvailable,
+  fullWidth,
+  showContent = false,
+  removeHeadlinePadding,
+  onClick
+}) => {
+  const t = useText();
+  const onClickHandler = useCallback(() => {
+    if (onClick) {
+      onClick();
+    }
+  }, [onClick]);
+
+  return (
+    <div
+      className={`disclosure text-body-large ${
+        fullWidth ? "disclosure--full-width" : ""
+      }`}
+    >
+      <div
+        className={clsx(
+          "disclosure__headline text-body-large",
+          removeHeadlinePadding && "disclosure__headline--no-padding"
+        )}
+        onClick={onClickHandler}
+        onKeyDown={onClickHandler}
+        role="button"
+        tabIndex={0}
+      >
+        {mainIconPath && (
+          <div className="disclosure__icon bg-identity-tint-120">
+            <img className="invert" src={mainIconPath} alt="" />
+          </div>
+        )}
+        <span
+          className={`disclosure__text${
+            isAvailable !== undefined ? "--shorter" : ""
+          }`}
+        >
+          {title}
+        </span>
+        {isAvailable !== undefined && (
+          <Pagefold
+            text={isAvailable ? t("available") : t("unavailable")}
+            state={isAvailable ? "success" : "alert"}
+          />
+        )}
+        <img
+          className="disclosure__expand noselect"
+          src={ExpandMoreIcon}
+          alt=""
+        />
+      </div>
+      {showContent && children}
+    </div>
+  );
+};
+
+export default FacetBrowserDisclosure;
