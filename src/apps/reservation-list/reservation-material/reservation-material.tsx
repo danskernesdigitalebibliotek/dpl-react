2598106df3e7037746b5d41824a33bc8fd081c8d
Sine Jespersen <sinejespersen@gmail.com>
2022-11-02 13:12:47 +0100

    5401: revert switch import order

--- src/apps/reservation-list/reservation-material/reservation-material.tsx
+++ src/apps/reservation-list/reservation-material/reservation-material.tsx
@@ -3,15 +3,15 @@ import fetchMaterial, {
   MaterialProps
 } from "../../loan-list/materials/utils/material-fetch-hoc";
 import { ReservationType } from "../../../core/utils/types/reservation-type";
+import ReservationInfo from "./reservation-info";
 import fetchDigitalMaterial from "../../loan-list/materials/utils/digital-material-fetch-hoc";
 import MaterialInfo from "../../loan-list/materials/stackable-material/material-info";
 import MaterialDetailsModal from "../../loan-list/modal/material-details-modal";
-import ReservationDetails from "../modal/reservation-details/reservation-details";
 import { useModalButtonHandler } from "../../../core/utils/modal";
 import { useConfig } from "../../../core/utils/config";
 import { AgencyBranch } from "../../../core/fbs/model";
 import { excludeBlacklistedBranches } from "../../../components/reservation/helper";
-import ReservationInfo from "./reservation-info";
+import ReservationDetails from "../modal/reservation-details/reservation-details";
 
 export interface ReservationMaterialProps {
   reservation: ReservationType;
