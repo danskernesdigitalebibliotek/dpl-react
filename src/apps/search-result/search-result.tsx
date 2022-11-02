8e796a781ce244dab5f7051f150e1fa0143a3a41
Adam Antal <adoantal@gmail.com>
2022-11-02 11:12:23 +0100

    Move campaign & its logic from "search result list" to "search result"
    This makes sense as the campaign isn't a part of the search result list,
    but rather just a different component that shows on the page under
    specific circumstances.

--- src/apps/search-result/search-result.tsx
+++ src/apps/search-result/search-result.tsx
@@ -3,7 +3,9 @@ import SearchResultHeader from "../../components/search-bar/search-result-header
 import usePager from "../../components/result-pager/use-pager";
 import SearchResultList from "../../components/search-result-list/SearchResultList";
 import {
+  FacetField,
   SearchWithPaginationQuery,
+  useSearchFacetQuery,
   useSearchWithPaginationQuery
 } from "../../core/dbc-gateway/generated/graphql";
 import { Work } from "../../core/utils/types/entities";
@@ -16,11 +18,32 @@ import {
 import useFilterHandler from "./useFilterHandler";
 import { TagOnclickHandler } from "./types";
 import FacetBrowserModal from "../../components/facet-browser/FacetBrowserModal";
+import { useCampaignMatchPOST } from "../../core/dpl-cms/dpl-cms";
+import {
+  CampaignMatchPOST200,
+  CampaignMatchPOSTBodyItem
+} from "../../core/dpl-cms/model";
+import { isObjectEmpty } from "../../core/utils/helpers/general";
+import { formatFilters } from "./helpers";
+import Campaign from "../../components/campaign/Campaign";
 
 interface SearchResultProps {
   q: string;
   pageSize: number;
 }
+
+export const allFacetFields = [
+  FacetField.MainLanguages,
+  FacetField.AccessTypes,
+  FacetField.ChildrenOrAdults,
+  FacetField.Creators,
+  FacetField.FictionNonfiction,
+  FacetField.FictionalCharacter,
+  FacetField.GenreAndForm,
+  FacetField.MaterialTypes,
+  FacetField.Subjects,
+  FacetField.WorkTypes
+];
 
 const SearchResult: React.FC<SearchResultProps> = ({ q, pageSize }) => {
   const config = useConfig();
@@ -30,28 +53,55 @@ const SearchResult: React.FC<SearchResultProps> = ({ q, pageSize }) => {
   const blacklistBranches = config("blacklistedSearchBranchesConfig", {
     transformer: "stringToArray"
   });
-
   const whitelistBranches = excludeBlacklistedBranches(
     branches,
     blacklistBranches
   );
   const cleanBranches = cleanBranchesId(whitelistBranches);
-
   const [resultItems, setResultItems] = useState<Work[]>([]);
   const [hitcount, setHitCount] = useState<number>(0);
   const { PagerComponent, page, resetPager } = usePager(hitcount, pageSize);
   const { filters, filterHandler } = useFilterHandler();
-
+  const { mutate } = useCampaignMatchPOST();
+  const [campaignData, setCampaignData] = useState<CampaignMatchPOST200 | null>(
+    null
+  );
   const filteringHandler: TagOnclickHandler = (filterInfo) => {
     filterHandler(filterInfo);
     resetPager();
   };
+  const { data } = useSearchFacetQuery({
+    q: { all: q },
+    facets: allFacetFields,
+    facetLimit: 10,
+    ...(isObjectEmpty(filters)
+      ? {}
+      : { filters: { ...formatFilters(filters) } })
+  });
 
   // If q changes (eg. in Storybook context)
   //  then make sure that we reset the entire result set.
   useEffect(() => {
     setResultItems([]);
   }, [q, pageSize, filters]);
+
+  useEffect(() => {
+    if (data) {
+      mutate(
+        {
+          data: data.search.facets as CampaignMatchPOSTBodyItem[]
+        },
+        {
+          onSuccess: (campaign) => {
+            setCampaignData(campaign);
+          },
+          onError: () => {
+            // TODO: when we handle errors - handle this error
+          }
+        }
+      );
+    }
+  }, [mutate, data]);
 
   useSearchWithPaginationQuery(
     {
@@ -92,7 +142,10 @@ const SearchResult: React.FC<SearchResultProps> = ({ q, pageSize }) => {
       {worksAreLoaded && (
         <>
           <SearchResultHeader hitcount={String(hitcount)} q={q} />
-          <SearchResultList resultItems={resultItems} filters={filters} q={q} />
+          {campaignData && campaignData.data && (
+            <Campaign campaignData={campaignData.data} />
+          )}
+          <SearchResultList resultItems={resultItems} />
           {PagerComponent}
         </>
       )}
--- src/components/search-result-list/SearchResultList.tsx
+++ src/components/search-result-list/SearchResultList.tsx
@@ -1,80 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { formatFilters } from "../../apps/search-result/helpers";
-import { FilterItemTerm } from "../../apps/search-result/types";
-import {
-  FacetField,
-  useSearchFacetQuery
-} from "../../core/dbc-gateway/generated/graphql";
-import { useCampaignMatchPOST } from "../../core/dpl-cms/dpl-cms";
-import {
-  CampaignMatchPOST200,
-  CampaignMatchPOSTBodyItem
-} from "../../core/dpl-cms/model";
-import { getCoverTint, isObjectEmpty } from "../../core/utils/helpers/general";
+import React from "react";
+import { getCoverTint } from "../../core/utils/helpers/general";
 import { Work } from "../../core/utils/types/entities";
-import Campaign from "../campaign/Campaign";
 import SearchResultListItem from "./search-result-list-item/search-result-list-item";
 
 export interface SearchResultListProps {
   resultItems: Work[];
-  filters: { [key: string]: { [key: string]: FilterItemTerm } };
-  q: string;
 }
 
-export const allFacetFields = [
-  FacetField.MainLanguages,
-  FacetField.AccessTypes,
-  FacetField.ChildrenOrAdults,
-  FacetField.Creators,
-  FacetField.FictionNonfiction,
-  FacetField.FictionalCharacter,
-  FacetField.GenreAndForm,
-  FacetField.MaterialTypes,
-  FacetField.Subjects,
-  FacetField.WorkTypes
-];
-
-const SearchResultList: React.FC<SearchResultListProps> = ({
-  resultItems,
-  filters,
-  q
-}) => {
-  const { mutate } = useCampaignMatchPOST();
-  const [campaignData, setCampaignData] = useState<CampaignMatchPOST200 | null>(
-    null
-  );
-  const { data } = useSearchFacetQuery({
-    q: { all: q },
-    facets: allFacetFields,
-    facetLimit: 10,
-    ...(isObjectEmpty(filters)
-      ? {}
-      : { filters: { ...formatFilters(filters) } })
-  });
-
-  useEffect(() => {
-    if (data) {
-      mutate(
-        {
-          data: data.search.facets as CampaignMatchPOSTBodyItem[]
-        },
-        {
-          onSuccess: (campaign) => {
-            setCampaignData(campaign);
-          },
-          onError: () => {
-            // TODO: when we handle errors - handle this error
-          }
-        }
-      );
-    }
-  }, [mutate, data]);
-
+const SearchResultList: React.FC<SearchResultListProps> = ({ resultItems }) => {
   return (
-    <ul className="search-result-page__list my-32">
-      {campaignData && campaignData.data && (
-        <Campaign campaignData={campaignData.data} />
-      )}
+    <ul className="search-result-page__list my-16">
       {resultItems.map((item, i) => (
         <li key={item.workId}>
           <SearchResultListItem item={item} coverTint={getCoverTint(i)} />
