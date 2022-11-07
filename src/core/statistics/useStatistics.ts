7862206bb3c95069413e4ba9d8333f38655d747b
Adam Antal <adoantal@gmail.com>
2022-11-07 15:31:23 +0100

    Improve documentation for useStatiscits and connected Mapp cloud
    We use Mapp's webtrekk for tracking - a specialized tool with extensive
    documentation. Helpful links and documentation in the code will be
    helpful for future developers trying to understand the tool.

--- src/core/statistics/useStatistics.ts
+++ src/core/statistics/useStatistics.ts
@@ -1,4 +1,14 @@
-export function useStatistics() {
+// Useful resources for Mapp tracking:
+// https://documentation.mapp.com/1.0/en/manual-track-request-25105181.html
+// https://documentation.mapp.com/1.0/en/how-to-send-manual-tracking-requests-page-updates-7240681.html
+
+// 1. page - when a page request is simulated
+// 2. click - for measuring actions that don't cause page load;
+// 3. link - clicking a link that triggers a new page load
+// 4. pageupdate - information on the page changes without a new page load
+type EventType = "page" | "click" | "link" | "pageupdate";
+
+export function useStatistics(eventType: EventType) {
   // If the global wts object doesn't exist, it means we are in dev environment.
   // Here instead of actually tracking we just log the data to the console.
   if (!window.wts) {
