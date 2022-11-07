0b9538570f68b05f78cb76abb5fb7d26ebf5b167
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-11-07 14:48:09 +0100

    Add working version of text handling version 2

---
+++ src/apps/text-handling-v2/textHandlingv2.dev.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useTextV2, withText } from "../../core/utils/text";
+
+export default {
+  title: "Apps / Text Handling v2",
+  argTypes: {
+    "version-2SimpleText": {
+      name: "Version 2 simple text",
+      defaultValue: '{"type":"simple","text":["This is a test"]}',
+      control: { type: "text" }
+    },
+    "version-2PlaceholdersText": {
+      name: "Version 2 placeholders text",
+      defaultValue:
+        '{"type":"simple","text":["This is a text with a @placeholder embedded. Does it work? @result it does!"]}',
+      control: { type: "text" }
+    },
+    "version-2PluralText": {
+      name: "Version 2 plural text",
+      defaultValue:
+        '{"type":"plural","text":["You have 1 material on the waiting list","You have @count materials on the waiting list"]}',
+      control: { type: "text" }
+    }
+  }
+};
+
+const Template = () => {
+  const t2 = useTextV2();
+  const simple = t2("version-2SimpleText");
+  const placeholders = t2("version-2PlaceholdersText", {
+    placeholders: {
+      "@placeholder": "teddy bear",
+      "@result": "Yes"
+    }
+  });
+  const plural1 = t2("version-2PluralText", {
+    count: 1,
+    placeholders: {
+      "@count": "1"
+    }
+  });
+  const plural2 = t2("version-2PluralText", {
+    count: 10,
+    placeholders: {
+      "@count": "10"
+    }
+  });
+  return (
+    <pre>
+      {JSON.stringify({ simple, placeholders, plural1, plural2 }, null, 2)}
+    </pre>
+  );
+};
+
+export const Demo = withText(Template);
--- src/core/utils/text.tsx
+++ src/core/utils/text.tsx
@@ -2,14 +2,125 @@ import { RootState, useSelector } from "../store";
 import { addTextEntries } from "../text.slice";
 import withSuffix from "./withSuffix";
 
+type TextDefinitionTypes = "simple" | "plural";
+type TextDefinition = {
+  type: TextDefinitionTypes;
+  text: string[];
+};
+const constructTextDefinitionFromEntry = (incoming: string): TextDefinition => {
+  const textDefinitionError = "Error in text definition";
+  try {
+    const textDefinition = JSON.parse(incoming);
+
+    if (
+      typeof textDefinition === "object" &&
+      Object.keys(textDefinition).length === 2 &&
+      Object.keys(textDefinition).includes("type") &&
+      Object.keys(textDefinition).includes("text") &&
+      ["simple", "plural"].includes(textDefinition?.type ?? "") &&
+      Array.isArray(textDefinition?.text)
+    ) {
+      const type = textDefinition?.type;
+      const text = textDefinition?.text ?? [];
+
+      if (["simple"].includes(type) && text.length !== 1) {
+        // We deliberately want to write an error to the console:
+        // eslint-disable-next-line no-console
+        console.error(
+          "Simple text definitions must have exactly one text entry"
+        );
+        textDefinition.text = [textDefinitionError];
+      }
+
+      if (["plural"].includes(type) && text.length !== 2) {
+        // We deliberately want to write an error to the console:
+        // eslint-disable-next-line no-console
+        console.error(
+          "Plural text definitions must have exactly two text entries"
+        );
+        textDefinition.text = [textDefinitionError, textDefinitionError];
+      }
+
+      return textDefinition;
+    }
+    // If we were able to parse the incoming string,
+    // but it did not match the expected format,
+    // we make sure to log an error.
+    // eslint-disable-next-line no-console
+    console.error(`Unknown text definition format: ${incoming}`);
+
+    // We do not want to break the app if we are unable to parse the incoming string.
+  } catch (error: unknown) {
+    // Instead we are logging an error to the console.
+    const message = error instanceof Error ? error.message : "Unknown error";
+    // eslint-disable-next-line no-console
+    console.error(
+      `Could not parse incoming text format: ${incoming}. Message: ${message}`
+    );
+  }
+
+  return {
+    type: "simple",
+    text: [textDefinitionError]
+  };
+};
+
 export type UseTextFunction = (key: string) => string;
 export const useText = (): UseTextFunction => {
   const { data } = useSelector((state: RootState) => state.text);
   return (key: string) => data?.[key] ?? key;
 };
 
+export type TextFunctionSimple = () => string;
+export type TextFunctionPlural = (count: number) => string;
+export type TextFunctionPlaceholders = (placeholders: {
+  [key: string]: string;
+}) => string;
+
+const processTexts = (
+  texts: string[],
+  placeholders: Record<string, string>
+) => {
+  return texts.reduce((acc: string[], text: string) => {
+    const newEntry = text.replace(/@\w+/g, (match) => {
+      return placeholders[match] || match;
+    });
+
+    acc.push(newEntry);
+    return acc;
+  }, []);
+};
+
+export const useTextV2 = () => {
+  const { data } = useSelector((state: RootState) => state.text);
+
+  return (
+    key: string,
+    {
+      placeholders,
+      count
+    }: {
+      placeholders?: { [key: string]: string };
+      count?: number;
+    } = {}
+  ) => {
+    const textDefinition = constructTextDefinitionFromEntry(data?.[key] ?? key);
+    const processedTexts = placeholders
+      ? processTexts(textDefinition.text, placeholders)
+      : textDefinition.text;
+
+    switch (textDefinition.type) {
+      case "plural":
+        // If count is 1 we select the first text entry
+        // otherwise we select the second text entry.
+        return processedTexts[1 % (count ?? 1)];
+      case "simple":
+      default:
+        return processedTexts[0];
+    }
+  };
+};
+
 export const withText = <T,>(Component: React.ComponentType<T>) => {
   return withSuffix(Component, "Text", addTextEntries);
 };
-
-export default {};
