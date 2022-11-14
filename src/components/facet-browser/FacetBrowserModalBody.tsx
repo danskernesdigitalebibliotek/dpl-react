a41b9026b9f5dcdbc5e8f91456bd74adafce4451
kasperbirch1 <kasperbirch1@gmail.com>
2022-11-14 09:21:50 +0100

    Change name from hasChosenTerms to hasSelectedTerms
    it's a better and more descriptive name

--- src/components/facet-browser/FacetBrowserModalBody.tsx
+++ src/components/facet-browser/FacetBrowserModalBody.tsx
@@ -40,7 +40,7 @@ const FacetBrowserModalBody: React.FunctionComponent<
         // Remove facets disclosures with no tags
         if (values.length === 0) return null;
 
-        const hasChosenTerms = Boolean(filters[name]);
+        const hasSelectedTerms = Boolean(filters[name]);
 
         return (
           <FacetBrowserDisclosure
@@ -49,7 +49,7 @@ const FacetBrowserModalBody: React.FunctionComponent<
             fullWidth
             removeHeadlinePadding
             title={t(`facet${upperFirst(name)}Text`)}
-            showContent={hasChosenTerms}
+            showContent={hasSelectedTerms}
           >
             <div className="facet-browser__facet-group">
               {values.map((termItem) => {
