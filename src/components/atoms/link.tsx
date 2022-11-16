c9334a2cac5a374b19ccf073638aebf688344704
Adam Antal <adoantal@gmail.com>
2022-11-16 14:40:27 +0100

    Update Link component to optionally be able to track before redirecting
    In order to be able to track the click events that use <a> tags to
    redirect, this component needed to be adjusted to not use href for
    redirecting but instead an onClick handler that tracks and subsequently
    redirects.

--- src/components/atoms/link.tsx
+++ src/components/atoms/link.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { redirectTo } from "../../core/utils/helpers/url";
 
 export interface LinkProps {
   href: URL;
@@ -6,6 +7,7 @@ export interface LinkProps {
   isNewTab?: boolean;
   className?: string;
   id?: string;
+  trackClick?: () => void;
 }
 
 export const Link: React.FC<LinkProps> = ({
@@ -13,15 +15,32 @@ export const Link: React.FC<LinkProps> = ({
   children,
   isNewTab,
   className,
-  id
+  id,
+  trackClick
 }) => {
+  const redirect = () => {
+    redirectTo(href);
+  };
+
   return (
+    // If we want to track this click we have to first NOT set the href attribute
+    // & instead redirect through an onclick handler
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
     <a
       id={id}
-      href={String(href)}
+      // eslint-disable-next-line no-script-url
+      href={trackClick ? "javascript:void(0)" : String(href)}
       target={isNewTab ? "_blank" : ""}
       rel="noreferrer"
       className={className}
+      onClick={
+        trackClick
+          ? () => {
+              trackClick();
+              redirect();
+            }
+          : undefined
+      }
     >
       {children}
     </a>
