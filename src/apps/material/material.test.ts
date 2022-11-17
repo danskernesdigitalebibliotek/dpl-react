1ef564fedc51e4119dd213ba73c4800f8dffb616
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-11-17 09:28:14 +0100

    Reenable other material tests

--- src/apps/material/material.test.ts
+++ src/apps/material/material.test.ts
@@ -1,116 +1,116 @@
 const coverUrlPattern = /^https:\/\/res\.cloudinary\.com\/.*\.(jpg|jpeg|png)$/;
 
 describe("Material", () => {
-  // it("Does the Material have title?", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.get(".text-header-h1").should("be.visible");
-  // });
+  it("Does the Material have title?", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.get(".text-header-h1").should("be.visible");
+  });
 
-  // it("Check that cover has a src", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+  it("Check that cover has a src", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
 
-  //   cy.get("img").should("have.attr", "src").and("match", coverUrlPattern);
-  // });
+    cy.get("img").should("have.attr", "src").and("match", coverUrlPattern);
+  });
 
-  // it("Does the material have favourite buttons?", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.get(".button-favourite").should(
-  //     "have.attr",
-  //     "aria-label",
-  //     "Add to favorites"
-  //   );
-  // });
+  it("Does the material have favourite buttons?", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.get(".button-favourite").should(
+      "have.attr",
+      "aria-label",
+      "Add to favorites"
+    );
+  });
 
-  // it("Does the material have horizontal lines?", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.contains("Nr. 1 in series");
-  //   cy.contains("De syv søstre-serien");
-  // });
+  it("Does the material have horizontal lines?", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.contains("Nr. 1 in series");
+    cy.contains("De syv søstre-serien");
+  });
 
-  // it("Does the material have authors?", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.contains("Lucinda Riley");
-  // });
+  it("Does the material have authors?", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.contains("Lucinda Riley");
+  });
 
-  // it("Does a material have a availibility label", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.contains("bog");
-  //   cy.contains("unavailable");
-  // });
+  it("Does a material have a availibility label", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.contains("bog");
+    cy.contains("unavailable");
+  });
 
-  // it("Open material details", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.get("details").last().click();
-  // });
+  it("Open material details", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.get("details").last().click();
+  });
 
-  // it("Does the material have a editions with a buttton to reserved", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.scrollTo("bottom");
-  //   cy.contains("Editions (7)").click();
-  //   cy.contains("Reserve");
-  // });
+  it("Does the material have a editions with a buttton to reserved", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.scrollTo("bottom");
+    cy.contains("Editions (7)").click();
+    cy.contains("Reserve");
+  });
 
-  // it("Opens modal by clicking on reserver button (reserve book) and close it with the x bottom", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.contains("button:visible", "Reserve bog").click();
-  //   cy.contains("Pick up at");
-  //   cy.contains("Hovedbiblioteket");
-  //   cy.contains("12345678");
-  //   cy.contains("test@test.com");
-  //   cy.get(`[aria-label="Close reservation modal"]`).click({
-  //     multiple: true,
-  //     force: true
-  //   });
-  // });
+  it("Opens modal by clicking on reserver button (reserve book) and close it with the x bottom", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.contains("button:visible", "Reserve bog").click();
+    cy.contains("Pick up at");
+    cy.contains("Hovedbiblioteket");
+    cy.contains("12345678");
+    cy.contains("test@test.com");
+    cy.get(`[aria-label="Close reservation modal"]`).click({
+      multiple: true,
+      force: true
+    });
+  });
 
-  // it("Clicking on Aprove resevation (Godkend reservation and close modal with Ok button)", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/fbi-api.json"
-  //   });
-  //   cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
-  //   cy.contains("button:visible", "Reserve bog").click();
-  //   cy.contains("button:visible", "Approve reservation").click();
-  //   cy.contains("Material is available and reserved for you!");
-  //   cy.contains("You are number 3 in the queue");
-  //   cy.contains("button:visible", "Ok").click();
-  // });
+  it("Clicking on Aprove resevation (Godkend reservation and close modal with Ok button)", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+    cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.contains("button:visible", "Reserve bog").click();
+    cy.contains("button:visible", "Approve reservation").click();
+    cy.contains("Material is available and reserved for you!");
+    cy.contains("You are number 3 in the queue");
+    cy.contains("button:visible", "Ok").click();
+  });
 
   //  periodical test.
   it("Render periodical + change to 2021, nr. 52 + Aprove resevation", () => {
@@ -150,21 +150,21 @@ describe("Material", () => {
   });
 
   //  infomedia test.
-  // it("Render infomedia + Read article + Close modal", () => {
-  //   cy.interceptGraphql({
-  //     operationName: "getMaterial",
-  //     fixtureFilePath: "material/infomedia-fbi-api.json"
-  //   });
-  //   cy.interceptGraphql({
-  //     operationName: "getInfomedia",
-  //     fixtureFilePath: "material/infomedia-article.json"
-  //   });
+  it("Render infomedia + Read article + Close modal", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/infomedia-fbi-api.json"
+    });
+    cy.interceptGraphql({
+      operationName: "getInfomedia",
+      fixtureFilePath: "material/infomedia-article.json"
+    });
 
-  //   cy.visit("/iframe.html?id=apps-material--infomedia&viewMode=story");
-  //   cy.contains("button:visible", "Read article").click();
-  //   cy.contains("h2", "BUTLERENS UTROLIGE HISTORIE");
-  //   cy.get(`[aria-label="Close infomedia modal"]`).click();
-  // });
+    cy.visit("/iframe.html?id=apps-material--infomedia&viewMode=story");
+    cy.contains("button:visible", "Read article").click();
+    cy.contains("h2", "BUTLERENS UTROLIGE HISTORIE");
+    cy.get(`[aria-label="Close infomedia modal"]`).click();
+  });
 
   beforeEach(() => {
     cy.interceptRest({
