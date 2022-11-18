4fd84f0a948b184c745db594799215135a8fc100
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-11-18 13:57:47 +0100

    Add test for zero count in plural texts

--- tests/unit/text.test.tsx
+++ tests/unit/text.test.tsx
@@ -25,7 +25,7 @@ const Wrapper = ({ children }: { children: ReactNode }) => (
   <Provider store={store}> {children} </Provider>
 );
 
-test("Should render expected text strings with placeholders and plural handling", () => {
+test("Should strings with placeholders", () => {
   const { result } = renderHook(() => useText(), { wrapper: Wrapper });
 
   act(() => {
@@ -40,17 +40,33 @@ test("Should render expected text strings with placeholders and plural handling"
         "@result": "yes"
       }
     });
+
+    expect(simpleOutput).toBe("This is a simple test");
+    expect(placeholdersOutput).toBe(
+      "This is a text with a placeholder embedded. Does it work? yes it does!"
+    );
+  });
+});
+
+test("Should handle plural text definitions", () => {
+  const { result } = renderHook(() => useText(), { wrapper: Wrapper });
+
+  act(() => {
+    const pluralTextZeroOutput = result.current("pluralText", {
+      count: 0,
+      placeholders: { "@count": 0 }
+    });
     const pluralTextOneOutput = result.current("pluralText", {
-      count: 1
+      count: 1,
+      placeholders: { "@count": 1 }
     });
     const pluralTextMultipleOutput = result.current("pluralText", {
       count: 10,
       placeholders: { "@count": 10 }
     });
 
-    expect(simpleOutput).toBe("This is a simple test");
-    expect(placeholdersOutput).toBe(
-      "This is a text with a placeholder embedded. Does it work? yes it does!"
+    expect(pluralTextZeroOutput).toBe(
+      "You have 0 materials on the waiting list"
     );
     expect(pluralTextOneOutput).toBe("You have 1 material on the waiting list");
     expect(pluralTextMultipleOutput).toBe(
