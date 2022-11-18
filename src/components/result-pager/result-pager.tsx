b599a7e6bf1e7fa568219e1d97eefeb28b40b148
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-11-18 13:27:05 +0100

    Use new text interpolation in whole result pager
    It was not implemented in the case when all items were shown on a page

--- src/components/result-pager/result-pager.tsx
+++ src/components/result-pager/result-pager.tsx
@@ -12,19 +12,6 @@ function ResultPager({
   hitcount
 }: ResultPagerProps) {
   const t = useText();
-
-  // If all items are visible, the button is not necessary.
-  if (itemsShown === hitcount) {
-    return (
-      <div className="result-pager">
-        <p className="text-small-caption result-pager__title">
-          {/* TODO: interpolate these strings */}
-          {t("showingText")} {hitcount} {t("outOfText")} {hitcount}
-        </p>
-      </div>
-    );
-  }
-
   return (
     <div className="result-pager">
       <p className="text-small-caption result-pager__title">
@@ -32,14 +19,17 @@ function ResultPager({
           placeholders: { "@itemsShown": itemsShown, "@hitcount": hitcount }
         })}
       </p>
-      <button
-        type="button"
-        className="btn-primary btn-outline btn-medium arrow__hover--right-small"
-        onClick={setPageHandler}
-      >
-        {/* TODO: Solve casing in CSS */}
-        {t("showMoreText").toUpperCase()}
-      </button>
+      {/* If all items are not visible yet, we need to show the button. */}
+      {itemsShown !== hitcount && (
+        <button
+          type="button"
+          className="btn-primary btn-outline btn-medium arrow__hover--right-small"
+          onClick={setPageHandler}
+        >
+          {/* TODO: Solve casing in CSS */}
+          {t("showMoreText").toUpperCase()}
+        </button>
+      )}
     </div>
   );
 }
