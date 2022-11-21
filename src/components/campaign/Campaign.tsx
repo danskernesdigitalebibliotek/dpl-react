1f6d4805a4cf75ba8fa6e17c9d113a7f2c0fed2b
Adam Antal <adoantal@gmail.com>
2022-11-21 14:15:53 +0100

    Don't document campaign cms API in the react code in campaign component

--- src/components/campaign/Campaign.tsx
+++ src/components/campaign/Campaign.tsx
@@ -10,9 +10,6 @@ export interface CampaignProps {
   campaignData: CampaignMatchPOST200Data;
 }
 
-// In order to see campaigns in development mode, you will most likely need a
-// browser plugin such as Chrome's "Allow CORS: Access-Control-Allow-Origin"
-// in order to bypass CORS policy for dpl-cms data calls
 const Campaign: FC<CampaignProps> = ({ campaignData }) => {
   const { track } = useStatistics();
   const trackClick = () => {
