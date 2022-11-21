ed9e28c3be06a0638c4d009f698527fc82fc2572
Jeppe Krogh <kjej@aarhus.dk>
2022-11-21 08:06:50 +0100

    added dashboard notification list

--- src/apps/dashboard/dashboard-notification-list/dashboard-notification-list.tsx
+++ src/apps/dashboard/dashboard-notification-list/dashboard-notification-list.tsx
@@ -157,6 +157,12 @@ const DashboardNotificationList: FC<DashboardNotificationListProps> = ({
             notificationLink={new URL(reservationsUrl)}
           />
         )}
+        {/* <DashboardNotification
+              notificationNumber={5}
+              notificationText="test5"
+              notificationColor="neutral"
+              notificationLink={new URL("https://www.google.dk")}
+            /> */}
       </div>
     </div>
   );
