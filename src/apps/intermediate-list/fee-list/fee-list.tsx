cc4f8c4f239f5e9b6e1731008027407487a5ead5
Jeppe Krogh <kjej@aarhus.dk>
2022-11-23 10:05:09 +0100

    added fee list

---
+++ src/apps/intermediate-list/fee-list/fee-list.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { FC } from "react";
+
+export interface FeesListProps {
+  testTest: string;
+}
+
+const FeesList: FC<FeesListProps> = () => {
+  return <>Hello World!</>;
+};
+
+export default FeesList;
