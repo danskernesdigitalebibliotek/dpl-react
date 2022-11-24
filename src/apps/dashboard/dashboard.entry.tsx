bb21416847a7646660faf5a90a34c66862a3bead
Jeppe Krogh <kjej@aarhus.dk>
2022-11-24 11:16:13 +0100

    added GuardedApp to .entry file

--- src/apps/dashboard/dashboard.entry.tsx
+++ src/apps/dashboard/dashboard.entry.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import GuardedApp from "../../components/guarded-app";
 import { withText } from "../../core/utils/text";
 import { withUrls } from "../../core/utils/url";
 
@@ -26,6 +27,10 @@ export interface DashBoardProps {
   reservationsUrl: string;
 }
 
-const MenuEntry: FC<DashBoardProps> = () => <DashBoard />;
+const MenuEntry: FC<DashBoardProps> = () => (
+  <GuardedApp app="dashboard">
+    <DashBoard />
+  </GuardedApp>
+);
 
 export default withUrls(withText(MenuEntry));
