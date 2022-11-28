62d432d959e95297687dd8000ba14c3261fcf5c8
Adam Antal <adoantal@gmail.com>
2022-11-28 16:31:12 +0100

    Use authUrl to redirect material app from infomedia article to login
    This is a test commit to be used in dpl-cms. That's why so many console
    logs.

--- src/components/material/material-buttons/online/MaterialButtonOnlineInfomediaArticle.tsx
+++ src/components/material/material-buttons/online/MaterialButtonOnlineInfomediaArticle.tsx
@@ -1,14 +1,8 @@
 import * as React from "react";
-import { useEffect, FC } from "react";
+import { FC } from "react";
 import { useDispatch } from "react-redux";
 import { openModal } from "../../../../core/modal.slice";
-import {
-  getToken,
-  setToken,
-  TOKEN_LIBRARY_KEY,
-  TOKEN_USER_KEY
-} from "../../../../core/token";
-import { setStatusAuthenticated } from "../../../../core/user.slice";
+import { getToken, TOKEN_USER_KEY } from "../../../../core/token";
 import {
   appendQueryParametersToUrl,
   getCurrentLocation,
@@ -39,51 +33,12 @@ const MaterialButtonOnlineInfomediaArticle: FC<
   const t = useText();
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const CLIENT_ID = process.env.STORYBOOK_CLIENT_ID;
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get("code");
-
-    if (!code || !CLIENT_ID) {
-      return;
-    }
-    const currentLocation = new URL(getCurrentLocation());
-    const REDIRECT_URL = currentLocation.href;
-    console.log({ currentLocation });
-    console.log({ REDIRECT_URL });
-
-    fetch("https://login.bib.dk/oauth/token", {
-      method: "POST",
-      headers: {},
-      body: new URLSearchParams({
-        grant_type: "authorization_code",
-        code,
-        client_id: CLIENT_ID,
-        client_secret: "secret",
-        redirect_uri: REDIRECT_URL
-      })
-    })
-      .then((res) => {
-        console.log(res);
-        return res.json();
-      })
-      .then((res) => {
-        // eslint-disable-next-line camelcase
-        if (!res?.access_token) {
-          throw res;
-        }
-        // We need to make the token available in two contexts:
-        // 1. Subsequent browser reloads. Consequently we set the token into sessionstorage, which are read by preview.js.
-        window.sessionStorage.setItem(TOKEN_USER_KEY, res.access_token);
-        // 2. Current storybook context.
-        setToken(TOKEN_USER_KEY, res.access_token);
-        setToken(TOKEN_LIBRARY_KEY, res.access_token);
-        dispatch(setStatusAuthenticated());
-      });
-  }, [pid, dispatch]);
+  // useEffect(() => {
+  //   // do stuff here upon coming back from teh login site
+  // }, []);
 
   const onClick = () => {
-    const userToken = getToken("user");
+    const userToken = getToken(TOKEN_USER_KEY);
     console.log({ userToken });
     const isUserAnonymous = userIsAnonymous();
     console.log({ isUserAnonymous });
@@ -91,20 +46,20 @@ const MaterialButtonOnlineInfomediaArticle: FC<
     if (userIsAnonymous() || !getToken("user")) {
       console.log("User is anonymous and doesn't have any token");
 
-      // redirect to login with a link back
+      // link back from the login platform - with an open modal id
       const { href: urlToOpenModal } = appendQueryParametersToUrl(
         new URL(getCurrentLocation()),
         {
           modal: infomediaModalId(pid)
         }
       );
-      const CLIENT_ID = process.env.STORYBOOK_CLIENT_ID;
-      const baseLoginUrl = new URL(
-        `https://login.bib.dk/oauth/authorize?response_type=code&client_id=${CLIENT_ID}`
-      );
-      const redirectUrl = appendQueryParametersToUrl(baseLoginUrl, {
+      console.log({ urlToOpenModal });
+      const baseAuthUrl = new URL(t("authUrl"));
+      console.log({ baseAuthUrl });
+      const redirectUrl = appendQueryParametersToUrl(baseAuthUrl, {
         redirect_uri: urlToOpenModal
       });
+      console.log({ redirectUrl });
       redirectTo(redirectUrl);
       return;
     }
