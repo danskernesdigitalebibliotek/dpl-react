41e7233538d28172340f0a9a5f08668e829f6c41
Adam Antal <adoantal@gmail.com>
2022-11-28 12:58:30 +0100

    Try to fetch login token after redirect to infomedia article material
    After we are redirected back from the login platform to the material
    page displaying an infomedia article, we need to be able to fetch the
    user authentification token. This is the first bid for that
    functionality, but it needs to be tested in drupal dpl-cms project.

--- src/components/material/material-buttons/online/MaterialButtonOnlineInfomediaArticle.tsx
+++ src/components/material/material-buttons/online/MaterialButtonOnlineInfomediaArticle.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
-import { FC } from "react";
+import { useEffect, FC } from "react";
 import { useDispatch } from "react-redux";
 import { openModal } from "../../../../core/modal.slice";
+import {
+  setToken,
+  TOKEN_LIBRARY_KEY,
+  TOKEN_USER_KEY
+} from "../../../../core/token";
+import { setStatusAuthenticated } from "../../../../core/user.slice";
 import {
   appendQueryParametersToUrl,
   getCurrentLocation,
@@ -31,6 +37,49 @@ const MaterialButtonOnlineInfomediaArticle: FC<
 }) => {
   const t = useText();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const CLIENT_ID = process.env.STORYBOOK_CLIENT_ID;
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+
+    if (!code || !CLIENT_ID) {
+      return;
+    }
+    const currentLocation = new URL(getCurrentLocation());
+    const REDIRECT_URL = currentLocation.href;
+    console.log({ currentLocation });
+    console.log({ REDIRECT_URL });
+
+    fetch("https://login.bib.dk/oauth/token", {
+      method: "POST",
+      headers: {},
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        code,
+        client_id: CLIENT_ID,
+        client_secret: "secret",
+        redirect_uri: REDIRECT_URL
+      })
+    })
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((res) => {
+        // eslint-disable-next-line camelcase
+        if (!res?.access_token) {
+          throw res;
+        }
+        // We need to make the token available in two contexts:
+        // 1. Subsequent browser reloads. Consequently we set the token into sessionstorage, which are read by preview.js.
+        window.sessionStorage.setItem(TOKEN_USER_KEY, res.access_token);
+        // 2. Current storybook context.
+        setToken(TOKEN_USER_KEY, res.access_token);
+        setToken(TOKEN_LIBRARY_KEY, res.access_token);
+        dispatch(setStatusAuthenticated());
+      });
+  }, [pid, dispatch]);
 
   const onClick = () => {
     if (userIsAnonymous()) {
