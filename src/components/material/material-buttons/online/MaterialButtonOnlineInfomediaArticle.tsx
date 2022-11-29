7973c72b880f8dcce7a7825340ea5137e13ec518
Adam Antal <adoantal@gmail.com>
2022-11-29 16:19:54 +0100

    Use redirectToLoginAndBack helper function for infomeaid article views
    On material page in MaterialButtonOnlineInfomediaArticle component.

--- src/components/material/material-buttons/online/MaterialButtonOnlineInfomediaArticle.tsx
+++ src/components/material/material-buttons/online/MaterialButtonOnlineInfomediaArticle.tsx
@@ -6,7 +6,7 @@ import { getToken, TOKEN_USER_KEY } from "../../../../core/token";
 import {
   appendQueryParametersToUrl,
   getCurrentLocation,
-  redirectTo
+  redirectToLoginAndBack
 } from "../../../../core/utils/helpers/url";
 import { userIsAnonymous } from "../../../../core/utils/helpers/user";
 import { useText } from "../../../../core/utils/text";
@@ -40,28 +40,16 @@ const MaterialButtonOnlineInfomediaArticle: FC<
     // to this page with an open modal.
     const userToken = getToken(TOKEN_USER_KEY);
     if (userIsAnonymous() || !userToken) {
-      // If we are in storybook context just redirect to the login story.
-      if (process && window.top) {
-        const { origin } = new URL(getCurrentLocation());
-        const storybookRedirect = `${origin}/?path=/story/sb-utilities-adgangsplatformen--sign-in`;
-        // We don't use redirectTo() because that would redirect inside the storybook iframe.
-        window.top.location.href = storybookRedirect;
-        return;
-      }
-
-      const { pathname, search } = appendQueryParametersToUrl(
+      const returnUrl = appendQueryParametersToUrl(
         new URL(getCurrentLocation()),
         {
           modal: infomediaModalId(pid)
         }
       );
-      const localPathToOpenModal = `${pathname}${search}`;
-      const baseAuthUrl = authUrl;
-      const redirectUrl = appendQueryParametersToUrl(baseAuthUrl, {
-        "current-path": localPathToOpenModal
-      });
-      trackOnlineView().then(() => {
-        redirectTo(redirectUrl);
+      redirectToLoginAndBack({
+        authUrl,
+        returnUrl,
+        trackingFunction: trackOnlineView
       });
       return;
     }
