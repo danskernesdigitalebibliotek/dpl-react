92d2938606fb6fe69532d12fac15107c05c922f7
Kasper Birch <kasperbirch1@gmail.com>
2022-11-29 16:07:15 +0100

    Combine to DigitalModalSuccess and DigitalModalError to DigitalModalFeedback
    There is no reason to have two components when only texts are the difference

---
+++ src/components/material/digital-modal/DigitalModalFeedback.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useModalButtonHandler } from "../../../core/utils/modal";
+import { useText } from "../../../core/utils/text";
+import ReservationForm from "../../reservation/forms/ReservationForm";
+
+type DigitalModalFeedbackProps = {
+  modalId: string;
+  isError: boolean;
+};
+
+const DigitalModalFeedback: React.FunctionComponent<
+  DigitalModalFeedbackProps
+> = ({ modalId, isError }) => {
+  const t = useText();
+  const { close } = useModalButtonHandler();
+
+  return (
+    <ReservationForm
+      title={
+        isError
+          ? t("orderDigitalCopyErrorTitleText")
+          : t("orderDigitalCopySuccessTitleText")
+      }
+      description={
+        isError
+          ? [t("orderDigitalCopyErrorDescriptionText")]
+          : [t("orderDigitalCopySuccessDescriptionText")]
+      }
+      onSubmit={() => close(modalId)}
+      buttonLabel={
+        isError
+          ? t("orderDigitalCopyErrorButtonText")
+          : t("orderDigitalCopySuccessButtonText")
+      }
+    />
+  );
+};
+
+export default DigitalModalFeedback;
