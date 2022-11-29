0961a6948f6f2cc6091002f017cfc9e149219198
Sine Jespersen <sinejespersen@gmail.com>
2022-11-29 15:40:37 +0100

    5411: all paginations work!

--- src/components/result-pager/use-pager.tsx
+++ src/components/result-pager/use-pager.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ResultPager from "./result-pager";
 
 const usePager = (
@@ -11,21 +11,28 @@ const usePager = (
     hitcount = 0;
   }
 
-  const [itemsShown, setitemsShown] = useState(
+  const [itemsShown, setItemsShown] = useState(
     pageSize >= hitcount ? hitcount : pageSize
   );
   const [page, setPage] = useState<number>(0);
 
   const resetPager = () => {
     setPage(0);
-    setitemsShown(pageSize);
+    setItemsShown(pageSize);
   };
+
+  useEffect(() => {
+    setItemsShown(pageSize >= hitcount ? hitcount : pageSize);
+  }, [hitcount, pageSize]);
 
   const pagehandler = () => {
     const currentPage = page + 1;
     const itemsOnPage = (currentPage + 1) * pageSize;
+    const onLastPage = itemsOnPage > hitcount;
+    // the "itemsOnPage > hitcount"-check is to
+    // To avoid the "showing 10 out of 8"-situation
+    setItemsShown(onLastPage ? hitcount : itemsOnPage);
     setPage(currentPage);
-    setitemsShown(itemsOnPage);
   };
 
   const PagerComponent = hitcount ? (
