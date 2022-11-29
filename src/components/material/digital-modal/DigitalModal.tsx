fd892945a00c4be4d430c71dab731c04fa0bdf08
Kasper Birch <kasperbirch1@gmail.com>
2022-11-29 16:35:20 +0100

    Add IssnId type to ids.ts
    This indication of type is more precise and meaningful for future developers and ensures that it is the same type that is used everywhere

--- src/components/material/digital-modal/DigitalModal.tsx
+++ src/components/material/digital-modal/DigitalModal.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { DigitalArticleService } from "../../../core/dbc-gateway/generated/graphql";
 import { useDplDasDigitalArticleOrderPOST } from "../../../core/dpl-cms/dpl-cms";
 import { useGetPatronInformationByPatronIdV2 } from "../../../core/fbs/fbs";
 import Modal from "../../../core/utils/modal";
 import { useText } from "../../../core/utils/text";
-import { Pid } from "../../../core/utils/types/ids";
+import { IssnId, Pid } from "../../../core/utils/types/ids";
 import DigitalModalBody from "./DigitalModalBody";
 import DigitalModalFeedback from "./DigitalModalFeedback";
 import { createDigitalModalId } from "./helper";
 
 type DigitalModalProps = {
-  digitalArticleIssn: DigitalArticleService["issn"];
+  digitalArticleIssn: IssnId;
   pid: Pid;
 };
 
