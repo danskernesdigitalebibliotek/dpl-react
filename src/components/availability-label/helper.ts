b8107ceb0070a11d4310699df731f5691c792224
Kasper Birch <kasperbirch1@gmail.com>
2022-12-16 10:59:28 +0100

    Add useAvailabilityData + Move logic from AvailabilityLabel to helper.ts
    useAvailabilityData is a custom hook, which checks the availability of a material. It takes an object with three properties: materialType, faustIds, and isbn. 
    
    The hook uses two other custom hooks, useGetAvailabilityV3 and useGetV1ProductsIdentifier, to make requests to APIs and check the availability of the material at different branches and on the online platform. 
    
    The hook returns an object with a boolean property, isAvailable, which represents the availability of the material.
    
    * as it is right now, online materials that are not cost-free will be listed as unavailable

---
+++ src/components/availability-label/helper.ts
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useGetAvailabilityV3 } from "../../core/fbs/fbs";
+import { useGetV1ProductsIdentifier } from "../../core/publizon/publizon";
+import { useConfig } from "../../core/utils/config";
+
+export const onlineMaterialAllowList = [
+  "lydbog (cd-mp3)",
+  "lydbog",
+  "lydbog (net)",
+  "ebog"
+];
+
+export const isMaterialTypeOnline = (materialType: string) =>
+  onlineMaterialAllowList.includes(materialType);
+
+export const useAvailabilityData = ({
+  materialType,
+  faustIds,
+  isbn
+}: {
+  materialType: string;
+  faustIds: string[];
+  isbn: string;
+}) => {
+  const [isAvailable, setIsAvailable] = useState(false);
+  const config = useConfig();
+  const blacklistBranches = config("blacklistedAvailabilityBranchesConfig", {
+    transformer: "stringToArray"
+  });
+
+  const hasIsbn = Boolean(isbn);
+  const isOnline = isMaterialTypeOnline(materialType);
+
+  useGetAvailabilityV3(
+    {
+      recordid: faustIds,
+      ...(blacklistBranches ? { exclude: blacklistBranches } : {})
+    },
+    {
+      query: {
+        // This should only be enabled if the material is NOT an online material type
+        enabled: isOnline === false,
+        onSuccess: (data) => {
+          if (data?.some((item) => item.available)) {
+            setIsAvailable(true);
+          }
+        }
+      }
+    }
+  );
+
+  useGetV1ProductsIdentifier(isbn, {
+    query: {
+      // This should only be enabled if the material is an online material type and the isbn is not empty
+      enabled: isOnline && hasIsbn,
+      onSuccess: (res) => {
+        // Set isAvailable to true if the material is costFree (blue title) ind Publizon
+        if (res?.product?.costFree) {
+          setIsAvailable(true);
+        }
+        // TODO:
+        // Check if the material quota is available if the material is not costFree
+      },
+      onError: (error: unknown) => {
+        if (error instanceof Error) {
+          // 128 is the error code for "Bogen er ikke tilgængelig for udlån"
+          if (error.cause && Number(error.cause) === 128) {
+            setIsAvailable(false);
+          }
+        }
+      }
+    }
+  });
+
+  return { isAvailable };
+};
+
+export default {};
