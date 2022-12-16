4706cf37978ccb3ae616619ac42ccd78c33e5dfd
Adam Antal <adoantal@gmail.com>
2022-12-16 11:27:24 +0100

    Don't render reservation+find on shelf modals inside editions disclosure
    Rendering these on the material page unconditionally allows us to open
    them even if the editions disclosure is closed - which solves the issue
    of having to render modal duplicates.

--- src/apps/material/material.tsx
+++ src/apps/material/material.tsx
@@ -177,28 +177,11 @@ const Material: React.FC<MaterialProps> = ({ wid }) => {
       >
         {manifestations.map((manifestation: Manifestation) => {
           return (
-            <>
-              <MaterialMainfestationItem
-                key={manifestation.pid}
-                manifestation={manifestation}
-                workId={wid}
-              />
-              <FindOnShelfModal
-                manifestations={[manifestation]}
-                workTitles={manifestation.titles.main}
-                authors={manifestation.creators}
-                key={`find-on-shelf-modal-${manifestation.pid}`}
-                selectedPeriodical={selectedPeriodical}
-                setSelectedPeriodical={setSelectedPeriodical}
-              />
-              <ReservationModal
-                mainManifestation={manifestation}
-                parallelManifestations={parallelManifestations}
-                selectedPeriodical={selectedPeriodical}
-                workId={wid}
-                work={work}
-              />
-            </>
+            <MaterialMainfestationItem
+              key={manifestation.pid}
+              manifestation={manifestation}
+              workId={wid}
+            />
           );
         })}
       </Disclosure>
@@ -230,6 +213,25 @@ const Material: React.FC<MaterialProps> = ({ wid }) => {
           />
         </DisclosureControllable>
       )}
+      {manifestations.map((manifestation) => (
+        <>
+          <ReservationModal
+            mainManifestation={manifestation}
+            parallelManifestations={parallelManifestations}
+            selectedPeriodical={selectedPeriodical}
+            workId={wid}
+            work={work}
+          />
+          <FindOnShelfModal
+            manifestations={[manifestation]}
+            workTitles={manifestation.titles.main}
+            authors={manifestation.creators}
+            key={`find-on-shelf-modal-${manifestation.pid}`}
+            selectedPeriodical={selectedPeriodical}
+            setSelectedPeriodical={setSelectedPeriodical}
+          />
+        </>
+      ))}
       {currentManifestation && (
         <>
           <FindOnShelfModal
