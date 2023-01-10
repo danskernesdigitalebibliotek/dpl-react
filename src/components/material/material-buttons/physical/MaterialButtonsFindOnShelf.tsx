ae9aed5fb6f83e2497b03d8e24e5c46070c29279
Adam Antal <adoantal@gmail.com>
2023-01-10 10:12:49 +0100

    Remove obsolete availability data call in MaterialButtonsFindOnShelf
    We no longer use this data for anything within the component or its
    children.

--- src/components/material/material-buttons/physical/MaterialButtonsFindOnShelf.tsx
+++ src/components/material/material-buttons/physical/MaterialButtonsFindOnShelf.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { FC } from "react";
-import { useGetAvailabilityV3 } from "../../../../core/fbs/fbs";
 import { useModalButtonHandler } from "../../../../core/utils/modal";
 import { useText } from "../../../../core/utils/text";
 import { ButtonSize } from "../../../../core/utils/types/button";
@@ -21,25 +20,15 @@ const MaterialButtonsFindOnShelf: FC<MaterialButtonsFindOnShelfProps> = ({
 }) => {
   const t = useText();
   const { open } = useModalButtonHandler();
-  const { data, isLoading, isError } = useGetAvailabilityV3({
-    recordid: faustIds
-  });
-
   const onClick = () => {
     open(findOnShelfModalId(faustIds[0]));
   };
-
   // If element is currently focused on, we would like to let users open it using enter
   const onKeyUp = (key: string) => {
     if (key === "Enter") {
       onClick();
     }
   };
-
-  if (!data || isError || isLoading) {
-    // TODO: handle error here once we handle errors
-    return null;
-  }
 
   if (size !== "small") {
     return (
