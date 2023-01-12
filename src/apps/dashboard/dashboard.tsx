25fe2114b41576351f768f4c091decbbf11a9590
Jeppe Krogh <kjej@aarhus.dk>
2023-01-12 15:32:07 +0100

    added stillinqueue modal

--- src/apps/dashboard/dashboard.tsx
+++ src/apps/dashboard/dashboard.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import DashboardFees from "./dashboard-fees/dashboard-fees";
 import DashboardNotificationList from "./dashboard-notification-list/dashboard-notification-list";
 import { useText } from "../../core/utils/text";
+import StillInQueueModal from "./modal/still-in-queue-modal";
 
 const DashBoard: FC = () => {
   const t = useText();
@@ -10,6 +11,9 @@ const DashBoard: FC = () => {
       <h1>{t("yourProfileText")}</h1>
       <DashboardFees />
       <DashboardNotificationList />
+      <StillInQueueModal modalId="HelloWorld123">
+        <h3>Hello WOrld!</h3>
+      </StillInQueueModal>
     </>
   );
 };
---
+++ src/apps/dashboard/modal/still-in-queue-modal.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { FC, ReactNode } from "react";
+import Modal from "../../../core/utils/modal";
+import { useText } from "../../../core/utils/text";
+
+export interface StillInQueueModalProps {
+  modalId?: string;
+  children: ReactNode;
+}
+
+const StillInQueueModal: FC<StillInQueueModalProps> = ({
+  modalId,
+  children
+}) => {
+  const t = useText();
+  if (!modalId) {
+    return null;
+  }
+  return (
+    <Modal
+      modalId={modalId}
+      classNames="modal-details"
+      closeModalAriaLabelText={t("materialDetailsCloseModalAriaLabelText")}
+      screenReaderModalDescriptionText={t(
+        "materialDetailsModalAriaDescriptionText"
+      )}
+    >
+      {children}
+    </Modal>
+  );
+};
+
+export default StillInQueueModal;
