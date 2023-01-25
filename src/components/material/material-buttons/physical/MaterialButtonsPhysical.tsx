2be1fdaf4b7ca1f4edffaa307a99efd3af9d77e5
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-01-25 09:12:12 +0100

    Add TODO

--- src/components/material/material-buttons/physical/MaterialButtonsPhysical.tsx
+++ src/components/material/material-buttons/physical/MaterialButtonsPhysical.tsx
@@ -45,6 +45,7 @@ const MaterialButtonsPhysical: React.FC<MaterialButtonsPhysicalProps> = ({
     return <MaterialButtonUserBlocked size={size} />;
   }
 
+  // TODO: Investigate if we could use UseReservableManifestations() instead.
   if (!areAnyReservable(data)) {
     return <MaterialButtonCantReserve size={size} />;
   }
