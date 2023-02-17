5b3744f756c1ec2be556ea95bb67994d54905aa2
Kasper Birch <kasperbirch1@gmail.com>
2023-02-17 14:56:39 +0100

    Add test for reservation of fiction/nonfiction materials
    This is to show that 'handleFictionOrNonFictionReservation' works as it supposed to.

---
+++ src/components/reservation/reservation.test.ts
@@ -0,0 +1,97 @@
+const coverUrlPattern = /^https:\/\/res\.cloudinary\.com\/.*\.(jpg|jpeg|png)$/;
+
+describe("Reservation", () => {
+  beforeEach(() => {
+    cy.interceptRest({
+      aliasName: "user",
+      url: "**/agencyid/patrons/patronid/v2",
+      fixtureFilePath: "material/user.json"
+    });
+    cy.interceptRest({
+      aliasName: "holdings",
+      url: "**/agencyid/catalog/holdings/**",
+      fixtureFilePath: "material/holdings.json"
+    });
+    cy.interceptRest({
+      aliasName: "branches",
+      url: "**/agencyid/branches",
+      fixtureFilePath: "material/branches.json"
+    });
+    // Intercept covers.
+    cy.intercept(
+      {
+        url: coverUrlPattern
+      },
+      {
+        fixture: "images/cover.jpg"
+      }
+    );
+    cy.interceptRest({
+      aliasName: "Cover",
+      url: "**/api/v2/covers?**",
+      fixtureFilePath: "cover.json"
+    });
+    // Intercept like button
+    cy.intercept("HEAD", "**/list/default/**", {
+      statusCode: 404
+    }).as("Favorite list service");
+    // Intercept url "translation".
+    cy.interceptRest({
+      aliasName: "UrlProxy",
+      url: "**/dpl-url-proxy?url=**",
+      fixtureFilePath: "material/dpl-url-proxy.json"
+    });
+  });
+
+  it("Renders a reservation modal for fiction", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/fbi-api.json"
+    });
+
+    cy.interceptRest({
+      aliasName: "Availability",
+      url: "**/availability/v3?recordid=**",
+      fixtureFilePath: "material/availability.json"
+    });
+
+    cy.visit("/iframe.html?id=apps-material--default&type=bog");
+
+    cy.getBySel("material-header-buttons-physical")
+      .should("be.visible")
+      .and("contain", "Reserve bog")
+      .click();
+
+    cy.getBySel("reservation-modal-list-item-text").should(
+      "contain",
+      "First available edition"
+    );
+  });
+
+  it("Renders a reservation modal for nonfiction", () => {
+    cy.interceptGraphql({
+      operationName: "getMaterial",
+      fixtureFilePath: "material/nonfiction-fbi-api.json"
+    });
+
+    cy.interceptRest({
+      aliasName: "Availability",
+      url: "**/availability/v3?recordid=**",
+      fixtureFilePath: "material/nonfiction-availability.json"
+    });
+
+    cy.visit("/iframe.html?id=apps-material--turen-gar-til-rom&type=bog");
+
+    cy.getBySel("material-header-buttons-physical")
+      .should("be.visible")
+      .and("contain", "Reserve bog")
+      .click();
+
+    cy.getBySel("reservation-modal-list-item-text").should(
+      "contain",
+      "29. udgave, 2021"
+    );
+  });
+});
+
+export default {};
