0f64d51ef7993c20e0d6980f4e4e48a6d18c6f61
Adam Antal <adoantal@gmail.com>
2023-02-20 13:47:39 +0100

    Validate whether external url for online material buttons is valid

--- src/components/material/material-buttons/online/MaterialButtonsOnline.tsx
+++ src/components/material/material-buttons/online/MaterialButtonsOnline.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import { AccessUrl } from "../../../../core/dbc-gateway/generated/graphql";
 import { statistics } from "../../../../core/statistics/statistics";
 import { useStatistics } from "../../../../core/statistics/useStatistics";
+import { isUrlValid } from "../../../../core/utils/helpers/url";
 import { ButtonSize } from "../../../../core/utils/types/button";
 import { Manifestation } from "../../../../core/utils/types/entities";
 import { WorkId } from "../../../../core/utils/types/ids";
@@ -45,8 +46,12 @@ const MaterialButtonsOnline: FC<MaterialButtonsOnlineProps> = ({
       url: externalUrl,
       loginRequired
     } = accessElement as AccessUrl;
-    // TODO:  We have experienced that externalUrl is not always valid.
-    // We should use isUrlValid helper function to check is it is valid.
+
+    //  We have experienced that externalUrl is not always valid.
+    if (!isUrlValid(externalUrl)) {
+      return null;
+    }
+
     return (
       <MaterialButtonOnlineExternal
         loginRequired={loginRequired}
