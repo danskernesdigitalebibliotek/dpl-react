5c17bb704534bf41564fc082e68db26546cde0f1
Adam Antal <adoantal@gmail.com>
2023-02-23 11:25:42 +0100

    Scroll down before checking user blocked button in material buttons test
    It isn't rendered before it enters the viewport.

--- src/components/material/material-buttons/material-buttons.test.ts
+++ src/components/material/material-buttons/material-buttons.test.ts
@@ -138,6 +138,7 @@ describe("Material buttons", () => {
       fixtureFilePath: "material/user-blocked.json"
     });
     cy.visit("/iframe.html?id=apps-material--default&viewMode=story&type=bog");
+    cy.scrollTo("bottom");
     cy.getBySel("material-header-buttons-physical-user-blocked")
       .should("be.visible")
       .and("contain", "Reserve")
