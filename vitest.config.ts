58b1e0549a8068ef7be40a4340e1247b68ba3210
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-02-27 14:54:52 +0100

    Revert "Move Vitest unit tests into src"
    This reverts commit c1a0bfa3af4cf65721ea61c3e478f9aad6abff9c.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,12 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "happy-dom",
-    include: ["./src/tests/unit/*.test.(ts|tsx)"],
-    coverage: {
-      all: true,
-      provider: "istanbul",
-      reporter: ["clover"],
-      include: ["src/**/*.{ts,tsx}"]
-    }
+    include: ["./tests/unit/*.test.(ts|tsx)"]
   }
 });
