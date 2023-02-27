28e5b79d568df874a5ac4078e216a8d7084bd43b
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-02-27 14:22:22 +0100

    Document reason why somme http errors are critical

--- src/core/fbs/mutator/FbsServiceHttpError.ts
+++ src/core/fbs/mutator/FbsServiceHttpError.ts
@@ -1,5 +1,7 @@
 import FetcherCriticalHttpError from "../../fetchers/FetcherCriticalHttpError";
 
+// This HttpError is critical because data such as availability of materials
+// and managing user data is crucial for the functioning of the application.
 export default class FbsServiceHttpError<
   ContextType
 > extends FetcherCriticalHttpError<ContextType> {
--- src/core/publizon/mutator/PublizonServiceHttpError.ts
+++ src/core/publizon/mutator/PublizonServiceHttpError.ts
@@ -1,5 +1,7 @@
 import FetcherCriticalHttpError from "../../fetchers/FetcherCriticalHttpError";
 
+// This HttpError is critical because we want to inform the user that something went wrong
+// while ordering a digitial article.
 export default class PublizonServiceHttpError<
   ContextType
 > extends FetcherCriticalHttpError<ContextType> {
