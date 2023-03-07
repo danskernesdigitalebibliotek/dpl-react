fc3861e6c14e27e0a51beccb65eeb3b0e3eecb50
Sine Jespersen <sinejespersen@gmail.com>
2023-03-07 12:13:47 +0100

    5616: add default to empty list data-cy

--- src/components/empty-list/empty-list.tsx
+++ src/components/empty-list/empty-list.tsx
@@ -2,10 +2,13 @@ import React, { FC } from "react";
 
 interface EmptyListProps {
   emptyListText: string;
-  dataCy?: string;
+  dataCy: string;
 }
 
-const EmptyList: FC<EmptyListProps> = ({ emptyListText, dataCy }) => {
+const EmptyList: FC<EmptyListProps> = ({
+  emptyListText,
+  dataCy = "empty-list"
+}) => {
   return (
     <div data-cy={dataCy} className="dpl-list-empty mt-24">
       {emptyListText}
