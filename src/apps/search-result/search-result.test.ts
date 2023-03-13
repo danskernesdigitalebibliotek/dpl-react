0d712a95a272ef294654ba4769ccf0230e7ef16c
Adam Antal <adoantal@gmail.com>
2023-03-13 14:55:58 +0100

    Use opposite quotes in the search result header

--- src/apps/search-result/search-result.test.ts
+++ src/apps/search-result/search-result.test.ts
@@ -10,7 +10,7 @@ describe("Search Result", () => {
   it("Renders search title", () => {
     cy.getBySel("search-result-header")
       .should("be.visible")
-      .and("contain", "Showing results for “harry“ (722)");
+      .and("contain", "Showing results for “harry” (722)");
   });
 
   it("Renders all the search results", () => {
