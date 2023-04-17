4155dba383358b5f224830ed55a8a1f108c555e4
Sine Jespersen <sinejespersen@gmail.com>
2023-04-17 09:28:41 +0200

    C;M-583: remove code that causes inf loop

--- src/apps/intermediate-list/intermediate-list.tsx
+++ src/apps/intermediate-list/intermediate-list.tsx
@@ -50,19 +50,6 @@ const IntermedateList: FC = () => {
     [fbsFees, open]
   );
 
-  // Todo fix this
-  // useEffect(() => {
-  //   const modalUrlParam = getUrlQueryParam("modal");
-  //   // If there is a query param with the due date, a modal should be opened
-  //   if (modalUrlParam) {
-  //     const faustId = faustIdModalQueryParam(modalUrlParam);
-  //     if (faustId) {
-  //       setFeeDetailsModalId(modalIdsConf.feeDetails + faustId);
-  //       openDetailsModalClickEvent(faustId);
-  //     }
-  //   }
-  // }, [openDetailsModalClickEvent]);
-
   useEffect(() => {
     if (fbsFees) {
       const feesPrePaymentChange = getFeesInRelationToPaymentChangeDate(
