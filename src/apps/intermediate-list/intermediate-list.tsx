02ca0e525be72456e619c21a884fe6fadba64070
Sine Jespersen <sinejespersen@gmail.com>
2023-04-18 11:25:46 +0200

    remove potential endless loop

--- src/apps/intermediate-list/intermediate-list.tsx
+++ src/apps/intermediate-list/intermediate-list.tsx
@@ -50,17 +50,17 @@ const IntermedateList: FC = () => {
     [fbsFees, open]
   );
 
-  useEffect(() => {
-    const modalUrlParam = getUrlQueryParam("modal");
-    // If there is a query param with the due date, a modal should be opened
-    if (modalUrlParam) {
-      const faustId = faustIdModalQueryParam(modalUrlParam);
-      if (faustId) {
-        setFeeDetailsModalId(modalIdsConf.feeDetails + faustId);
-        openDetailsModalClickEvent(faustId);
-      }
-    }
-  }, [openDetailsModalClickEvent]);
+  // useEffect(() => {
+  //   const modalUrlParam = getUrlQueryParam("modal");
+  //   // If there is a query param with the due date, a modal should be opened
+  //   if (modalUrlParam) {
+  //     const faustId = faustIdModalQueryParam(modalUrlParam);
+  //     if (faustId) {
+  //       setFeeDetailsModalId(modalIdsConf.feeDetails + faustId);
+  //       openDetailsModalClickEvent(faustId);
+  //     }
+  //   }
+  // }, [openDetailsModalClickEvent]);
 
   useEffect(() => {
     if (fbsFees) {
