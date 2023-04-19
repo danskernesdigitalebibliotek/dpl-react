8aca66fc46f676dfbefaeedc5061bdc77d4b2a14
Kasper Birch <kasperbirch1@gmail.com>
2023-04-19 13:32:09 +0200

    Don't open ReservationModal modal if the user is blocked.
    When an unauthenticated user clicks on the Reserve button, the user is sent to the login platform. The user is then sent back with a URL containing `&modal=reservation-modal-********`, which opens the modal. This is not desired when a user is blocked, therefore I have added a check `isUserAllowed` before the modal is rendered.
    
    In addition, I have added `userIsBlocked` and `userIsAllowed` to make the code more readable and maintainable.

--- src/components/material/material-buttons/physical/MaterialButtonsPhysical.tsx
+++ src/components/material/material-buttons/physical/MaterialButtonsPhysical.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { isEmpty } from "lodash";
 import {
   getAllFaustIds,
   getManifestationType
 } from "../../../../core/utils/helpers/general";
 import { useGetPatronInformationByPatronIdV2 } from "../../../../core/fbs/fbs";
-import { userIsAnonymous } from "../../../../core/utils/helpers/user";
+import {
+  userIsAllowed,
+  userIsAnonymous,
+  userIsBlocked
+} from "../../../../core/utils/helpers/user";
 import { ButtonSize } from "../../../../core/utils/types/button";
 import { Manifestation } from "../../../../core/utils/types/entities";
 import UseReservableManifestations from "../../../../core/utils/UseReservableManifestations";
@@ -41,14 +44,14 @@ const MaterialButtonsPhysical: React.FC<MaterialButtonsPhysicalProps> = ({
     return <MaterialButtonCantReserve size={size} />;
   }
 
-  if (!isEmpty(userData?.patron?.blockStatus)) {
+  if (userIsBlocked(userData)) {
     return <MaterialButtonUserBlocked size={size} dataCy={dataCy} />;
   }
 
   // We show the reservation button if the user isn't logged in or isn't blocked.
   // In the former case there there's no way to see if they're blocked, so we
   // redirect anonymous user to the login page.
-  if (!userData || isEmpty(userData?.patron?.blockStatus)) {
+  if (!userData || userIsAllowed(userData)) {
     return (
       <MaterialButtonReservePhysical
         dataCy={dataCy}
--- src/core/utils/helpers/user.ts
+++ src/core/utils/helpers/user.ts
@@ -1,6 +1,23 @@
+import { isEmpty } from "lodash";
+import { AuthenticatedPatronV6 } from "../../fbs/model";
 import { hasToken } from "../../token";
 
 export const userIsAnonymous = () => {
   return !hasToken("user");
 };
+
+export const userIsBlocked = (
+  userData: AuthenticatedPatronV6 | null | undefined
+) => {
+  if (!userData) return false;
+  return !isEmpty(userData.patron?.blockStatus);
+};
+
+export const userIsAllowed = (
+  userData: AuthenticatedPatronV6 | null | undefined
+) => {
+  if (!userData) return false;
+  return isEmpty(userData.patron?.blockStatus);
+};
+
 export default {};
