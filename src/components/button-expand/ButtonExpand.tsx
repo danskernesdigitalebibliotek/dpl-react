6b145708d462fec904f3b58a568d7b5f4d17d500
Kasper Birch <kasperbirch1@gmail.com>
2023-04-24 14:58:14 +0200

    add translation for button expand

--- src/components/button-expand/ButtonExpand.tsx
+++ src/components/button-expand/ButtonExpand.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import React, { FC } from "react";
 import ExpandMoreIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/ExpandMore.svg";
+import { useText } from "../../core/utils/text";
 
 export type ButtonExpandProps = {
   showMore: boolean;
@@ -8,12 +9,13 @@ export type ButtonExpandProps = {
 };
 
 const ButtonExpand: FC<ButtonExpandProps> = ({ showMore, setShowMore }) => {
+  const t = useText();
   return (
     <button
       className="button-expand"
       type="button"
       onClick={() => setShowMore(!showMore)}
-      aria-label="Expand More"
+      aria-label={t("expandMoreText")}
     >
       <img
         className={clsx("button-expand__image", {
