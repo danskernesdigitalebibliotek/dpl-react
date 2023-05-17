c4e53037afbe56f136cf41ae2e8e1d580da50d21
Kasper Birch <kasperbirch1@gmail.com>
2023-05-17 15:53:35 +0200

    Add possibility to filter on `workTypes` in `AutosuggestCategory`
    Before it was only possible to create a search with `materialTypes` in `AutosuggestCategory. 
    
    - rename FacetMaterialType to AutosuggestCategory
    - add addFilterFromUrlParamListener in useFilterHandler()
    - add missing types
    
    
    Fix so the user can now use the mouse to navigate to the search page with filters from in `AutosuggestCategory`

--- src/core/utils/types/material-type.ts
+++ src/core/utils/types/material-type.ts
@@ -1,19 +1,3 @@
-export const enum FacetMaterialType {
-  book = "bog",
-  ebook = "e-bog",
-  movie = "film",
-  audioBook = "lydbog (net)",
-  audioBookGeneric = "lydbog",
-  music = "node",
-  game = "playstation 5",
-  animatedSeries = "tegneserie",
-  newspaperArticle = "tidsskriftsartikel",
-  earticle = "artikel",
-  boardGame = "spil",
-  cdRom = "cd",
-  magazine = "tidsskrift"
-}
-
 export const enum ManifestationMaterialType {
   book = "bog",
   ebook = "ebog",
@@ -29,5 +13,23 @@ export const enum ManifestationMaterialType {
   cdRom = "cd",
   magazine = "tidsskrift"
 }
+
+export const enum AutosuggestCategory {
+  book = "bog",
+  ebook = "e-bog",
+  movie = "Film",
+  audioBook = "lydbog (online)",
+  music = "Musik",
+  game = "Spil",
+  animatedSeries = "tegneserie"
+}
+
+export type AutosuggestCategoryFacet = "materialTypes" | "workTypes";
+
+export type AutosuggestCategoryList = {
+  render: string;
+  term: AutosuggestCategory;
+  facet: AutosuggestCategoryFacet;
+};
 
 export default {};
