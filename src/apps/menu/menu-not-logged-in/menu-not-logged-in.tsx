c14d9f78a0149987c6b3fa928a29339dec9b4a75
Sine Jespersen <sinejespersen@gmail.com>
2023-05-17 10:34:47 +0200

    5715: use nav li ul for menu

--- src/apps/menu/menu-not-logged-in/menu-not-logged-in.tsx
+++ src/apps/menu/menu-not-logged-in/menu-not-logged-in.tsx
@@ -2,7 +2,6 @@ import React, { FC } from "react";
 import { useUrls } from "../../../core/utils/url";
 import { useText } from "../../../core/utils/text";
 import Link from "../../../components/atoms/links/Link";
-import { Button } from "../../../components/Buttons/Button";
 import Modal from "../../../core/utils/modal";
 import { getModalIds } from "../../../core/utils/helpers/general";
 
@@ -21,26 +20,19 @@ const MenuNotLoggedInContent: FC = () => {
       )}
       isSlider
     >
-      <div className="modal-login">
-        <div className="modal-login__container">
-          <Link href={menuLoginUrl}>
-            <Button
-              label={t("menuLoginText")}
-              buttonType="none"
-              disabled={false}
-              collapsible={false}
-              size="large"
-              variant="filled"
-              classNames="btn-primary btn-filled btn-large arrow__hover--right-small"
-            />
-          </Link>
-          <Link
-            href={menuSignUpUrl}
-            className="link-tag color-secondary-gray modal-login__btn-create-profile"
-          >
-            {t("menuSignUpText")}
-          </Link>
-        </div>
+      <div className="modal-login modal-login--anonymous">
+        <Link
+          href={menuLoginUrl}
+          className="btn-primary btn-filled btn-large arrow__hover--right-small"
+        >
+          {t("menuLoginText")}
+        </Link>
+        <Link
+          href={menuSignUpUrl}
+          className="mt-32 link-tag color-secondary-gray modal-login__btn-create-profile"
+        >
+          {t("menuSignUpText")}
+        </Link>
       </div>
     </Modal>
   );
