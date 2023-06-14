ecbc71ce072b67953b15f904c60653d4d8e1d9f1
Adam Antal <adoantal@gmail.com>
2023-06-14 11:11:20 +0200

    Add new entry params for search header app for advanced search
    advancedSearchSubmitButtonText
    advancedSearchMinimalLengthErrorText
    advancedSearchExplanationText
    searchHeaderAdvancedSearchIconText
    advancedSearchModalHeaderText
    advancedSearchModalScreenReaderModalDescriptionText
    advancedSearchModalCloseModalAriaLabelText

--- src/apps/search-header/search-header.dev.tsx
+++ src/apps/search-header/search-header.dev.tsx
@@ -56,6 +56,32 @@ export default {
       defaultValue: "/search",
       control: { type: "text" }
     },
+    advancedSearchUrl: {
+      name: "Advanced search url",
+      defaultValue: "/advanced-search",
+      control: { type: "text" }
+    },
+    advancedSearchSubmitButtonText: {
+      name: "Advanced search submit button text",
+      defaultValue: "Submit CQL search query",
+      control: { type: "text" }
+    },
+    advancedSearchMinimalLengthErrorText: {
+      name: "Advanced search minimal query length error",
+      defaultValue: "The search query needs to be at least 3 characters long.",
+      control: { type: "text" }
+    },
+    advancedSearchExplanationText: {
+      name: "Advanced search explanation text",
+      defaultValue:
+        "I am some text explaining advanced search. It is not a normal search, so watch out folks.",
+      control: { type: "text" }
+    },
+    searchHeaderAdvancedSearchIconText: {
+      name: "Search header advanced search icon label",
+      defaultValue: "Advanced search",
+      control: { type: "text" }
+    },
     materialUrl: {
       name: "Base material page url",
       defaultValue: "/work/:workid",
@@ -109,6 +135,22 @@ export default {
     searchNoValidCharactersErrorText: {
       name: "Search non-whitespace character error",
       defaultValue: "Input must contain at least one non-whitespace character.",
+      control: { type: "text" }
+    },
+    advancedSearchModalHeaderText: {
+      name: "Advanced search modal header",
+      defaultValue: "Advanced search",
+      control: { type: "text" }
+    },
+    advancedSearchModalScreenReaderModalDescriptionText: {
+      name: "Advanced search modal screen reader description",
+      defaultValue:
+        "Advanced search lets you use CQL language to query our database",
+      control: { type: "text" }
+    },
+    advancedSearchModalCloseModalAriaLabelText: {
+      name: "Advanced search modal - close modal",
+      defaultValue: "Close the advanced search",
       control: { type: "text" }
     }
   }
