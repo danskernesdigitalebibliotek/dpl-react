caeb0dbadd5d842fc57b12fa59b4f7d9f44bcd93
Kasper Birch <kasperbirch1@gmail.com>
2023-06-27 13:06:17 +0200

    Invoke isAnonymous in console.log

--- src/apps/material/material.tsx
+++ src/apps/material/material.tsx
@@ -150,7 +150,7 @@ const Material: React.FC<MaterialProps> = ({ wid }) => {
   // Get disclosure URL parameter from the current URL to see if it should be open.
   const shouldOpenReviewDisclosure = !!getUrlQueryParam("disclosure");
 
-  console.log("🚀 ~ file: material.tsx ~ isAnonymous:", isAnonymous);
+  console.log("🚀 ~ file: material.tsx ~ isAnonymous:", isAnonymous());
 
   return (
     <section className="material-page">
