3f5514db2d63f2ec934cd8ea2c67e263fdf31b4a
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-07-13 16:08:05 +0200

    Fix weird availability-label click behaviour
    Solved by ignoring all other specific material types
    on manifestations except the first one.
    This behaviour has to be double checked with DDF.

---
+++ src/apps/material/__vitest_data__/helper.ts
@@ -0,0 +1,353 @@
+import { Manifestation } from "../../../core/utils/types/entities";
+
+export default {
+  divideManifestationsByMaterialType: {
+    manifestations: [
+      {
+        pid: "870970-basis:44504928",
+        genreAndForm: ["noveller"],
+        source: ["Bibliotekskatalog"],
+        languages: {
+          main: [
+            {
+              display: "engelsk",
+              isoCode: "eng"
+            }
+          ]
+        },
+        titles: {
+          main: ["I, Robot (Ved Tricia Reilly)"],
+          original: []
+        },
+        fictionNonfiction: {
+          display: "skønlitteratur",
+          code: "FICTION"
+        },
+        materialTypes: [
+          {
+            specific: "lydoptagelse (cd)"
+          },
+          {
+            specific: "bog"
+          }
+        ],
+        creators: [
+          {
+            display: "Isaac Asimov",
+            __typename: "Person"
+          },
+          {
+            display: "Tricia Reilly",
+            __typename: "Person"
+          }
+        ],
+        publisher: ["Macmillan"],
+        identifiers: [
+          {
+            value: "9780230026810"
+          },
+          {
+            value: "9780230026827"
+          }
+        ],
+        contributors: [],
+        edition: {
+          summary: "2008",
+          publicationYear: {
+            display: "2008"
+          }
+        },
+        dateFirstEdition: {
+          display: "1950",
+          year: 1950
+        },
+        audience: {
+          generalAudience: []
+        },
+        physicalDescriptions: [
+          {
+            numberOfPages: 95,
+            playingTime: "2t., 31 min."
+          }
+        ],
+        accessTypes: [
+          {
+            code: "PHYSICAL"
+          }
+        ],
+        access: [
+          {
+            __typename: "InterLibraryLoan",
+            loanIsPossible: true
+          }
+        ],
+        shelfmark: {
+          postfix: "Asimov",
+          shelfmark: "83.8"
+        },
+        workYear: {
+          year: 1950
+        }
+      },
+      {
+        pid: "150061-ebog:ODN0000039136",
+        genreAndForm: [],
+        source: ["eReolen Global"],
+        languages: {
+          main: [
+            {
+              display: "engelsk",
+              isoCode: "eng"
+            }
+          ]
+        },
+        titles: {
+          main: ["I, Robot"],
+          original: []
+        },
+        fictionNonfiction: {
+          display: "skønlitteratur",
+          code: "FICTION"
+        },
+        materialTypes: [
+          {
+            specific: "e-bog"
+          }
+        ],
+        creators: [
+          {
+            display: "Isaac Asimov",
+            __typename: "Person"
+          }
+        ],
+        publisher: [],
+        identifiers: [
+          {
+            value: "9780553900330"
+          }
+        ],
+        contributors: [],
+        edition: {
+          summary: "2004",
+          publicationYear: {
+            display: "2004"
+          }
+        },
+        dateFirstEdition: null,
+        audience: {
+          generalAudience: [
+            "Text Difficulty 3 - Text Difficulty 5",
+            "UG/Upper grades (9th-12)",
+            "820. Lexile",
+            "6.1. ATOS Level"
+          ]
+        },
+        physicalDescriptions: [
+          {
+            numberOfPages: null,
+            playingTime: null
+          }
+        ],
+        accessTypes: [
+          {
+            code: "ONLINE"
+          }
+        ],
+        access: [
+          {
+            __typename: "AccessUrl",
+            origin: "link.overdrive.com",
+            url: "http://link.overdrive.com/?websiteID=100515&titleID=39136",
+            loginRequired: false
+          },
+          {
+            __typename: "AccessUrl",
+            origin: "samples.overdrive.com",
+            url: "https://samples.overdrive.com/?crid=9AB7D235-9D58-4180-8DF3-57A4A60CD51E&.epub-sample.overdrive.com",
+            loginRequired: false
+          },
+          {
+            __typename: "AccessUrl",
+            origin: "img1.od-cdn.com",
+            url: "https://img1.od-cdn.com/ImageType-100/0111-1/%7B9AB7D235-9D58-4180-8DF3-57A4A60CD51E%7DImg100.jpg",
+            loginRequired: false
+          },
+          {
+            __typename: "AccessUrl",
+            origin: "img1.od-cdn.com",
+            url: "https://img1.od-cdn.com/ImageType-200/0111-1/%7B9AB7D235-9D58-4180-8DF3-57A4A60CD51E%7DImg200.jpg",
+            loginRequired: false
+          }
+        ],
+        shelfmark: null,
+        workYear: null
+      },
+      {
+        pid: "150061-netlydbog:ODN0000105225",
+        genreAndForm: [],
+        source: ["eReolen Global"],
+        languages: {
+          main: [
+            {
+              display: "engelsk",
+              isoCode: "eng"
+            }
+          ]
+        },
+        titles: {
+          main: ["I, Robot"],
+          original: []
+        },
+        fictionNonfiction: {
+          display: "vides ikke",
+          code: "NOT_SPECIFIED"
+        },
+        materialTypes: [
+          {
+            specific: "lydbog (online)"
+          }
+        ],
+        creators: [
+          {
+            display: "Isaac Asimov",
+            __typename: "Person"
+          },
+          {
+            display: "Scott Brick",
+            __typename: "Person"
+          }
+        ],
+        publisher: ["Random House Audio"],
+        identifiers: [
+          {
+            value: "9780739346273"
+          }
+        ],
+        contributors: [],
+        edition: {
+          summary: "Unabridged, 2004",
+          publicationYear: {
+            display: "2004"
+          }
+        },
+        dateFirstEdition: null,
+        audience: {
+          generalAudience: [
+            "Text Difficulty 3 - Text Difficulty 4",
+            "820. Lexile"
+          ]
+        },
+        physicalDescriptions: [
+          {
+            numberOfPages: null,
+            playingTime: null
+          }
+        ],
+        accessTypes: [
+          {
+            code: "ONLINE"
+          }
+        ],
+        access: [
+          {
+            __typename: "AccessUrl",
+            origin: "link.overdrive.com",
+            url: "http://link.overdrive.com/?websiteID=100515&titleID=105225",
+            loginRequired: false
+          },
+          {
+            __typename: "AccessUrl",
+            origin: "samples.overdrive.com",
+            url: "https://samples.overdrive.com/?crid=A6AAE8CE-F944-44F3-960D-4E9A7715CDD5&.epub-sample.overdrive.com",
+            loginRequired: false
+          },
+          {
+            __typename: "AccessUrl",
+            origin: "img1.od-cdn.com",
+            url: "https://img1.od-cdn.com/ImageType-100/1191-1/%7BA6AAE8CE-F944-44F3-960D-4E9A7715CDD5%7DImg100.jpg",
+            loginRequired: false
+          },
+          {
+            __typename: "AccessUrl",
+            origin: "img1.od-cdn.com",
+            url: "https://img1.od-cdn.com/ImageType-200/1191-1/%7BA6AAE8CE-F944-44F3-960D-4E9A7715CDD5%7DImg200.jpg",
+            loginRequired: false
+          }
+        ],
+        shelfmark: null,
+        workYear: null
+      },
+      {
+        pid: "870970-basis:23798255",
+        genreAndForm: ["noveller"],
+        source: ["Bibliotekskatalog"],
+        languages: {
+          main: [
+            {
+              display: "engelsk",
+              isoCode: "eng"
+            }
+          ]
+        },
+        titles: {
+          main: ["I, Robot (Oxford bookworms library, ved Rowena Akinyemi)"],
+          original: []
+        },
+        fictionNonfiction: {
+          display: "skønlitteratur",
+          code: "FICTION"
+        },
+        materialTypes: [
+          {
+            specific: "bog"
+          }
+        ],
+        creators: [
+          {
+            display: "Isaac Asimov",
+            __typename: "Person"
+          }
+        ],
+        publisher: ["Oxford University Press"],
+        identifiers: [
+          {
+            value: "0-19-423069-4"
+          }
+        ],
+        contributors: [],
+        edition: {
+          summary: "2. edition, 2000",
+          publicationYear: {
+            display: "2000"
+          }
+        },
+        dateFirstEdition: null,
+        audience: {
+          generalAudience: []
+        },
+        physicalDescriptions: [
+          {
+            numberOfPages: 104,
+            playingTime: null
+          }
+        ],
+        accessTypes: [
+          {
+            code: "PHYSICAL"
+          }
+        ],
+        access: [
+          {
+            __typename: "InterLibraryLoan",
+            loanIsPossible: true
+          }
+        ],
+        shelfmark: {
+          postfix: "Asimov",
+          shelfmark: "83.8"
+        },
+        workYear: null
+      }
+    ] as Manifestation[]
+  }
+};
