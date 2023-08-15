d38bc5a0628212a401d21e49159f60d823e65ea5
Sine Jespersen <sinejespersen@gmail.com>
2023-08-15 09:05:57 +0200

    5885: add accept modal to loan list

--- src/apps/loan-list/list/loan-list.entry.tsx
+++ src/apps/loan-list/list/loan-list.entry.tsx
@@ -10,6 +10,7 @@ import GlobalUrlEntryPropsInterface from "../../../core/utils/types/global-url-p
 import { GroupModalProps } from "../../../core/storybook/groupModalArgs";
 import { GroupModalLoansProps } from "../../../core/storybook/loanGroupModalArgs";
 import { MaterialDetailsModalProps } from "../../../core/storybook/materialDetailsModalArgs";
+import { AcceptModalEntryTextProps } from "../../../core/storybook/acceptModalArgs";
 
 export interface LoanListEntryConfigProps {
   thresholdConfig: string;
@@ -60,6 +61,7 @@ export interface LoanListEntryWithPageSizeProps
     GroupModalLoansProps,
     LoanListEntryUrlProps,
     MaterialDetailsModalProps,
+    AcceptModalEntryTextProps,
     GlobalUrlEntryPropsInterface {}
 
 const LoanListEntry: FC<LoanListEntryWithPageSizeProps> = ({
---
+++ src/core/storybook/acceptModalArgs.ts
@@ -0,0 +1,41 @@
+export default {
+  acceptModalHeaderText: {
+    defaultValue: "Hov, dit gebyr forhøjes!",
+    control: { type: "text" }
+  },
+  acceptModalBodyText: {
+    defaultValue:
+      "Fornyer du dine lån, forhøjes dit gebyr. Alle materialer ikke kan fornys og lånet splittes derfor op. Ved overskredne lån modtager du et gebyr pr. lån og derfor forhøjes dit samlede gebyr, hvis du vælger at gå videre med at fornye.",
+    control: { type: "text" }
+  },
+  acceptModalAreYouSureText: {
+    defaultValue: "Er du sikker på du vil fornye?",
+    control: { type: "text" }
+  },
+  acceptModalAcceptButtonText: {
+    defaultValue: "Ja, forny mulige",
+    control: { type: "text" }
+  },
+  acceptModalCancelButtonText: {
+    defaultValue: "Annuller fornyelse",
+    control: { type: "text" }
+  },
+  acceptModalAriaDescriptionText: {
+    defaultValue: "accept modal aria description text",
+    control: { type: "text" }
+  },
+  acceptModalAriaLabelText: {
+    defaultValue: "accept modal aria label text",
+    control: { type: "text" }
+  }
+};
+
+export interface AcceptModalEntryTextProps {
+  acceptModalHeaderText: string;
+  acceptModalAreYouSureText: string;
+  acceptModalBodyText: string;
+  acceptModalCancelButtonText: string;
+  acceptModalAcceptButtonText: string;
+  acceptModalAriaDescriptionText: string;
+  acceptModalAriaLabelText: string;
+}
