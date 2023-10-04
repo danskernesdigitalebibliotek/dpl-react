984b00e486f6712ab90effbac9617e382ae4bd2a
Kasper Garnæs <kasperg@users.noreply.github.com>
2023-10-04 14:45:59 +0200

    Add Cypress test for Multiselect

---
+++ src/components/multiselect/Multiselect.test.ts
@@ -0,0 +1,72 @@
+describe("Multiselect", () => {
+  beforeEach(() => {
+    cy.visit(
+      "/iframe.html?args=&id=components-multiselect--default&viewMode=story"
+    );
+  });
+
+  it("Has a title", () => {
+    cy.contains("Title");
+  });
+
+  it("Shows options and checkboxes when opened", () => {
+    cy.get("button:visible").should("contain", "All").click();
+    cy.get("[role=option]")
+      .should("contain", "All")
+      .should("contain", "First item")
+      .should("contain", "2. item")
+      .should("contain", "III");
+    cy.get("[type=checkbox]").should("have.length", 4);
+  });
+
+  it("Has the all option selected by default", () => {
+    cy.get("button:visible").should("contain", "All").click();
+    cy.get("[role=option]")
+      .contains("All")
+      .find("[type=checkbox]")
+      .should("be.checked");
+  });
+
+  it("Allows selection of single value", () => {
+    cy.get("button:visible").click();
+    cy.get("[role=option]")
+      .contains("First item")
+      .click()
+      .find("input[type=checkbox]")
+      .should("be.checked");
+    cy.get("[role=option]")
+      .contains("All")
+      .find("[type=checkbox]")
+      .should("not.be.checked");
+  });
+
+  it("Allows selection of multiple values", () => {
+    cy.get("button:visible").click();
+    cy.get("[role=option]")
+      .contains("First item")
+      .click()
+      .find("[type=checkbox]")
+      .should("be.checked");
+    cy.get("[role=option]")
+      .contains("2. item")
+      .click()
+      .find("[type=checkbox]")
+      .should("be.checked");
+    cy.get("[role=option]")
+      .contains("All")
+      .find("[type=checkbox]")
+      .should("not.be.checked");
+  });
+
+  it("Selects the all option if all values are selected", () => {
+    cy.get("button:visible").click();
+    cy.get("[role=option]").click({ multiple: true });
+    cy.get("[role=option]")
+      .contains("All")
+      .find("[type=checkbox]")
+      .should("be.checked");
+  });
+
+});
+
+export {};
