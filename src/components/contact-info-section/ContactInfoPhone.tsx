766c11c516eb14bfe991e60578ed1dc5aa0b8749
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-10-07 16:45:29 +0200

    Refactor contact info input
    And add a test for wrapping the contact info depending on that the input
    fields should be shown inline or not.

---
+++ src/components/contact-info-section/ContactInfoPhone.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { FC } from "react";
+import TextInput from "../atoms/input/TextInput";
+import { PatronSettingsV3, PatronV5 } from "../../core/fbs/model";
+import CheckBox from "../checkbox/Checkbox";
+import { useText } from "../../core/utils/text";
+import { ChangePatronProps } from "./types";
+
+export interface ContactInfoPhoneProps {
+  patron: PatronV5 | PatronSettingsV3 | null;
+  changePatron: ChangePatronProps;
+  showCheckboxes: boolean;
+  className?: string;
+}
+
+const ContactInfoPhone: FC<ContactInfoPhoneProps> = ({
+  patron,
+  changePatron,
+  showCheckboxes,
+  className = ""
+}) => {
+  const t = useText();
+  return (
+    <>
+      <TextInput
+        className={className}
+        id="phone-input"
+        required
+        type="number"
+        onChange={(newPhoneNumber) =>
+          changePatron(newPhoneNumber, "phoneNumber")
+        }
+        value={patron?.phoneNumber}
+        label={t("patronContactPhoneLabelText")}
+      />
+      {showCheckboxes && (
+        <CheckBox
+          className="mt-8 mb-16"
+          onChecked={(newReceiveSms: boolean) =>
+            changePatron(newReceiveSms, "receiveSms")
+          }
+          id="phone-messages"
+          selected={patron?.receiveSms}
+          disabled={false}
+          label={t("patronContactPhoneCheckboxText")}
+        />
+      )}
+    </>
+  );
+};
+
+export default ContactInfoPhone;
