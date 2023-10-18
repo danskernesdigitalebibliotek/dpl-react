68b8c283b894963e38526bcf2291df76ac447f26
Adam Antal <adoantal@gmail.com>
2023-10-18 13:21:49 +0200

    Make modals' z-index be the highest so they are always on top
    Before, the header was obstructing modals on mobile, and it wasn't
    possible to close them.

--- src/core/utils/modal.tsx
+++ src/core/utils/modal.tsx
@@ -74,11 +74,11 @@ function Modal({
         <div
           className="modal-backdrop"
           style={{
-            // some elements are designed with z-index which means they pop up over the modal
-            // so I add 10 to the z-index of the modal
-            // the index of the modalid is used, so the newest modal is always on top of
+            // Some elements are designed with z-index which means they pop up over the modal
+            // so we add 20 to the z-index of the modal (20 is the highest z-index - header).
+            // The index of the modalid is used, so the newest modal is always on top of
             // the remaining modals
-            zIndex: modalIds.indexOf(modalId) + 10
+            zIndex: modalIds.indexOf(modalId) + 20
           }}
           onClick={() => {
             close();
