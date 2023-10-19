298386074cd4da1e9eae9282667a059e7bfe7478
Kasper Birch <kasperbirch1@gmail.com>
2023-10-19 16:37:40 +0200

    Refactor logic to decide returning `<NotificationComponent />` or null.
    Centralize all `useNotificationMessage` related logic.

--- src/components/notification/NotificationComponent.tsx
+++ src/components/notification/NotificationComponent.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import PromoBar from "../promo-bar/PromoBar";
 
 interface NotificationComponentProps {
-  notificationMessage: string | null;
+  notificationMessage: string;
 }
 
 const NotificationComponent: React.FC<NotificationComponentProps> = ({
   notificationMessage
 }) => {
-  if (!notificationMessage) return null;
   return <PromoBar text={notificationMessage} type="info" />;
 };
 
