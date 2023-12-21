c11cdae41f632091370e83a0a3b99c817cef74eb
Adam Antal <adoantal@gmail.com>
2023-12-21 15:01:05 +0100

    Remove the option to pay from fee details content & connected elements
    It is not possible to pay through our solution. Thus we are removing all
    of these payment instances.

--- src/apps/fee-list/stackable-fees/fee-details-content.tsx
+++ src/apps/fee-list/stackable-fees/fee-details-content.tsx
@@ -1,16 +1,11 @@
 import dayjs from "dayjs";
 import * as React from "react";
-import { FC, useState } from "react";
-import Link from "../../../components/atoms/links/Link";
+import { FC } from "react";
 import { FeeV2 } from "../../../core/fbs/model";
 import { useText } from "../../../core/utils/text";
 import { FaustId } from "../../../core/utils/types/ids";
-import { useUrls } from "../../../core/utils/url";
-import FeeStatusCircle from "../utils/fee-status-circle";
-import { isDateBeforePaymentChangeDate } from "../utils/helper";
 import { dateFormatCustom } from "../../../core/configuration/date-format.json";
 import StackableFeesList from "./stackable-fees-list";
-import AcceptTermsCheckbox from "./AcceptTermsCheckbox";
 import GroupModalContent from "../../../components/GroupModal/GroupModalContent";
 
 export interface FeeDetailsContentProps {
@@ -19,31 +14,12 @@ export interface FeeDetailsContentProps {
 
 const FeeDetailsContent: FC<FeeDetailsContentProps> = ({ feeDetailsData }) => {
   const t = useText();
-  const u = useUrls();
-  const paymentOverviewUrl = u("paymentOverviewUrl");
-  const [check, setCheck] = useState(false);
-  const handleAcceptedTerms = () => {
-    setCheck(!check);
-  };
-  const {
-    amount = 0,
-    creationDate = "",
-    dueDate = "",
-    materials = []
-  } = feeDetailsData;
-  const prePaymentTypeChange = isDateBeforePaymentChangeDate(dueDate);
+  const { amount = 0, creationDate = "", materials = [] } = feeDetailsData;
   const creationDateFormatted = dayjs(creationDate).format(dateFormatCustom);
-  const showPaymentButton = !prePaymentTypeChange && check;
 
   return (
     <div className="modal-loan__container">
       <div className="modal-loan__header">
-        <div className="mr-32">
-          <FeeStatusCircle
-            dueDate={dueDate || ""}
-            feeCreationDate={creationDate}
-          />
-        </div>
         <div>
           <h2 className="modal-loan__title text-header-h2">
             {t("turnedInText", {
@@ -55,33 +31,13 @@ const FeeDetailsContent: FC<FeeDetailsContentProps> = ({ feeDetailsData }) => {
       <GroupModalContent
         amountOfSelectableMaterials={0}
         buttonComponent={
-          <>
-            <AcceptTermsCheckbox handleAcceptedTerms={handleAcceptedTerms} />
-            <div>
-              <p>
-                {t("amountText", {
-                  placeholders: { "@amount": amount }
-                })}
-              </p>
-            </div>
-            {!showPaymentButton && (
-              <button
-                type="button"
-                className="btn-primary btn-outline btn-small arrow__hover--right-small"
-                disabled={!showPaymentButton}
-              >
-                {t("payText")}
-              </button>
-            )}
-            {showPaymentButton && (
-              <Link
-                className="btn-primary btn-filled btn-small arrow__hover--right-small disabled"
-                href={paymentOverviewUrl}
-              >
-                {t("payText")}
-              </Link>
-            )}
-          </>
+          <div>
+            <p className="text-body-small-medium">
+              {t("totalText", {
+                placeholders: { "@total": amount }
+              })}
+            </p>
+          </div>
         }
       >
         <div />
