6d224e879026edafd71688c79e630e0ccd08e24a
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-01-11 14:28:36 +0100

    Add tests for resolving fbi fetcher base urls

--- src/core/dbc-gateway/helper.ts
+++ src/core/dbc-gateway/helper.ts
@@ -1,4 +1,5 @@
 import { QueryFunctionContext } from "react-query";
+import { beforeAll, vi } from "vitest";
 import {
   getServiceBaseUrl,
   serviceUrlKeys
@@ -36,3 +37,47 @@ export const getQueryUrlFromContext = (
 };
 
 export default {};
+
+/* ********************************* Vitest Section  ********************************* */
+if (import.meta.vitest) {
+  const { describe, expect, it } = import.meta.vitest;
+
+  describe("DBC Gateway Requests", () => {
+    beforeAll(() => {
+      vi.mock("../utils/reduxMiddleware/extractServiceBaseUrls", () => {
+        const urls = {
+          fbiBaseUrl: "i-am-fbi-url",
+          fbiSearchBaseUrl: "i-am-fbi-search-url",
+          fbiMaterialBaseUrl: "i-am-fbi-material-url"
+        } as const;
+
+        return {
+          getServiceBaseUrl: (apiBaseUrlKey: keyof typeof urls) => {
+            return urls[apiBaseUrlKey] ?? urls.fbiBaseUrl;
+          },
+          serviceUrlKeys: {
+            fbi: "fbiBaseUrl",
+            fbiSearch: "fbiSearchBaseUrl",
+            fbiMaterial: "fbiMaterialBaseUrl"
+          }
+        };
+      });
+    });
+
+    it("should resolve baseurl based on query name", () => {
+      expect(resolveBaseUrl("searchWithPagination")).toEqual(
+        "i-am-fbi-search-url"
+      );
+      expect(resolveBaseUrl("getMaterial")).toEqual("i-am-fbi-material-url");
+    });
+
+    it("should resolve default to the fbi base url if the query is unknown", () => {
+      expect(resolveBaseUrl("someUnknownQuery")).toEqual("i-am-fbi-url");
+      expect(resolveBaseUrl()).toEqual("i-am-fbi-url");
+    });
+
+    it("should resolve default to the fbi base url if no query has been specified", () => {
+      expect(resolveBaseUrl()).toEqual("i-am-fbi-url");
+    });
+  });
+}
