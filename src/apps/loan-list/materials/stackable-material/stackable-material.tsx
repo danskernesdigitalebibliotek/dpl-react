fa9fab9ed064b034728219992c8a3cd25b089285
Adam Antal <adoantal@gmail.com>
2024-01-19 12:14:17 +0100

    Don't open material details modal after interacting with the pager
    Pager gives focus to the next stackable material too quickly while still
    registering the key press. So when interacting with the pager on the
    keyboard, material details of the following, newly shown item would open
    unintentionally.

--- src/apps/loan-list/materials/stackable-material/stackable-material.tsx
+++ src/apps/loan-list/materials/stackable-material/stackable-material.tsx
@@ -48,7 +48,10 @@ const StackableMaterial: FC<StackableMaterialProps & MaterialProps> = ({
       role="button"
       onClick={handleOpenDueDateModal}
       onKeyUp={(e) => {
-        if (e.key === "Enter" || e.key === "Space") {
+        // `!focused` prevents opening material details modal after clicking
+        // enter on pager. Pager gives focus to the next stackable material too
+        // quickly while still registering the enter key press.
+        if ((e.key === "Enter" || e.key === "Space") && !focused) {
           handleOpenDueDateModal();
         }
       }}
