5a34de5c88fa9c431499b6830a1fd5d60a116571
clausbruun <claus@b-ruu.dk>
2024-01-26 14:30:14 +0100

    Remove queue number label for physical materials, third approach

--- src/apps/reservation-list/reservation-material/reservation-info.tsx
+++ src/apps/reservation-list/reservation-material/reservation-info.tsx
@@ -104,12 +104,19 @@ const ReservationInfo: FC<ReservationInfoProps> = ({
   }
 
   if (state === "reserved" && pickupBranch && numberInQueue && expiryDate) {
+    const numberInLineLabel =
+      numberInQueue === 1
+        ? t("reservationListFirstInQueueText")
+        : t("reservationListNumberInQueueText", {
+            placeholders: { "@count": numberInQueue - 1 }
+          });
+
     return (
       <ReservationStatus
         // The decision regarding this is, that if the user is number 4
         // in the queue for a material, the "percent-wheel-thing" should be 1/4 full.
         percent={(1 / numberInQueue) * 100}
-        label=""
+        label={showStatusCircleIcon ? numberInLineLabel : ""}
         reservationInfo={reservationInfo}
         openReservationDetailsModal={openReservationDetailsModal}
         empty={!showStatusCircleIcon}
