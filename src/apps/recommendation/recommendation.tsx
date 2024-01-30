fc4c5f838cadf45afdededdcc80611a3aeb1daa1
LasseStaus <l.stausgaard@gmail.com>
2024-01-30 13:41:07 +0100

    Add test recommendation component.
    Modified component with data attributes.
    Added test graphl fixture data
    Added test for recommendation.
    
    DDFFORM-128

--- src/apps/recommendation/recommendation.tsx
+++ src/apps/recommendation/recommendation.tsx
@@ -45,14 +45,22 @@ const Recommendation: React.FC<RecommendationProps> = ({
         "recommendation",
         positionImageRight && "recommendation--reversed"
       )}
+      data-cy="recommendation"
     >
       <RecommendedMaterial wid={wid} />
       <Link
         href={materialFullUrl}
         className="recommendation__texts arrow__hover--right-small"
       >
-        <h3 className="recommendation__title">{materialFullTitle}</h3>
-        <p className="recommendation__description">{materialDescription}</p>
+        <h3 className="recommendation__title" data-cy="recommendation-title">
+          {materialFullTitle}
+        </h3>
+        <p
+          className="recommendation__description"
+          data-cy="recommendation-description"
+        >
+          {materialDescription}
+        </p>
         <Arrow />
       </Link>
     </div>
