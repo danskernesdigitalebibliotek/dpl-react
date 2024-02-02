373aaa20d6451df9ccf1dd0a41ef83572aeb568a
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-02-02 20:19:50 +0100

    Introducing IconVarious used for the loan list

---
+++ src/components/icon-various/icon-various.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import VariousIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/Various.svg";
+
+const IconVarious = () => {
+  return <img width={24} height={24} src={VariousIcon} alt="Icon various" />;
+};
+
+export default IconVarious;
