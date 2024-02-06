f4725aa1f690e4af95c6d43f4920fba4e126f6df
Adam Antal <adoantal@gmail.com>
2024-02-06 11:11:32 +0100

    Unify list header component with the design from the design system
    Margin was missing

--- src/apps/loan-list/list/ListHeader.tsx
+++ src/apps/loan-list/list/ListHeader.tsx
@@ -8,7 +8,7 @@ export interface ListHeaderProps {
 
 const ListHeader: FC<ListHeaderProps> = ({ header, amount, children }) => {
   return (
-    <div className="dpl-list-buttons">
+    <div className="dpl-list-buttons m-32">
       <h2 className="dpl-list-buttons__header">
         {header}
         <span className="dpl-list-buttons__power">{amount}</span>
