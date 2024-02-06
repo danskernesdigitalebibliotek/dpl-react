ef46495021f254f020bfb04b1c8179abd6b257ac
Adam Antal <adoantal@gmail.com>
2024-02-06 14:46:44 +0100

    Remove pause reservations section from the patron page

--- src/apps/patron-page/sections/ReservationDetailsSection.tsx
+++ src/apps/patron-page/sections/ReservationDetailsSection.tsx
@@ -1,10 +1,7 @@
 import React, { FC } from "react";
 import { PatronV5 } from "../../../core/fbs/model";
-import CheckBox from "../../../components/checkbox/Checkbox";
 import BranchesDropdown from "../util/BranchesDropdown";
 import { useText } from "../../../core/utils/text";
-import { useModalButtonHandler } from "../../../core/utils/modal";
-import { getModalIds } from "../../../core/utils/helpers/modal-helpers";
 
 export interface ChangePatronProps {
   (newValue: string | boolean, key: string): void;
@@ -20,12 +17,6 @@ const ReservationDetailsSection: FC<ReservationDetailsSectionProps> = ({
   changePatron
 }) => {
   const t = useText();
-  const { open } = useModalButtonHandler();
-  const { pauseReservation } = getModalIds();
-
-  const openPauseReservationModal = () => {
-    open(pauseReservation as string);
-  };
 
   return (
     <section data-cy="pickup-reservations-section">
@@ -41,21 +32,6 @@ const ReservationDetailsSection: FC<ReservationDetailsSectionProps> = ({
         onChange={(newPreferredPickupBranch) =>
           changePatron(newPreferredPickupBranch, "preferredPickupBranch")
         }
-      />
-
-      <h3 className="text-body-small-regular mt-32 mb-16">
-        {t("patronPagePauseReservationsHeaderText")}
-      </h3>
-      <p className="text-body-small-regular mb-8">
-        {t("patronPagePauseReservationsBodyText")}
-      </p>
-      <CheckBox
-        className="my-16"
-        id="show-reservation-pause-section"
-        onChecked={openPauseReservationModal}
-        ariaLabel={t("patronPageOpenPauseReservationsSectionAriaText")}
-        selected={false}
-        label={t("patronPageOpenPauseReservationsSectionText")}
       />
     </section>
   );
