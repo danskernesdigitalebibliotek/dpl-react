8be9b835c753c0b5d20869edc02da8b5cebddc07
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-02-14 12:33:19 +0100

    Use `ìsUnregistered()` in CreatePatron component
    Only to allow access to unregistered users at the registration page.

--- src/apps/create-patron-user-info/CreatePatron.tsx
+++ src/apps/create-patron-user-info/CreatePatron.tsx
@@ -1,24 +1,23 @@
 import React, { useState, FC, useEffect } from "react";
+import { is } from "cypress/types/bluebird";
 import UserInfo from "./UserInfo";
 import { useUrls } from "../../core/utils/url";
 import { useText } from "../../core/utils/text";
 import { redirectTo } from "../../core/utils/helpers/url";
 import { useConfig } from "../../core/utils/config";
 import useUserInfo from "../../core/adgangsplatformen/useUserInfo";
+import { isUnregistered } from "../../core/utils/helpers/user";
 
 const CreatePatron: FC = () => {
   const [cpr, setCpr] = useState<string | null>(null);
   const config = useConfig();
   const t = useText();
-  const u = useUrls();
-  const dashboardUrl = u("dashboardUrl");
 
   const { id: agencyId } = config<{
     id: `${number}`;
   }>("agencyConfig", {
     transformer: "jsonParse"
   });
-
   // Fetch user info data.
   const { data: userInfo, isLoading } = useUserInfo();
 
@@ -28,21 +27,12 @@ const CreatePatron: FC = () => {
     }
 
     const {
-      attributes: { agencies, cpr: userCpr }
+      attributes: { cpr: userCpr }
     } = userInfo;
-
-    const userWasAlreadyCreated = agencies.some(
-      (agency) => agency.agencyId === agencyId
-    );
-
-    // If the user was already created, redirect to the dashboard.
-    if (userWasAlreadyCreated) {
-      redirectTo(dashboardUrl);
-    }
 
     // Otherwise set the cpr so we can show the create patron form.
     setCpr(String(userCpr));
-  }, [agencyId, dashboardUrl, isLoading, userInfo]);
+  }, [agencyId, isLoading, userInfo]);
 
   if (isLoading) {
     return <div>{t("loadingText")}</div>;
