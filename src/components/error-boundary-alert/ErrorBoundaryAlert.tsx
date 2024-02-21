97ef4251f5a3df3751c39a348de66987a1805d7a
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-02-21 13:17:37 +0100

    Render ErrorBoundaryAlert in a portal
    In order to put the error messages from the different applikations on
    the same spot we use portals to show them in the container id set by the
    hosting application

--- src/components/error-boundary-alert/ErrorBoundaryAlert.tsx
+++ src/components/error-boundary-alert/ErrorBoundaryAlert.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
 import ReachAlert from "@reach/alert";
+import ReactDOM from "react-dom";
 import { useText } from "../../core/utils/text";
+import { useConfig } from "../../core/utils/config";
 
 interface ErrorBoundaryAlertProps {
   className?: string;
@@ -19,7 +21,41 @@ const ErrorBoundaryAlert: FC<ErrorBoundaryAlertProps> = ({
   resetErrorBoundary
 }) => {
   const t = useText();
-  return (
+  const config = useConfig();
+  const errorMessagesConfig = config<{
+    containerId: string;
+    shouldOnlyShowOneError?: boolean;
+  }>("errorMessagesConfig", {
+    transformer: "jsonParse"
+  });
+
+  // We need to have a container to render the alert in.
+  if (errorMessagesConfig.containerId === undefined) {
+    return null;
+  }
+  const { containerId } = errorMessagesConfig;
+  const container = document.getElementById(containerId);
+
+  // If we do not have configured if we should only show one error, we default to true.
+  const shouldOnlyShowOneError =
+    errorMessagesConfig.shouldOnlyShowOneError !== undefined
+      ? errorMessagesConfig.shouldOnlyShowOneError
+      : true;
+
+  // If container is not present, we don't want to render the alert.
+  if (!container) {
+    return null;
+  }
+  // If we should only show one error and there is already an error message
+  // we don't want to render multiple error messages.
+  if (
+    shouldOnlyShowOneError &&
+    (container?.textContent ?? "").trim().length > 0
+  ) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
     <ReachAlert
       className={`dpl-alert dpl-alert--${variant} ${className}`}
       type={type}
@@ -34,7 +70,8 @@ const ErrorBoundaryAlert: FC<ErrorBoundaryAlertProps> = ({
           ({t("alertErrorCloseText")})
         </button>
       </>
-    </ReachAlert>
+    </ReachAlert>,
+    container
   );
 };
 
