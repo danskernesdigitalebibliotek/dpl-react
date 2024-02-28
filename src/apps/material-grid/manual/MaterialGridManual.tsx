fde803cc87a23791e48b6a3ff4b849834baeb307
LasseStaus <l.stausgaard@gmail.com>
2024-02-28 09:34:24 +0100

    Add component and story for MaterialGridManuel
    This adds a grid display of recommended materials, that can be selected for display by adding the workID of the materials.
    
    DDFFORM-314

---
+++ src/apps/material-grid/manual/MaterialGridManual.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { useEffect } from "react";
+import { WorkId } from "../../../core/utils/types/ids";
+import MaterialGrid from "../MaterialGrid";
+import MaterialGridSkeleton from "../MaterialGridSkeleton";
+import { calculateAmountToDisplay } from "../materiel-grid-util";
+
+export type MaterialGridManualProps = {
+  materialIds: WorkId[];
+  title: string;
+};
+
+const MaterialGridManual: React.FC<MaterialGridManualProps> = ({
+  materialIds,
+  title
+}) => {
+  const [parsedMaterialIds, setParsedMaterials] =
+    React.useState<WorkId[]>(materialIds);
+
+  useEffect(() => {
+    // Materials must be parsed from string to WorkId[] array as they are
+    // received as a string from the backend.
+    if (typeof materialIds === "string") {
+      setParsedMaterials(JSON.parse(materialIds));
+    }
+  }, [materialIds]);
+
+  const selectedAmountOfMaterialsForDisplay = calculateAmountToDisplay(
+    parsedMaterialIds.length
+  );
+
+  if (!parsedMaterialIds.length || typeof parsedMaterialIds === "string") {
+    return <MaterialGridAutomaticSkeleton />;
+  }
+
+  return (
+    <MaterialGrid
+      title={title}
+      materialIds={parsedMaterialIds}
+      selectedAmountOfMaterialsForDisplay={selectedAmountOfMaterialsForDisplay}
+    />
+  );
+};
+export default MaterialGridManual;
