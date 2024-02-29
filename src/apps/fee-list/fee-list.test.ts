5df37349f91453231167b6e618faa9e880113e3a
clausbruun <claus@b-ruu.dk>
2024-02-29 15:18:06 +0100

    Fix test

--- src/apps/fee-list/fee-list.test.ts
+++ src/apps/fee-list/fee-list.test.ts
@@ -213,7 +213,7 @@ describe("Fee list", () => {
       .find(".list-reservation__fee")
       .find(".text-body-medium-regular")
       .should("exist")
-      .should("have.text", "Fee 15,00&nbsp;kr.");
+      .should("have.text", "Fee 70,00&nbsp;kr.");
 
     // 4.b +x other materials
     cy.getBySel("fee-list-page")
