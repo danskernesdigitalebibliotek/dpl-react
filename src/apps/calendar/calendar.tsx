a7146fb4781d8b5ae9af07ec698d2de6b825aa62
Kasper Birch <kasperbirch1@gmail.com>
2024-03-12 19:33:48 +0100

    Implement `FullCalendar` functionality
    - Configured to use Danish week format (week starts on Monday)
    - Added default event setup
    - Enabled drag-and-select for creating events ( temporarily stored in local states )
    - Utilized the color property for event customization

--- src/apps/calendar/calendar.tsx
+++ src/apps/calendar/calendar.tsx
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import da from "@fullcalendar/core/locales/da";
+import { DateSelectArg, EventInput } from "@fullcalendar/core";
 
 const Calendar: React.FC = () => {
-  return <div>App: Calendar</div>;
+  const [events, setEvents] = useState<EventInput[]>([
+    {
+      title: "My Event",
+      start: "2024-03-14T10:00:00",
+      end: "2024-03-14T15:00:00",
+      color: "blue"
+    }
+  ]);
+
+  const handleDateSelect = (selectInfo: DateSelectArg) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selectInfo.view.calendar;
+
+    if (title) {
+      setEvents([
+        ...events,
+        {
+          title,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay,
+          color: "green"
+        }
+      ]);
+    }
+
+    // clear date selection
+    calendarApi.unselect();
+  };
+
+  return (
+    <div>
+      <FullCalendar
+        plugins={[timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        locale={da}
+        selectable
+        select={handleDateSelect}
+        events={events}
+      />
+    </div>
+  );
 };
+
 export default Calendar;
