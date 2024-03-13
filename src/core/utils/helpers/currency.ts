b3cff8b5c96c661df6c71e4ad34f6b12726de8e9
clausbruun <claus@b-ruu.dk>
2024-03-13 08:21:30 +0100

    Change JSDoc comment to inline comment.

--- src/core/utils/helpers/currency.ts
+++ src/core/utils/helpers/currency.ts
@@ -1,9 +1,4 @@
-/**
- * Formats a number as a Danish Krone (DKK) currency string using the "da-DK" locale.
- *
- * @param {number} number - The numeric value to format into a currency string.
- * @returns {string} A string formatted in Danish Krone currency format.
- */
+// Formats a number as a Danish Krone (DKK) currency string using the "da-DK" locale.
 export function formatCurrency(number: number): string {
   const options = { style: "currency", currency: "DKK" };
   return number.toLocaleString("da-DK", options);
