4220af36066ab1e955f2323a2aa4b1d7a1f99815
Kasper Birch <kasperbirch1@gmail.com>
2024-03-13 22:35:45 +0100

    Refactor handler functions into `helper.ts`
    Improve code readability and cleanliness.

--- src/apps/opening-hours-editor/OpeningHoursEditor.tsx
+++ src/apps/opening-hours-editor/OpeningHoursEditor.tsx
@@ -4,9 +4,14 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import da from "@fullcalendar/core/locales/da";
-import { DateSelectArg, EventClickArg, EventInput } from "@fullcalendar/core";
+import { EventInput } from "@fullcalendar/core";
 import { useDplOpeningHoursListGET } from "../../core/dpl-cms/dpl-cms";
-import { createCmsEventId, formatCmsEventsToFullCalendar } from "./helper";
+import {
+  formatCmsEventsToFullCalendar,
+  handleDateSelect,
+  handleEventClick,
+  handleEventRemove
+} from "./helper";
 import renderOpeningHoursEditorEventContent from "./OpeningHoursEditorEventContent";
 
 const OpeningHoursEditor: React.FC = () => {
@@ -20,61 +25,6 @@ const OpeningHoursEditor: React.FC = () => {
     }
   }, [openingHoursData]);
 
-  const handleDateSelect = (selectInfo: DateSelectArg) => {
-    // Todo: Replace prompt with a modal
-    // eslint-disable-next-line no-alert
-    const title = prompt("Please enter a new title for your event");
-    const calendarApi = selectInfo.view.calendar;
-
-    if (title) {
-      // Checks if the selected end date is different from the start day; if so, sets the end date to be the same as the start day and the end time to 00:00:00
-      const startDay = new Date(selectInfo.startStr);
-      let endDay = new Date(selectInfo.endStr);
-
-      if (
-        endDay.getDate() !== startDay.getDate() ||
-        endDay.getMonth() !== startDay.getMonth() ||
-        endDay.getFullYear() !== startDay.getFullYear()
-      ) {
-        endDay = new Date(startDay);
-        // Adds one day to the end day and sets the time to 00:00:00ß
-        endDay.setDate(endDay.getDate() + 1);
-        endDay.setHours(0, 0, 0);
-      }
-
-      setEvents([
-        ...events,
-        {
-          title,
-          start: startDay.toISOString(),
-          end: endDay.toISOString(),
-          allDay: selectInfo.allDay,
-          color: "green",
-          id: createCmsEventId(title, startDay)
-        }
-      ]);
-    }
-
-    // clear date selection
-    calendarApi.unselect();
-  };
-
-  const handleEventClick = (clickInfo: EventClickArg) => {
-    // eslint-disable-next-line no-alert
-    const newTitle = prompt(
-      "Enter a new title for this event",
-      clickInfo.event.title
-    );
-
-    if (newTitle) {
-      clickInfo.event.setProp("title", newTitle);
-    }
-  };
-
-  const handleEventRemove = (eventToRemove: EventInput) => {
-    setEvents(events.filter((event) => event.id !== eventToRemove.id));
-  };
-
   return (
     <FullCalendar
       plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -86,15 +36,18 @@ const OpeningHoursEditor: React.FC = () => {
       initialView="timeGridWeek"
       locale={da}
       selectable
-      select={handleDateSelect}
-      eventClick={handleEventClick}
-      events={events}
+      select={(selectInfo) =>
+        handleDateSelect({ selectInfo, events, setEvents })
+      }
+      eventClick={(clickInfo) => handleEventClick(clickInfo)}
       eventContent={(eventInput) =>
         renderOpeningHoursEditorEventContent({
           eventInput,
-          handleEventRemove
+          handleEventRemove: (eventToRemove) =>
+            handleEventRemove(eventToRemove, events, setEvents)
         })
       }
+      events={events}
       stickyHeaderDates
       height="auto"
     />
--- src/apps/opening-hours-editor/helper.ts
+++ src/apps/opening-hours-editor/helper.ts
@@ -1,3 +1,4 @@
+import { DateSelectArg, EventClickArg, EventInput } from "@fullcalendar/core";
 import { DplOpeningHoursListGET200Item } from "../../core/dpl-cms/model";
 
 function formatDateTimeString(date: string, time: string) {
@@ -22,4 +23,75 @@ export const formatCmsEventsToFullCalendar = (
       color: "blue"
     };
   });
+};
+
+//  handler functions for the calendar
+
+interface HandleDateSelectParams {
+  selectInfo: DateSelectArg;
+  events: EventInput[];
+  setEvents: React.Dispatch<React.SetStateAction<EventInput[]>>;
+}
+
+export const handleDateSelect = ({
+  selectInfo,
+  events,
+  setEvents
+}: HandleDateSelectParams) => {
+  // Todo: Replace prompt with a modal
+  // eslint-disable-next-line no-alert
+  const title = prompt("Please enter a new title for your event");
+  const calendarApi = selectInfo.view.calendar;
+
+  if (title) {
+    // Checks if the selected end date is different from the start day; if so, sets the end date to be the same as the start day and the end time to 00:00:00
+    const startDay = new Date(selectInfo.startStr);
+    let endDay = new Date(selectInfo.endStr);
+
+    if (
+      endDay.getDate() !== startDay.getDate() ||
+      endDay.getMonth() !== startDay.getMonth() ||
+      endDay.getFullYear() !== startDay.getFullYear()
+    ) {
+      endDay = new Date(startDay);
+      // Adds one day to the end day and sets the time to 00:00:00ß
+      endDay.setDate(endDay.getDate() + 1);
+      endDay.setHours(0, 0, 0);
+    }
+
+    setEvents([
+      ...events,
+      {
+        title,
+        start: startDay.toISOString(),
+        end: endDay.toISOString(),
+        allDay: selectInfo.allDay,
+        color: "green",
+        id: createCmsEventId(title, startDay)
+      }
+    ]);
+  }
+
+  // clear date selection
+  calendarApi.unselect();
+};
+
+export const handleEventClick = (clickInfo: EventClickArg) => {
+  // eslint-disable-next-line no-alert
+  const newTitle = prompt(
+    "Enter a new title for this event",
+    clickInfo.event.title
+  );
+
+  if (newTitle) {
+    clickInfo.event.setProp("title", newTitle);
+  }
+};
+
+export const handleEventRemove = (
+  eventToRemove: EventInput,
+  events: EventInput[],
+  setEvents: React.Dispatch<React.SetStateAction<EventInput[]>>
+) => {
+  setEvents(events.filter((event) => event.id !== eventToRemove.id));
 };
