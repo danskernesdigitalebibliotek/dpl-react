8233f9511c7628a56700e4828cd61e4f32821523
Kasper Birch <kasperbirch1@gmail.com>
2024-03-13 13:28:22 +0100

    Add WireMock for `https://dpl-cms.docker` and add opening hours endpoint to `/dpl_opening_hours`
    Enables testing of the Orval-generated `formatCmsEventsToFullCalendar` hook in `Calendar` app.

--- src/apps/calendar/calendar.dev.tsx
+++ src/apps/calendar/calendar.dev.tsx
@@ -1,11 +1,14 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
+import serviceUrlArgs from "../../core/storybook/serviceUrlArgs";
 import Calendar from "./calendar.entry";
 
 export default {
   title: "Apps / Calendar",
   component: Calendar,
-  argTypes: {}
+  argTypes: {
+    ...serviceUrlArgs
+  }
 } as ComponentMeta<typeof Calendar>;
 
 export const App: ComponentStory<typeof Calendar> = (args) => (
--- src/apps/calendar/calendar.entry.tsx
+++ src/apps/calendar/calendar.entry.tsx
@@ -3,6 +3,7 @@ import GuardedApp from "../../components/guarded-app";
 import { withText } from "../../core/utils/text";
 
 import Calendar from "./calendar";
+import { withUrls } from "../../core/utils/url";
 
 const CalendarEntry: React.FC = () => (
   <GuardedApp app="calendar">
@@ -10,4 +11,4 @@ const CalendarEntry: React.FC = () => (
   </GuardedApp>
 );
 
-export default withText(CalendarEntry);
+export default withUrls(withText(CalendarEntry));
