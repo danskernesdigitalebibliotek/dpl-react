ce72f37b5029fb0048ef5b5dd5362d3fdba31666
Adam Antal <adoantal@gmail.com>
2024-04-02 12:35:21 +0200

    Use admin blacklisted branches when asking for holdings on material page
    These should be taken into consideration when checking whether a
    material can be reserved from another library.

--- src/core/utils/useReservableFromAnotherLibrary.tsx
+++ src/core/utils/useReservableFromAnotherLibrary.tsx
@@ -10,6 +10,7 @@ const useReservableFromAnotherLibrary = (
   const config = useConfig();
   const { data: holdingsData } = useGetHoldings({
     faustIds: getAllFaustIds(manifestations),
+    useAvailabilityBlacklist: true,
     config
   });
 
