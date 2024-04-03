d2e301854e4aef2d1fdcfd3678a1c391caf78fe0
LasseStaus <l.stausgaard@gmail.com>
2024-04-03 19:57:34 +0200

    Added helper functions for openingHours
    These are all the helpers used for the openingHours app.
    
    DDFFORM-417

---
+++ src/apps/opening-hours/OpeningHoursHelpers.ts
@@ -0,0 +1,91 @@
+import dayjs from "dayjs";
+import "dayjs/locale/da";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import lodash from "lodash";
+import { DplOpeningHoursListGET200Item as OpeningHoursItem } from "../../core/dpl-cms/model";
+
+dayjs.locale("da");
+dayjs.extend(weekOfYear);
+
+export const getNextWeek = (date: Date): Date => {
+  return dayjs(date).add(1, "week").toDate();
+};
+
+export const getPreviousWeek = (date: Date): Date => {
+  return dayjs(date).subtract(1, "week").toDate();
+};
+
+export const getWeek = (date: Date): string => {
+  return dayjs(date).week().toString();
+};
+
+export const getYear = (date: Date): string => {
+  return dayjs(date).year().toString();
+};
+
+export const formatWeekString = (
+  translationKey: string,
+  date: Date
+): string => {
+  const week = getWeek(date);
+  const year = getYear(date);
+
+  return `${translationKey} ${week}, ${year}`;
+};
+
+const capitalizeString = (string: string): string => {
+  return lodash.upperFirst(string);
+};
+
+export const formatDateToWeekday = (date: Date): string => {
+  const formattedAsWeekday = dayjs(date).format("dddd");
+  const capitalizedWeekday = capitalizeString(formattedAsWeekday);
+
+  return capitalizedWeekday;
+};
+
+export const formatDateForAPI = (date: Date): string => {
+  return dayjs(date).format("YYYY-MM-DD");
+};
+
+export const getWeekStartAndEndDate = (date = new Date()) => {
+  const start = dayjs(date).startOf("week").toDate();
+  const end = dayjs(date).endOf("week").toDate();
+
+  return { start, end };
+};
+
+export type GroupedOpeningHours = Array<{
+  dateTime: Date;
+  openingHourEntries: OpeningHoursItem[];
+}>;
+
+export const groupOpeningHoursByWeekday = (
+  startDate: Date,
+  endDate: Date,
+  openingHours: OpeningHoursItem[]
+): GroupedOpeningHours => {
+  const startDay = dayjs(startDate);
+  const endDay = dayjs(endDate);
+  let currentDay = startDay;
+
+  // Generate array of days between start and end dates
+  const allDays = [];
+
+  while (!currentDay.isAfter(endDay)) {
+    allDays.push(currentDay.toDate());
+    currentDay = currentDay.add(1, "day");
+  }
+
+  const daysWithOpeningHours: GroupedOpeningHours = allDays.map((day) => {
+    return {
+      dateTime: day,
+      date: day,
+      openingHourEntries: openingHours.filter((individualOpeningHour) =>
+        dayjs(individualOpeningHour.date).isSame(day, "day")
+      )
+    };
+  });
+
+  return daysWithOpeningHours;
+};
