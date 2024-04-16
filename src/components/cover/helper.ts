37dc10665b2582969917041cedaadeeea00dc556
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-04-16 13:27:44 +0200

    Add getCoverUrl unit tests
    To get a bit of hold of the logic and the precedence of it.

--- src/components/cover/helper.ts
+++ src/components/cover/helper.ts
@@ -48,3 +48,115 @@ export const getCoverUrl = ({
 };
 
 export default {};
+
+// ************** VITEST ***************
+if (import.meta.vitest) {
+  const { describe, expect, it } = import.meta.vitest;
+  const coverData = [
+    {
+      id: "1-somestring:2",
+      imageUrls: {
+        small: {
+          url: "url1"
+        },
+        large: {
+          url: null
+        }
+      }
+    },
+    {
+      id: "3-somestring:4",
+      imageUrls: {
+        small: {
+          url: "url2"
+        }
+      }
+    },
+    {
+      id: "4-somestring:5",
+      imageUrls: {
+        small: {
+          url: null
+        }
+      }
+    }
+  ];
+
+  describe("Testing getCoverUrl function with no best representation given", () => {
+    it("Should show first cover if cover is available in size given", () => {
+      const url = getCoverUrl({
+        coverData,
+        size: "small"
+      });
+      expect(url).toBe("url1");
+    });
+
+    it("Should return null if the size is available but the url is null", () => {
+      const url = getCoverUrl({
+        coverData,
+        size: "large"
+      });
+      expect(url).toBe(null);
+    });
+
+    it("Should return null if the size does not exist", () => {
+      const url = getCoverUrl({
+        coverData,
+        size: "medium"
+      });
+      expect(url).toBe(null);
+    });
+    it("Should return null if there are no covvers in data", () => {
+      const url = getCoverUrl({
+        coverData: [],
+        size: "small"
+      });
+      expect(url).toBe(null);
+    });
+  });
+
+  describe("Testing getCoverUrl function with a best representation given", () => {
+    it("Should show the best representation cover if size matches", () => {
+      type GetCoverUrlParams = Parameters<typeof getCoverUrl>[0];
+      const bestRepresentation = {
+        genreAndForm: ["Book"],
+        pid: "3-somestring:4"
+      };
+
+      const url = getCoverUrl({
+        coverData,
+        size: "small",
+        bestRepresentation
+      } as GetCoverUrlParams);
+      expect(url).toBe("url2");
+    });
+    it("Should show the first cover if size matches the best representation cover but url is null", () => {
+      type GetCoverUrlParams = Parameters<typeof getCoverUrl>[0];
+      const bestRepresentation = {
+        genreAndForm: ["Book"],
+        pid: "4-somestring:5"
+      };
+
+      const url = getCoverUrl({
+        coverData,
+        size: "small",
+        bestRepresentation
+      } as GetCoverUrlParams);
+      expect(url).toBe("url1");
+    });
+    it("Should show the first cover if size matches the best representation cover but url is null", () => {
+      type GetCoverUrlParams = Parameters<typeof getCoverUrl>[0];
+      const bestRepresentation = {
+        genreAndForm: ["Book"],
+        pid: "4-somestring:5"
+      };
+
+      const url = getCoverUrl({
+        coverData,
+        size: "small",
+        bestRepresentation
+      } as GetCoverUrlParams);
+      expect(url).toBe("url1");
+    });
+  });
+}
