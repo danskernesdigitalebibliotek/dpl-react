0fdcae9c8e545d91eeeee987020319529a8fe37a
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-08-19 13:48:39 +0200

    Fix blocked modal cypress tests
    Since the behaviour has changed we need to update the tests accordingly.

--- src/components/blocked-patron/BlockedPatron.test.ts
+++ src/components/blocked-patron/BlockedPatron.test.ts
@@ -1,7 +1,24 @@
+import { resetPersistedData } from "../../core/store";
+
 describe("Patron page", () => {
   before(() => {
+    // Make sure we have a clean slate before we start testing.
+    resetPersistedData();
     cy.createFakeAuthenticatedSession();
     cy.createFakeLibrarySession();
+  });
+
+  beforeEach(() => {
+    cy.intercept("GET", "**/external/agencyid/patrons/patronid/loans/v2**", {
+      statusCode: 200,
+      body: []
+    });
+    cy.intercept("GET", "**/v1/user/**", {
+      statusCode: 200,
+      body: {
+        loans: []
+      }
+    });
   });
 
   it("Patron not blocked", () => {
@@ -14,6 +31,7 @@ describe("Patron page", () => {
     cy.get(".modal").should("not.exist");
   });
 
+  // TYPE: E (fee)
   // Blocked types:
   // https://github.com/itk-dev/dpl-react/blob/develop/src/core/utils/types/BlockedTypes.ts
   it("Patron blocked E", () => {
@@ -30,15 +48,33 @@ describe("Patron page", () => {
     });
     cy.visit("/iframe.html?path=/story/apps-loan-list--loan-list-entry");
     cy.getBySel("modal").should("exist");
-    cy.getBySel("modal").should("exist");
     cy.getBySel("modal").get("h1").should("exist");
     cy.getBySel("modal").get("p").should("exist");
     cy.getBySel("modal").get("a").should("exist");
   });
 
+  it("Does NOT show the blocked modal again once it has been shown", () => {
+    cy.intercept("GET", "**/external/agencyid/patrons/patronid/v2**", {
+      patron: {
+        blockStatus: [
+          {
+            blockedReason: "E",
+            blockedSince: "",
+            message: ""
+          }
+        ]
+      }
+    });
+    cy.visit("/iframe.html?path=/story/apps-loan-list--loan-list-entry");
+    cy.getBySel("modal").should("not.exist");
+  });
+
+  // TYPE: U (exclusion)
   // Blocked types:
   // https://github.com/itk-dev/dpl-react/blob/develop/src/core/utils/types/BlockedTypes.ts
   it("Patron blocked U", () => {
+    // To make sure that the modal is shown, we need to reset the persisted data.
+    resetPersistedData();
     cy.intercept("GET", "**/external/agencyid/patrons/patronid/v2**", {
       patron: {
         blockStatus: [
@@ -57,9 +93,12 @@ describe("Patron page", () => {
     cy.getBySel("modal").get("a").should("not.exist");
   });
 
+  // TYPE: W (selfcreated)
   // Blocked types:
   // https://github.com/itk-dev/dpl-react/blob/develop/src/core/utils/types/BlockedTypes.ts
   it("Patron blocked W", () => {
+    // To make sure that the modal is shown, we need to reset the persisted data.
+    resetPersistedData();
     cy.intercept("GET", "**/external/agencyid/patrons/patronid/v2**", {
       patron: {
         blockStatus: [
