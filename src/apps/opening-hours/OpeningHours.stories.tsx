8b828f0b69fb7eeacb047e0b217572e293012332
thomasgross <thomasgrossrasmussen@gmail.com>
2024-09-05 16:24:09 +0200

    chore: Update argTypes and rewrite args for OpeningHours.stories.tsx

--- src/apps/opening-hours/OpeningHours.stories.tsx
+++ src/apps/opening-hours/OpeningHours.stories.tsx
@@ -1,16 +1,22 @@
-import type { Meta, StoryFn } from "@storybook/react";
-import React from "react";
-import globalConfigArgs from "../../core/storybook/globalConfigArgs";
+import type { Meta, StoryObj } from "@storybook/react";
+import globalConfigArgs, {
+  argTypes as globalConfigArgTypes
+} from "../../core/storybook/globalConfigArgs";
 import globalTextArgs, {
-  GlobalEntryTextProps
+  argTypes as globalTextArgTypes
 } from "../../core/storybook/globalTextArgs";
-import serviceUrlArgs from "../../core/storybook/serviceUrlArgs";
-import OpeningHours, { OpeningHoursEntryProps } from "./OpeningHours.entry";
+import serviceUrlArgs, {
+  argTypes as serviceUrlArgTypes
+} from "../../core/storybook/serviceUrlArgs";
+import OpeningHours from "./OpeningHours.entry";
 
-export default {
+const meta: Meta<typeof OpeningHours> = {
   title: "Apps / Opening Hours",
   component: OpeningHours,
   argTypes: {
+    ...globalConfigArgTypes,
+    ...globalTextArgTypes,
+    ...serviceUrlArgTypes,
     initialDate: {
       defaultValue: new Date("2024-03-25"),
       control: { type: "date" }
@@ -34,15 +40,30 @@ export default {
     openingHoursHeadingText: {
       defaultValue: "Opening hours",
       control: { type: "text" }
-    },
+    }
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof OpeningHours>;
+
+export const Primary: Story = {
+  args: {
     ...globalConfigArgs,
     ...globalTextArgs,
-    ...serviceUrlArgs
+    ...serviceUrlArgs,
+    initialDate: new Date("2024-03-25"),
+    branchId: 12,
+    showOpeningHoursForWeekText: "Show opening hours for week",
+    weekText: "Week",
+    libraryIsClosedText: "The library is closed this day",
+    openingHoursHeadingText: "Opening hours"
   }
-} as Meta<typeof OpeningHours>;
+};
 
-export const Default: StoryFn<typeof OpeningHours> = (
-  args: OpeningHoursEntryProps & GlobalEntryTextProps
-) => <OpeningHours {...args} />;
-
-export const materialWithoutType = Default.bind({});
+export const materialWithoutType: Story = {
+  args: {
+    ...Primary.args
+  }
+};
