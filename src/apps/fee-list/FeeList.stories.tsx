eaa696371c9152d4284f8f6523c01248cbd2cd73
thomasgross <thomasgrossrasmussen@gmail.com>
2024-09-05 10:46:17 +0200

    chore: Update argTypes and rewrite args for FeeList.stories.tsx

--- src/apps/fee-list/FeeList.stories.tsx
+++ src/apps/fee-list/FeeList.stories.tsx
@@ -1,20 +1,182 @@
-import React from "react";
-import type { Meta, StoryFn } from "@storybook/react";
-import blockedArgs from "../../core/storybook/blockedArgs";
-import serviceUrlArgs from "../../core/storybook/serviceUrlArgs";
+import type { Meta, StoryObj } from "@storybook/react";
+import blockedArgs, {
+  argTypes as blockedArgTypes
+} from "../../core/storybook/blockedArgs";
+import serviceUrlArgs, {
+  argTypes as serviceUrlArgTypes
+} from "../../core/storybook/serviceUrlArgs";
 import FeeList from "./FeeList.entry";
 import { getModalIds } from "../../core/utils/helpers/modal-helpers";
-import globalTextArgs from "../../core/storybook/globalTextArgs";
-import globalConfigArgs from "../../core/storybook/globalConfigArgs";
-import groupModalArgs from "../../core/storybook/groupModalArgs";
-import groupModalLoansArgs from "../../core/storybook/loanGroupModalArgs";
-import materialDetailsModalArgs from "../../core/storybook/materialDetailsModalArgs";
-import renewalArgs from "../../core/storybook/renewalArgs";
+import globalTextArgs, {
+  argTypes as globalTextArgTypes
+} from "../../core/storybook/globalTextArgs";
+import globalConfigArgs, {
+  argTypes as globalConfigArgTypes
+} from "../../core/storybook/globalConfigArgs";
+import groupModalArgs, {
+  argTypes as groupModalArgTypes
+} from "../../core/storybook/groupModalArgs";
+import groupModalLoansArgs, {
+  argTypes as groupModalLoansArgTypes
+} from "../../core/storybook/loanGroupModalArgs";
+import materialDetailsModalArgs, {
+  argTypes as materialDetailsModalArgTypes
+} from "../../core/storybook/materialDetailsModalArgs";
+import renewalArgs, {
+  argTypes as renewalArgTypes
+} from "../../core/storybook/renewalArgs";
 
-export default {
+const meta: Meta<typeof FeeList> = {
   title: "Apps / Fee list",
   component: FeeList,
   argTypes: {
+    ...serviceUrlArgTypes,
+    ...blockedArgTypes,
+    ...globalTextArgTypes,
+    ...globalConfigArgTypes,
+    ...groupModalArgTypes,
+    ...groupModalLoansArgTypes,
+    ...materialDetailsModalArgTypes,
+    ...renewalArgTypes,
+    feeListHeadlineText: {
+      control: { type: "text" }
+    },
+    feeListBodyText: {
+      control: { type: "text" }
+    },
+    viewFeesAndCompensationRatesText: {
+      control: { type: "text" }
+    },
+    etAlText: {
+      control: { type: "text" }
+    },
+    totalFeeAmountText: {
+      control: { type: "text" }
+    },
+    otherMaterialsText: {
+      control: { type: "text" }
+    },
+    materialByAuthorText: {
+      control: { type: "text" }
+    },
+    materialAndAuthorText: {
+      control: { type: "text" }
+    },
+    feeListDaysText: {
+      control: { type: "text" }
+    },
+    payText: {
+      control: { type: "text" }
+    },
+    totalText: {
+      control: { type: "text" }
+    },
+    expirationWarningDaysBeforeConfig: {
+      control: { type: "text" }
+    },
+    iAcceptText: {
+      control: { type: "text" }
+    },
+    termsOfTradeText: {
+      control: { type: "text" }
+    },
+    unpaidFeesPayableByClientHeadlineText: {
+      control: { type: "text" }
+    },
+    unpaidFeesNotPayableByClientHeadlineText: {
+      control: { type: "text" }
+    },
+    alreadyPaidText: {
+      control: { type: "text" }
+    },
+    feePaymentModalHeaderText: {
+      control: { type: "text" }
+    },
+    feePaymentModalBodyText: {
+      control: { type: "text" }
+    },
+    feePaymentModalNoticeText: {
+      control: { type: "text" }
+    },
+    feePaymentModalGotoText: {
+      control: { type: "text" }
+    },
+    feePaymentModalCancelText: {
+      control: { type: "text" }
+    },
+    feeDetailsModalScreenReaderText: {
+      control: { type: "text" }
+    },
+    emptyFeeListText: {
+      control: { type: "text" }
+    },
+    feeDetailsModalCloseModalAriaLabelText: {
+      control: { type: "text" }
+    },
+    feeDetailsModalDescriptionText: {
+      control: { type: "text" }
+    },
+    turnedInText: {
+      control: { type: "text" }
+    },
+    plusXOtherMaterialsText: {
+      control: { type: "text" }
+    },
+    itemFeeAmountText: {
+      control: { type: "text" }
+    },
+    feeCreatedText: {
+      control: { type: "text" }
+    },
+    feeListAlreadyPaidInfoText: {
+      control: { type: "text" }
+    },
+    feeListAlreadyPaidSecondInfoText: {
+      control: { type: "text" }
+    },
+    feeListMaterialNumberText: {
+      control: { type: "text" }
+    },
+    feeListConfig: {
+      control: { type: "text" }
+    },
+    feeListYouHaveOverdueLoansText: {
+      control: { type: "text" }
+    },
+    feeListSeeYourOverdueLoansText: {
+      control: { type: "text" }
+    },
+    feeListSeeYourOverdueLoansAriaText: {
+      control: { type: "text" }
+    },
+    loansOverdueText: {
+      control: { type: "text" }
+    },
+    // Urls
+    physicalLoansUrl: {
+      control: { type: "text" }
+    },
+    feeListPaymentSiteUrl: {
+      control: { type: "text" }
+    },
+    availablePaymentTypesUrl: {
+      control: { type: "text" }
+    },
+    termsOfTradeUrl: {
+      control: { type: "text" }
+    },
+    viewFeesAndCompensationRatesUrl: {
+      control: { type: "text" }
+    }
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FeeList>;
+
+export const Primary: Story = {
+  args: {
     ...serviceUrlArgs,
     ...blockedArgs,
     ...globalTextArgs,
@@ -23,207 +185,71 @@ export default {
     ...groupModalLoansArgs,
     ...materialDetailsModalArgs,
     ...renewalArgs,
-    feeListHeadlineText: {
-      defaultValue: "Fees & Replacement costs",
-      control: { type: "text" }
-    },
-    feeListBodyText: {
-      defaultValue:
-        "Overdue fees and replacement costs that were created before 27/10/2020 can still be paid on this page.",
-      control: { type: "text" }
-    },
-    viewFeesAndCompensationRatesText: {
-      defaultValue: "See our fees and replacement costs",
-      control: { type: "text" }
-    },
-    etAlText: {
-      control: {
-        type: "text"
-      },
-      defaultValue: "et al."
-    },
-    totalFeeAmountText: {
-      defaultValue: "Fee",
-      control: { type: "text" }
-    },
-    otherMaterialsText: {
-      defaultValue: "Other materials",
-      control: { type: "text" }
-    },
-    materialByAuthorText: {
-      defaultValue: "By",
-      control: { type: "text" }
-    },
-    materialAndAuthorText: {
-      defaultValue: "and",
-      control: { type: "text" }
-    },
-    feeListDaysText: {
-      defaultValue: "Days",
-      control: { type: "text" }
-    },
-    payText: {
-      defaultValue: "Pay",
-      control: { type: "text" }
-    },
-    totalText: {
-      defaultValue: "Total @total",
-      control: { type: "text" }
-    },
-    expirationWarningDaysBeforeConfig: {
-      defaultValue: "6",
-      control: { type: "text" }
-    },
-    iAcceptText: {
-      defaultValue: "I accept the",
-      control: { type: "text" }
-    },
-    termsOfTradeText: {
-      defaultValue: "Terms of trade",
-      control: { type: "text" }
-    },
-    unpaidFeesPayableByClientHeadlineText: {
-      defaultValue: "Unsettled debt - paid on site",
-      control: { type: "text" }
-    },
-    unpaidFeesNotPayableByClientHeadlineText: {
-      defaultValue: "Unsettled debt - paid externally",
-      control: { type: "text" }
-    },
-    alreadyPaidText: {
-      defaultValue:
-        "Please note that paid fees are not registered up until 72 hours after your payment after which your debt is updated and your user unblocked if it has been blocked.",
-      control: { type: "text" }
-    },
-    feePaymentModalHeaderText: {
-      defaultValue: "Unpaid fees post 27/10 2020",
-      control: { type: "text" }
-    },
-    feePaymentModalBodyText: {
-      defaultValue: "You will be redirected to Mit Betalingsoverblik.",
-      control: { type: "text" }
-    },
-    feePaymentModalNoticeText: {
-      defaultValue: "Paid fees can take up to 24 hours to registrer.",
-      control: { type: "text" }
-    },
-    feePaymentModalGotoText: {
-      defaultValue: "Go to Mit Betalingsoverblik",
-      control: { type: "text" }
-    },
-    feePaymentModalCancelText: {
-      defaultValue: "Cancel",
-      control: { type: "text" }
-    },
-    feeDetailsModalScreenReaderText: {
-      defaultValue: "A modal containing details about a fee",
-      control: { type: "text" }
-    },
-    emptyFeeListText: {
-      defaultValue: "You have 0 unpaid fees or replacement costs",
-      control: { type: "text" }
-    },
-    feeDetailsModalCloseModalAriaLabelText: {
-      defaultValue: "Close fee details modal",
-      control: { type: "text" }
-    },
-    feeDetailsModalDescriptionText: {
-      defaultValue:
-        "Modal containing information about this element or group of elements fees",
-      control: { type: "text" }
-    },
-    turnedInText: {
-      control: {
-        type: "text"
-      },
-      defaultValue: "Turned in @date"
-    },
-    plusXOtherMaterialsText: {
-      control: {
-        type: "text"
-      },
-      defaultValue: "+ @amount other materials"
-    },
-    itemFeeAmountText: {
-      control: {
-        type: "text"
-      },
-      defaultValue: "Fee @fee"
-    },
-    feeCreatedText: {
-      control: {
-        type: "text"
-      },
-      defaultValue: "Fees charged @date"
-    },
-    feeListAlreadyPaidInfoText: {
-      defaultValue:
-        "Already paid? It can take up to 72 hours to register the transaction.",
-      control: { type: "text" }
-    },
-    feeListAlreadyPaidSecondInfoText: {
-      defaultValue:
-        "Already paid? It can take up to 72 hours to register the transaction. (not payable by user)",
-      control: { type: "text" }
-    },
-    feeListMaterialNumberText: {
-      defaultValue: "# @materialNumber",
-      control: { type: "text" }
-    },
-    feeListConfig: {
-      defaultValue: '{ "paymentSiteButtonLabel": "Go to payment page" }',
-      control: { type: "text" }
-    },
-    feeListYouHaveOverdueLoansText: {
-      defaultValue: `{"type":"plural","text":["You have an overdue loan. You may be charged fees upon handing in or renewing it.","You have overdue loans. You may be charged fees upon handing in or renewing them."]}`,
-      control: { type: "text" }
-    },
-    feeListSeeYourOverdueLoansText: {
-      defaultValue: "See your overdue loans",
-      control: { type: "text" }
-    },
-    feeListSeeYourOverdueLoansAriaText: {
-      defaultValue: "Go to the loan list page to see your overdue loans",
-      control: { type: "text" }
-    },
-    loansOverdueText: {
-      defaultValue: "Returned too late",
-      control: { type: "text" }
-    },
+    feeListHeadlineText: "Fees & Replacement costs",
+    feeListBodyText:
+      "Overdue fees and replacement costs that were created before 27/10/2020 can still be paid on this page.",
+    viewFeesAndCompensationRatesText: "See our fees and replacement costs",
+    etAlText: "et al.",
+    totalFeeAmountText: "Fee",
+    otherMaterialsText: "Other materials",
+    materialByAuthorText: "By",
+    materialAndAuthorText: "and",
+    feeListDaysText: "Days",
+    payText: "Pay",
+    totalText: "Total @total",
+    expirationWarningDaysBeforeConfig: "6",
+    iAcceptText: "I accept the",
+    termsOfTradeText: "Terms of trade",
+    unpaidFeesPayableByClientHeadlineText: "Unsettled debt - paid on site",
+    unpaidFeesNotPayableByClientHeadlineText:
+      "Unsettled debt - paid externally",
+    alreadyPaidText:
+      "Please note that paid fees are not registered up until 72 hours after your payment after which your debt is updated and your user unblocked if it has been blocked.",
+    feePaymentModalHeaderText: "Unpaid fees post 27/10 2020",
+    feePaymentModalBodyText: "You will be redirected to Mit Betalingsoverblik.",
+    feePaymentModalNoticeText:
+      "Paid fees can take up to 24 hours to registrer.",
+    feePaymentModalGotoText: "Go to Mit Betalingsoverblik",
+    feePaymentModalCancelText: "Cancel",
+    feeDetailsModalScreenReaderText: "A modal containing details about a fee",
+    emptyFeeListText: "You have 0 unpaid fees or replacement costs",
+    feeDetailsModalCloseModalAriaLabelText: "Close fee details modal",
+    feeDetailsModalDescriptionText:
+      "Modal containing information about this element or group of elements fees",
+    turnedInText: "Turned in @date",
+    plusXOtherMaterialsText: "+ @amount other materials",
+    itemFeeAmountText: "Fee @fee",
+    feeCreatedText: "Fees charged @date",
+    feeListAlreadyPaidInfoText:
+      "Already paid? It can take up to 72 hours to register the transaction.",
+    feeListAlreadyPaidSecondInfoText:
+      "Already paid? It can take up to 72 hours to register the transaction. (not payable by user)",
+    feeListMaterialNumberText: "# @materialNumber",
+    feeListConfig: '{ "paymentSiteButtonLabel": "Go to payment page" }',
+    feeListYouHaveOverdueLoansText:
+      '{"type":"plural","text":["You have an overdue loan. You may be charged fees upon handing in or renewing it.","You have overdue loans. You may be charged fees upon handing in or renewing them."]}',
+    feeListSeeYourOverdueLoansText: "See your overdue loans",
+    feeListSeeYourOverdueLoansAriaText:
+      "Go to the loan list page to see your overdue loans",
+    loansOverdueText: "Returned too late",
     // Urls
-    physicalLoansUrl: {
-      defaultValue: "/user/me/loans",
-      control: { type: "text" }
-    },
-    feeListPaymentSiteUrl: {
-      defaultValue: "https://google.com",
-      control: { type: "text" }
-    },
-    availablePaymentTypesUrl: {
-      defaultValue: "https://unsplash.com/photos/JDzoTGfoogA", // Open source image of an adventurous duck
-      control: { type: "text" }
-    },
-    termsOfTradeUrl: {
-      defaultValue: "https://unsplash.com/photos/JDzoTGfoogA", // Open source image of an adventurous duck
-      control: { type: "text" }
-    },
-    viewFeesAndCompensationRatesUrl: {
-      defaultValue: "https://unsplash.com/photos/NEJcmvLFcws", // Open source image of a curious giraffe
-      control: { type: "text" }
-    }
+    physicalLoansUrl: "/user/me/loans",
+    feeListPaymentSiteUrl: "https://google.com",
+    availablePaymentTypesUrl: "https://unsplash.com/photos/JDzoTGfoogA",
+    termsOfTradeUrl: "https://unsplash.com/photos/JDzoTGfoogA",
+    viewFeesAndCompensationRatesUrl: "https://unsplash.com/photos/NEJcmvLFcws"
   }
-} as Meta<typeof FeeList>;
+};
 
-const Template: StoryFn<typeof FeeList> = (props) => <FeeList {...props} />;
-
-export const FeeListEntry = Template.bind({});
-
-FeeListEntry.args = {};
 const { feeDetails } = getModalIds();
 
-export const FeeListFeeDetailsModal = Template.bind({});
-FeeListFeeDetailsModal.parameters = {
-  query: {
-    modal: `${feeDetails}48724566`
+export const FeeListFeeDetailsModal: Story = {
+  parameters: {
+    query: {
+      modal: `${feeDetails}48724566`
+    }
+  },
+  args: {
+    ...Primary.args
   }
 };
