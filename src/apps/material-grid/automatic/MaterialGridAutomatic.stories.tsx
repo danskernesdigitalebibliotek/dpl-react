fe6e138a6d3c2c3149ab9bff227d627856eabd9e
thomasgross <thomasgrossrasmussen@gmail.com>
2024-09-05 11:52:10 +0200

    chore: Update argTypes and rewrite args for MaterialGridAutomatic.stories.tsx

--- src/apps/material-grid/automatic/MaterialGridAutomatic.stories.tsx
+++ src/apps/material-grid/automatic/MaterialGridAutomatic.stories.tsx
@@ -1,92 +1,98 @@
-import type { Meta, StoryFn } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
-
 import globalTextArgs, {
-  GlobalEntryTextProps
+  argTypes as globalTextArgTypes
 } from "../../../core/storybook/globalTextArgs";
-import serviceUrlArgs from "../../../core/storybook/serviceUrlArgs";
-
-import MaterialGridAutomatic, {
-  MaterialGridAutomaticEntryProps
-} from "./MaterialGridAutomatic.entry";
+import serviceUrlArgs, {
+  argTypes as serviceUrlArgTypes
+} from "../../../core/storybook/serviceUrlArgs";
+import MaterialGridAutomatic from "./MaterialGridAutomatic.entry";
 import MaterialGridSkeleton from "../MaterialGridSkeleton";
 
-export default {
+const meta: Meta<typeof MaterialGridAutomatic> = {
   title: "Apps / Material Grid / Automatic",
   component: MaterialGridAutomatic,
   argTypes: {
+    ...globalTextArgTypes,
+    ...serviceUrlArgTypes,
     title: {
-      name: "Title",
-      defaultValue: "Recommended materials",
+      description: "Title",
       control: { type: "text" }
     },
     description: {
-      name: "Description",
-      defaultValue:
-        "This is a long description of the materials selected, or whatever else you want to put in here",
+      description: "Description",
       control: { type: "text" }
     },
     cql: {
-      name: "CQL Search String",
-      defaultValue: "'heste' OR 'PIPPI'",
       control: { type: "text" },
       description:
         "CQL search string to use for the material grid, search for a result and copy the CQL string from an advanced search"
     },
     selectedAmountOfMaterialsForDisplay: {
-      name: "Amount of materials to show",
-      defaultValue: 12,
+      description: "Amount of materials to show",
       control: {
         type: "select",
         options: [4, 8, 12, 16, 20, 24, 28, 32]
       }
     },
     buttonText: {
-      name: "Button text",
-      defaultValue: "Show all",
+      description: "Button text",
       control: { type: "text" }
     },
     materialUrl: {
-      name: "Path to the material page",
-      defaultValue: "/work/:workid",
+      description: "Path to the material page",
       control: { type: "text" }
     },
     etAlText: {
-      name: "Et al. Text",
-      defaultValue: "et al.",
+      description: "Et al. Text",
       control: { type: "text" }
     },
-    ...globalTextArgs,
-    ...serviceUrlArgs,
     blacklistedPickupBranchesConfig: {
-      name: "Blacklisted Pickup branches",
-      defaultValue: "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024",
+      description: "Blacklisted Pickup branches",
       control: { type: "text" }
     },
     blacklistedAvailabilityBranchesConfig: {
-      name: "Blacklisted Availability branches",
-      defaultValue: "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024",
+      description: "Blacklisted Availability branches",
       control: { type: "text" }
     },
     blacklistedSearchBranchesConfig: {
-      name: "Blacklisted branches",
-      defaultValue: "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024",
+      description: "Blacklisted branches",
       control: { type: "text" }
     },
     branchesConfig: {
-      name: "Branches",
-      defaultValue:
-        '[\n   {\n      "branchId":"DK-775120",\n      "title":"Højbjerg"\n   },\n   {\n      "branchId":"DK-775122",\n      "title":"Beder-Malling"\n   },\n   {\n      "branchId":"DK-775144",\n      "title":"Gellerup"\n   },\n   {\n      "branchId":"DK-775167",\n      "title":"Lystrup"\n   },\n   {\n      "branchId":"DK-775146",\n      "title":"Harlev"\n   },\n   {\n      "branchId":"DK-775168",\n      "title":"Skødstrup"\n   },\n   {\n      "branchId":"FBS-751010",\n      "title":"Arresten"\n   },\n   {\n      "branchId":"DK-775147",\n      "title":"Hasle"\n   },\n   {\n      "branchId":"FBS-751032",\n      "title":"Må ikke benyttes"\n   },\n   {\n      "branchId":"FBS-751031",\n      "title":"Fjernlager 1"\n   },\n   {\n      "branchId":"DK-775126",\n      "title":"Solbjerg"\n   },\n   {\n      "branchId":"FBS-751030",\n      "title":"ITK"\n   },\n   {\n      "branchId":"DK-775149",\n      "title":"Sabro"\n   },\n   {\n      "branchId":"DK-775127",\n      "title":"Tranbjerg"\n   },\n   {\n      "branchId":"DK-775160",\n      "title":"Risskov"\n   },\n   {\n      "branchId":"DK-775162",\n      "title":"Hjortshøj"\n   },\n   {\n      "branchId":"DK-775140",\n      "title":"Åby"\n   },\n   {\n      "branchId":"FBS-751009",\n      "title":"Fjernlager 2"\n   },\n   {\n      "branchId":"FBS-751029",\n      "title":"Stadsarkivet"\n   },\n   {\n      "branchId":"FBS-751027",\n      "title":"Intern"\n   },\n   {\n      "branchId":"FBS-751026",\n      "title":"Fælles undervejs"\n   },\n   {\n      "branchId":"FBS-751025",\n      "title":"Fællessekretariatet"\n   },\n   {\n      "branchId":"DK-775133",\n      "title":"Bavnehøj"\n   },\n   {\n      "branchId":"FBS-751024",\n      "title":"Fjernlånte materialer"\n   },\n   {\n      "branchId":"DK-775100",\n      "title":"Hovedbiblioteket"\n   },\n   {\n      "branchId":"DK-775170",\n      "title":"Trige"\n   },\n   {\n      "branchId":"DK-775150",\n      "title":"Tilst"\n   },\n   {\n      "branchId":"DK-775130",\n      "title":"Viby"\n   },\n   {\n      "branchId":"DK-775164",\n      "title":"Egå"\n   }\n]',
+      description: "Branches",
       control: { type: "text" }
     }
   }
-} as Meta<typeof MaterialGridAutomatic>;
-
-export const App: StoryFn<typeof MaterialGridAutomatic> = (
-  args: MaterialGridAutomaticEntryProps & GlobalEntryTextProps
-) => <MaterialGridAutomatic {...args} />;
-
-const SkeletonTemplate: StoryFn<typeof MaterialGridAutomatic> = () => {
-  return <MaterialGridSkeleton />;
 };
-export const Skeleton = SkeletonTemplate.bind({});
+
+export default meta;
+
+type Story = StoryObj<typeof MaterialGridAutomatic>;
+
+export const Primary: Story = {
+  args: {
+    ...globalTextArgs,
+    ...serviceUrlArgs,
+    title: "Recommended materials",
+    description:
+      "This is a long description of the materials selected, or whatever else you want to put in here",
+    cql: "'heste' OR 'PIPPI'",
+    selectedAmountOfMaterialsForDisplay: 12,
+    buttonText: "Show all",
+    materialUrl: "/work/:workid",
+    etAlText: "et al.",
+    blacklistedPickupBranchesConfig:
+      "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024",
+    blacklistedAvailabilityBranchesConfig:
+      "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024",
+    blacklistedSearchBranchesConfig:
+      "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024",
+    branchesConfig:
+      '[\n   {\n      "branchId":"DK-775120",\n      "title":"Højbjerg"\n   },\n   {\n      "branchId":"DK-775122",\n      "title":"Beder-Malling"\n   },\n   {\n      "branchId":"DK-775144",\n      "title":"Gellerup"\n   },\n   {\n      "branchId":"DK-775167",\n      "title":"Lystrup"\n   },\n   {\n      "branchId":"DK-775146",\n      "title":"Harlev"\n   },\n   {\n      "branchId":"DK-775168",\n      "title":"Skødstrup"\n   },\n   {\n      "branchId":"FBS-751010",\n      "title":"Arresten"\n   },\n   {\n      "branchId":"DK-775147",\n      "title":"Hasle"\n   },\n   {\n      "branchId":"FBS-751032",\n      "title":"Må ikke benyttes"\n   },\n   {\n      "branchId":"FBS-751031",\n      "title":"Fjernlager 1"\n   },\n   {\n      "branchId":"DK-775126",\n      "title":"Solbjerg"\n   },\n   {\n      "branchId":"FBS-751030",\n      "title":"ITK"\n   },\n   {\n      "branchId":"DK-775149",\n      "title":"Sabro"\n   },\n   {\n      "branchId":"DK-775127",\n      "title":"Tranbjerg"\n   },\n   {\n      "branchId":"DK-775160",\n      "title":"Risskov"\n   },\n   {\n      "branchId":"DK-775162",\n      "title":"Hjortshøj"\n   },\n   {\n      "branchId":"DK-775140",\n      "title":"Åby"\n   },\n   {\n      "branchId":"FBS-751009",\n      "title":"Fjernlager 2"\n   },\n   {\n      "branchId":"FBS-751029",\n      "title":"Stadsarkivet"\n   },\n   {\n      "branchId":"FBS-751027",\n      "title":"Intern"\n   },\n   {\n      "branchId":"FBS-751026",\n      "title":"Fælles undervejs"\n   },\n   {\n      "branchId":"FBS-751025",\n      "title":"Fællessekretariatet"\n   },\n   {\n      "branchId":"DK-775133",\n      "title":"Bavnehøj"\n   },\n   {\n      "branchId":"FBS-751024",\n      "title":"Fjernlånte materialer"\n   },\n   {\n      "branchId":"DK-775100",\n      "title":"Hovedbiblioteket"\n   },\n   {\n      "branchId":"DK-775170",\n      "title":"Trige"\n   },\n   {\n      "branchId":"DK-775150",\n      "title":"Tilst"\n   },\n   {\n      "branchId":"DK-775130",\n      "title":"Viby"\n   },\n   {\n      "branchId":"DK-775164",\n      "title":"Egå"\n   }\n]'
+  }
+};
+
+export const Skeleton: Story = {
+  render: () => <MaterialGridSkeleton />
+};
