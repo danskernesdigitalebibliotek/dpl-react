526edca83c777951e496bc938f00409906d6f209
thomasgross <thomasgrossrasmussen@gmail.com>
2024-09-06 13:49:45 +0200

    chore: Update argTypes and rewrite args for availability-label.stories.tsx

--- src/components/availability-label/availability-label.stories.tsx
+++ src/components/availability-label/availability-label.stories.tsx
@@ -1,21 +1,18 @@
-import type { StoryFn, Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import { AccessTypeCode } from "../../core/dbc-gateway/generated/graphql";
 import serviceUrlArgs from "../../core/storybook/serviceUrlArgs";
 import { withConfig } from "../../core/utils/config";
 import { getCurrentLocation } from "../../core/utils/helpers/url";
 import { withUrls } from "../../core/utils/url";
-import {
-  AvailabilityLabelProps,
-  AvailabilityLabel
-} from "./availability-label";
+import { AvailabilityLabel } from "./availability-label";
 import globalTextArgs from "../../core/storybook/globalTextArgs";
 import globalConfigArgs from "../../core/storybook/globalConfigArgs";
 
 // The configuration below addresses the different variables,
 // their default values, and how they translate into storybook
 // controls.
-export default {
+const meta: Meta<typeof AvailabilityLabel> = {
   title: "Components/Availability Label",
   component: AvailabilityLabel,
   argTypes: {
@@ -62,46 +59,63 @@ export default {
     }
   },
   args: {
+    ...serviceUrlArgs,
+    ...globalTextArgs,
+    ...globalConfigArgs,
+    faustIds: ["62523611"],
+    blacklistedAvailabilityBranchesConfig:
+      "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024",
+    cursorPointer: false,
+    dataCy: "",
+    isbns: [],
+    accessTypes: [],
     manifestText: "Bog",
     availabilityText: "Hjemme",
     url: new URL("/", getCurrentLocation()),
     selected: false
+  },
+  decorators: [
+    (Story) => {
+      const DecoratedStory = withUrls(withConfig(Story));
+      return <DecoratedStory />;
+    }
+  ]
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AvailabilityLabel>;
+
+export const Available: Story = {
+  args: {
+    faustIds: ["61435867"]
   }
 };
 
-const Template: StoryFn<typeof AvailabilityLabel> = (
-  args: AvailabilityLabelProps
-) => {
-  const ConfiguredAvailabilityLabel = withUrls(withConfig(AvailabilityLabel));
-  return <ConfiguredAvailabilityLabel {...args} />;
+export const MoreThanOneID: Story = {
+  args: {
+    faustIds: ["62523611", "62150041", "61435867"]
+  }
 };
 
-export const Available = Template.bind({});
-Available.args = {
-  faustIds: ["61435867"]
+export const Selected: Story = {
+  args: {
+    faustIds: ["62523611"],
+    manifestText: "lydbog (cd-mp3)",
+    selected: true
+  }
 };
 
-export const MoreThanOneID = Template.bind({});
-MoreThanOneID.args = {
-  faustIds: ["62523611", "62150041", "61435867"]
+export const Unavailable: Story = {
+  args: {
+    faustIds: ["62523611"],
+    manifestText: "ebog"
+  }
 };
 
-export const Selected = Template.bind({});
-Selected.args = {
-  faustIds: ["62523611"],
-  manifestText: "lydbog (cd-mp3)",
-  selected: true
-};
-
-export const Unavailable = Template.bind({});
-Unavailable.args = {
-  faustIds: ["62523611"],
-  manifestText: "ebog"
-};
-
-export const EBogPrinsenHarry = Template.bind({});
-EBogPrinsenHarry.args = {
-  isbns: ["9788763844123"],
-  manifestText: "ebog",
-  accessTypes: [AccessTypeCode.Online]
+export const EBogPrinsenHarry: Story = {
+  args: {
+    isbns: ["9788763844123"],
+    manifestText: "ebog"
+  }
 };
