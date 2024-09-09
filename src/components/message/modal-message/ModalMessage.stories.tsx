a46d9e74ae01d75398f52eadee1aba3568abad18
thomasgross <thomasgrossrasmussen@gmail.com>
2024-09-09 11:39:35 +0200

    chore: Update ModalMessage component and its stories

--- src/components/message/modal-message/ModalMessage.stories.tsx
+++ src/components/message/modal-message/ModalMessage.stories.tsx
@@ -1,24 +1,8 @@
-import type { Meta, StoryFn } from "@storybook/react";
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
 import React, { useEffect } from "react";
 import Modal, { useModalButtonHandler } from "../../../core/utils/modal";
 import ModalMessage from "./ModalMessage";
 import globalTextArgs from "../../../core/storybook/globalTextArgs";
-
-export default {
-  title: "Components / Message / Modal Message",
-  argTypes: {
-    ...globalTextArgs,
-    title: {
-      defaultValue: "This is a title",
-      control: { type: "text" }
-    },
-    subTitle: {
-      defaultValue: "This is a subtitle",
-      control: { type: "text" }
-    }
-  },
-  component: ModalMessage
-} as Meta<typeof ModalMessage>;
 
 const Template: StoryFn<typeof ModalMessage> = (props) => {
   const { open } = useModalButtonHandler();
@@ -44,4 +28,30 @@ const Template: StoryFn<typeof ModalMessage> = (props) => {
   );
 };
 
-export const ModalMessageExample = Template.bind({});
+const meta: Meta<typeof ModalMessage> = {
+  title: "Components / Message / Modal Message",
+  component: ModalMessage,
+  argTypes: {
+    ...globalTextArgs,
+    title: {
+      control: { type: "text" }
+    },
+    subTitle: {
+      control: { type: "text" }
+    }
+  },
+  args: {
+    ...globalTextArgs,
+    title: "This is a title",
+    subTitle: "This is a subtitle"
+  },
+  render: (args) => {
+    return <Template {...args} />;
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ModalMessage>;
+
+export const ModalMessageExample: Story = {};
