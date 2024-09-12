c1b4a3c4840e227664d30aec3f6622b9c533d3c5
Ivan Danilov <ida@inleadmedia.com>
2024-09-12 19:15:34 +0300

    EONEXT-48 - Added hint information block for zero results.

--- src/apps/search-result/search-result-zero-hits.tsx
+++ src/apps/search-result/search-result-zero-hits.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { FC } from "react";
 import { useText } from "../../core/utils/text";
+import "./no-results.scss";
 
 export interface SearchResultZeroHitsProps {
   dataCy?: string;
@@ -10,6 +11,15 @@ const SearchResultZeroHits: FC<SearchResultZeroHitsProps> = ({
   dataCy = "search-result-zero-hits"
 }) => {
   const t = useText();
+  const searchAppElement = document.querySelector(
+    '[data-dpl-app="search-result"]'
+  );
+  const noResultsHeading =
+    searchAppElement?.getAttribute("data-no-results-heading") || "";
+  const noResultsText =
+    searchAppElement?.getAttribute("data-no-results-text") || "";
+  const noResultsEnabled =
+    searchAppElement?.getAttribute("data-no-results-enabled") === "true";
 
   return (
     <div className="content-list-page" data-cy={dataCy}>
@@ -19,6 +29,12 @@ const SearchResultZeroHits: FC<SearchResultZeroHitsProps> = ({
       >
         {t("noSearchResultText")}
       </h1>
+      {noResultsEnabled && (
+        <div className="content-list-page--help">
+          <h2 className="content-list-page--help-title">{noResultsHeading}</h2>
+          <p className="content-list-page--help-description">{noResultsText}</p>
+        </div>
+      )}
     </div>
   );
 };
