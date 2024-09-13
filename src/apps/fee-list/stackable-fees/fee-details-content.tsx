9264cace0112d57e26058be6891ae1e41b7dbbae
Ivan Danilov <ida@inleadmedia.com>
2024-09-13 12:14:56 +0300

    EONEXT-45 - Fix errors for Github build.

--- src/apps/fee-list/stackable-fees/fee-details-content.tsx
+++ src/apps/fee-list/stackable-fees/fee-details-content.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { FC } from "react";
 import { FeeV2 } from "../../../core/fbs/model";
 import { useText } from "../../../core/utils/text";
-import { dateFormatCustom } from "../../../core/configuration/date-format.json";
+import dateFormat from "../../../core/configuration/date-format.json";
 import StackableFeesList from "./stackable-fees-list";
 import GroupModalContent from "../../../components/GroupModal/GroupModalContent";
 import { formatCurrency } from "../../../core/utils/helpers/currency";
@@ -11,6 +11,8 @@ import { formatCurrency } from "../../../core/utils/helpers/currency";
 export interface FeeDetailsContentProps {
   feeDetailsData: FeeV2;
 }
+
+const { dateFormatCustom } = dateFormat;
 
 const FeeDetailsContent: FC<FeeDetailsContentProps> = ({ feeDetailsData }) => {
   const t = useText();
