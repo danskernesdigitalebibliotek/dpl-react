2d6ea05793b145413ddafad26dc8bd97f71fe1bb
Jacob Pihl <jacob@reload.dk>
2024-10-24 13:22:48 +0200

    Upgrade Cypress to version 10
    IntegrationFolder is now deprecated and the config file should instead rely on Testing Type-Specific Options
    https://docs.cypress.io/app/references/configuration#Testing-Type-Specific-Options

--- cypress.config.js
+++ cypress.config.js
@@ -6,13 +6,13 @@ module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 10000,
   requestTimeout: 30000,
-  testFiles: "@(apps|components)/**/*.test.@(ts|tsx)",
-  integrationFolder: "./src",
   retries: {
     runMode: 3,
     openMode: 0
   },
   e2e: {
+    supportFile: "cypress/support/index.ts",
+    specPattern: "./src/@(apps|components)/**/*.test.@(ts|tsx)",
     baseUrl: "http://localhost:57021"
   }
 });
