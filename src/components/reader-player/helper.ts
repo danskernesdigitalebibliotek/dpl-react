25a2eda304f87604a9c0f41194c1ac71f0dae561
Kasper Birch <kasperbirch1@gmail.com>
2024-11-19 14:29:14 +0100

    Add `Reader` + Story
    The `Reader` component supports both teaser and reserved material.
    Use the `identifier` for teaser and `orderId` for reserved.
    
    To load the reader, it is necessary to attach `readerAssets` to the `<head>` dynamically. A function called `appendAsset` has been introduced to manage this process.
    
    Currently, there is a problem where `readerAssets` need to be reloaded each time the reader is initialized. As a temporary workaround, `window.location.reload();` is being used. A better solution will be implemented in the future.

---
+++ src/components/reader-player/helper.ts
@@ -0,0 +1,63 @@
+type AssetType = {
+  src: string;
+  type: "script" | "link";
+  id: string;
+};
+
+export const readerAssets: AssetType[] = [
+  {
+    src: "https://reader.pubhub.dk/2.2.0/js/chunk-vendors.js",
+    type: "script",
+    id: "reader-chunk-vendors-js"
+  },
+  {
+    src: "https://reader.pubhub.dk/2.2.0/js/app.js",
+    type: "script",
+    id: "reader-app-js"
+  },
+  {
+    src: "https://reader.pubhub.dk/2.2.0/css/chunk-vendors.css",
+    type: "link",
+    id: "reader-chunk-vendors-css"
+  },
+  {
+    src: "https://reader.pubhub.dk/2.2.0/css/app.css",
+    type: "link",
+    id: "reader-app-css"
+  }
+];
+
+const appendedAssets = new Set<HTMLElement>();
+
+export const appendAsset = ({ src, type, id }: AssetType) => {
+  if (type === "script") {
+    const scriptElement = document.createElement("script");
+    scriptElement.src = src;
+    scriptElement.defer = true;
+    scriptElement.async = false;
+    scriptElement.type = "module";
+    scriptElement.id = id;
+    document.head.appendChild(scriptElement);
+    appendedAssets.add(scriptElement);
+  }
+
+  if (type === "link") {
+    const linkElement = document.createElement("link");
+    linkElement.href = src;
+    linkElement.rel = "stylesheet";
+    linkElement.id = id;
+    document.head.appendChild(linkElement);
+    appendedAssets.add(linkElement);
+  }
+};
+
+export const removeAppendedAssets = () => {
+  appendedAssets.forEach((element) => {
+    if (document.head.contains(element)) {
+      document.head.removeChild(element);
+    }
+    appendedAssets.delete(element);
+  });
+};
+
+export default {};
