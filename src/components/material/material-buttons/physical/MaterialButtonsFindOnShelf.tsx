b8acece9494c4d277eb1f2e0d7b3ea327ed48584
Kasper Birch <kasperbirch1@gmail.com>
2024-11-20 11:25:48 +0100

    Introduce `MaterialSecondaryButton`
    To ensure the `MaterialButtonReaderTeaser` and `MaterialButtonsFindOnShelf` have identical behavior, I created a shared component to eliminate code duplication.

--- src/components/material/material-buttons/physical/MaterialButtonsFindOnShelf.tsx
+++ src/components/material/material-buttons/physical/MaterialButtonsFindOnShelf.tsx
@@ -1,10 +1,9 @@
-import * as React from "react";
-import { FC } from "react";
+import React, { FC } from "react";
 import { useModalButtonHandler } from "../../../../core/utils/modal";
 import { useText } from "../../../../core/utils/text";
 import { ButtonSize } from "../../../../core/utils/types/button";
 import { FaustId } from "../../../../core/utils/types/ids";
-import { Button } from "../../../Buttons/Button";
+import MaterialSecondaryButton from "../generic/MaterialSecondaryButton";
 import { findOnShelfModalId } from "../../../find-on-shelf/FindOnShelfModal";
 
 export interface MaterialButtonsFindOnShelfProps {
@@ -21,43 +20,19 @@ const MaterialButtonsFindOnShelf: FC<MaterialButtonsFindOnShelfProps> = ({
   const t = useText();
   const { open } = useModalButtonHandler();
   const modalId = findOnShelfModalId(faustIds);
+
   const onClick = () => {
     open(modalId);
   };
-  // If element is currently focused on, we would like to let users open it using enter
-  const onKeyUp = (key: string) => {
-    if (key === "Enter") {
-      onClick();
-    }
-  };
-
-  if (size !== "small") {
-    return (
-      <Button
-        label={t("findOnBookshelfText")}
-        buttonType="none"
-        variant="outline"
-        disabled={false}
-        collapsible={false}
-        size="large"
-        onClick={onClick}
-        dataCy={dataCy}
-      />
-    );
-  }
 
   return (
-    <button
-      className="link-tag text-small-caption material-manifestation-item__find capitalize-all btn-ui"
-      aria-describedby={t("findOnShelfExpandButtonExplanationText")}
+    <MaterialSecondaryButton
+      label={t("findOnBookshelfText")}
+      size={size || "large"}
       onClick={onClick}
-      onKeyUp={(e) => onKeyUp(e.key)}
-      tabIndex={0}
-      type="button"
-      data-cy={dataCy}
-    >
-      {t("findOnBookshelfText")}
-    </button>
+      dataCy={dataCy}
+      ariaDescribedBy={t("findOnShelfExpandButtonExplanationText")}
+    />
   );
 };
 
