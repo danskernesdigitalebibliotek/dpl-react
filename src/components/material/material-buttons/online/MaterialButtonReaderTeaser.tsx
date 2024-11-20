537e8c4efbdf8a9583e9a72334c16eb65bcfe001
Kasper Birch <kasperbirch1@gmail.com>
2024-11-20 11:25:48 +0100

    Introduce `MaterialSecondaryButton`
    To ensure the `MaterialButtonReaderTeaser` and `MaterialButtonsFindOnShelf` have identical behavior, I created a shared component to eliminate code duplication.

--- src/components/material/material-buttons/online/MaterialButtonReaderTeaser.tsx
+++ src/components/material/material-buttons/online/MaterialButtonReaderTeaser.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useModalButtonHandler } from "../../../../core/utils/modal";
 import { useText } from "../../../../core/utils/text";
 import { ButtonSize } from "../../../../core/utils/types/button";
-import { Button } from "../../../Buttons/Button";
+import MaterialSecondaryButton from "../generic/MaterialSecondaryButton";
 
 type MaterialButtonOnlineTeaserType = {
   identifier: string;
@@ -22,40 +22,14 @@ const MaterialButtonReaderTeaser = ({
     open(`reader-modal-${identifier}`);
   };
 
-  // If element is currently focused on, we would like to let users open it using enter
-  const onKeyUp = (key: string) => {
-    if (key === "Enter") {
-      onClick();
-    }
-  };
-
-  if (size !== "small") {
-    return (
-      <Button
-        label={t("onlineMaterialTeaserText")}
-        buttonType="none"
-        variant="outline"
-        disabled={false}
-        collapsible={false}
-        size="large"
-        onClick={onClick}
-        dataCy={dataCy}
-      />
-    );
-  }
-
   return (
-    <button
-      className="link-tag text-small-caption material-manifestation-item__find capitalize-all btn-ui"
-      aria-describedby={t("onlineMaterialTeaserText")}
+    <MaterialSecondaryButton
+      label={t("onlineMaterialTeaserText")}
+      size={size}
       onClick={onClick}
-      onKeyUp={(e) => onKeyUp(e.key)}
-      tabIndex={0}
-      type="button"
-      data-cy={dataCy}
-    >
-      {t("onlineMaterialTeaserText")}
-    </button>
+      dataCy={dataCy}
+      ariaDescribedBy={t("onlineMaterialTeaserText")}
+    />
   );
 };
 
