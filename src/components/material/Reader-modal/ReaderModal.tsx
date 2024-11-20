c1ab716210bea173d2d5f8de834cd9204459363f
Kasper Birch <kasperbirch1@gmail.com>
2024-11-20 14:33:37 +0100

    Move `window.closeModal` logic into `ReaderModal`
    The addition of `closeModal` to the `window` object is only necessary in the `ReaderModal`, so it is unnecessary to keep it in the generic `Modal` component.

--- src/components/material/Reader-modal/ReaderModal.tsx
+++ src/components/material/Reader-modal/ReaderModal.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Modal from "../../../core/utils/modal";
 import { useText } from "../../../core/utils/text";
 import Reader from "../../reader-player/Reader";
+import { closeModal } from "../../../core/modal.slice";
 
 type ReaderModalType = {
   identifier: string;
@@ -9,6 +11,25 @@ type ReaderModalType = {
 
 const ReaderModal = ({ identifier }: ReaderModalType) => {
   const t = useText();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Attach the closeModal function to the window object to be able to close the modal
+    // from the reader / player.
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    window.closeModal = (modalId: string) => {
+      dispatch(closeModal({ modalId }));
+    };
+
+    return () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      delete window.closeModal;
+    };
+  }, [dispatch]);
+
   return (
     <Modal
       isFullScreen
