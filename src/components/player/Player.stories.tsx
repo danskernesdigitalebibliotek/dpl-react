3b75c58bb25137b058337a2a3435f8a8d73e902e
Kasper Birch <kasperbirch1@gmail.com>
2024-11-25 18:16:31 +0100

    Add `Player` Component + Story
    Implemented the `Player` component, which functions similarly to the `Reader`. Considering moving the `Reader` component to the `components` folder for consistency in folder structure.

---
+++ src/components/player/Player.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Player from "./Player";
+
+const meta: Meta<typeof Player> = {
+  title: "Components / Player",
+  component: Player,
+  argTypes: {
+    identifier: {
+      control: { type: "text" }
+    },
+    orderId: {
+      control: { type: "text" }
+    }
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Player>;
+
+export const WithIdentifier: Story = {
+  render: () => (
+    <div style={{ minHeight: "250px" }}>
+      <Player identifier="9788702366600" />
+    </div>
+  )
+};
+
+// Works only if the matrial is reserved
+export const WithOrderId: Story = {
+  render: () => (
+    <div style={{ minHeight: "250px" }}>
+      <Player orderId="" />
+    </div>
+  )
+};
