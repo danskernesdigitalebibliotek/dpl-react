6ef7ac30a74b67330b1272ccef6e47c0ec3e6274
thomasgross <thomasgrossrasmussen@gmail.com>
2025-01-08 16:06:04 +0100

    update mount function

--- src/core/mount.js
+++ src/core/mount.js
@@ -1,5 +1,5 @@
+import { createRoot } from "react-dom/client";
 import { createElement } from "react";
-import { render } from "react-dom";
 import { withErrorBoundary } from "react-error-boundary";
 import { setToken } from "./token";
 import Store from "../components/store";
@@ -24,10 +24,9 @@ function mount(context) {
     // Ensure that the application exists.
     const isValidMount = app;
     if (isValidMount) {
-      // Todo: This should be updated because render() is deprecated.
-      // After the update, ensure that prefixes (identifierPrefix) are specified for all apps.
-      // This will guarantee unique IDs everywhere useID() is utilized.
-      render(
+      const root = createRoot(container);
+
+      root.render(
         createElement(
           Store,
           {},
@@ -46,8 +45,7 @@ function mount(context) {
               ...container.dataset
             }
           )
-        ),
-        container
+        )
       );
     }
   }
