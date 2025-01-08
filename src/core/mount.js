b1458e564ea6d797e22e329be610c6629cd5a5af
thomasgross <thomasgrossrasmussen@gmail.com>
2025-01-08 20:53:51 +0100

    fix: update import statement for createRoot from react-dom/client to react-dom

--- src/core/mount.js
+++ src/core/mount.js
@@ -1,4 +1,4 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom";
 import { createElement } from "react";
 import { withErrorBoundary } from "react-error-boundary";
 import { setToken } from "./token";
