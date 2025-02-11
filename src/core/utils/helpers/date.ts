681719a1265965f22b87b77c56b64a7691023400
Kasper Birch <kasperbirch1@gmail.com>
2025-02-11 11:11:39 +0100

    Centralize date formatting logic in helper function (Part 3)

--- src/core/utils/helpers/date.ts
+++ src/core/utils/helpers/date.ts
@@ -7,16 +7,22 @@ import {
 
 const getCurrentUnixTime = () => Math.floor(Date.now() / 1000);
 
+// Date comparison functions
 export const dateHasPassed = (date: string) => {
   return dayjs().isAfter(date, "day");
 };
 
+// Date formatting functions
 export const formatDate = (date: string) => {
   return dayjs(date).format("DD-MM-YYYY");
 };
+
 export const formatDateTime = (date: string) => {
   return dayjs(date).format("DD-MM-YYYY HH:mm");
 };
+
+export const formatCustomDateString = (dateString: string) =>
+  dayjs(dateString).format(dateFormatCustom);
 
 export const formatDateDependingOnDigitalMaterial = ({
   date,
@@ -28,6 +34,10 @@ export const formatDateDependingOnDigitalMaterial = ({
   return isDigital ? formatDateTime(date) : formatDate(date);
 };
 
+// Date calculation functions: Get dates relative to today
+export const getMonthAgoDate = () =>
+  dayjs().subtract(1, "month").format(dateFormatDayjs);
+
 export const getYesterdayDate = () =>
   dayjs().subtract(1, "day").format(dateFormatDayjs);
 
@@ -36,8 +46,5 @@ export const getNextWeekDate = () =>
 
 export const getNextYearDate = () =>
   dayjs().add(1, "year").format(dateFormatDayjs);
-
-export const formatCustomDateString = (dateString: string) =>
-  dayjs(dateString).format(dateFormatCustom);
 
 export default getCurrentUnixTime;
