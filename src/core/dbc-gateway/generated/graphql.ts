f39e19759f404fde6dcba8f32cd135cec612cb4c
Kasper Birch <kasperbirch1@gmail.com>
2025-02-17 15:37:31 +0100

    Remove unnecessary naming script from DBC GraphQL codegen
    The `dbc-gateway.codegen.naming` script is no longer required and causes issues when executing `yarn codegen:dbc:gateway`.

--- src/core/dbc-gateway/generated/graphql.ts
+++ src/core/dbc-gateway/generated/graphql.ts
@@ -5,7 +5,6 @@ import {
   UseMutationOptions
 } from "react-query";
 import { fetcher } from "../graphql-fetcher";
-
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = {
@@ -1681,6 +1680,8 @@ export type Series = {
   alternativeTitles: Array<Scalars["String"]["output"]>;
   /** Description of the series */
   description?: Maybe<Scalars["String"]["output"]>;
+  /** The number of members in the series */
+  hitcount: Scalars["Int"]["output"];
   /** Additional information  */
   identifyingAddition?: Maybe<Scalars["String"]["output"]>;
   /** Whether this is a popular series or general series */
