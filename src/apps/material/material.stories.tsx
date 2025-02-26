598c50c34f9cc79c68bac44f9788689407ef802f
Kasper Birch <kasperbirch1@gmail.com>
2025-02-26 13:35:20 +0100

    Update reservation notification message for clarity and detail

--- src/apps/material/material.stories.tsx
+++ src/apps/material/material.stories.tsx
@@ -900,7 +900,7 @@ const meta: Meta<typeof MaterialEntry> = {
     onlineInternalSuccessLoanedText:
       "You can read/listen to the material until @expirationDate",
     onlineInternalSuccessReservedText:
-      "You will receive a message when reservation is met and the material is borrowed to you.",
+      "You have reserved the material. If you have provided an phonenumber or email during the reservation, you will receive a notification when the material is ready. Please note that the loan does not happen automatically – you must manually borrow the digital material yourself within 48 hours.",
     missingDataText: "Missing data",
     reservationModalScreenReaderModalDescriptionText: "Modal for reservation",
     reservationModalCloseModalAriaLabelText: "Close reservation modal",
