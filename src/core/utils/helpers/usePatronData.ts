ac6bf57db0031d167251862d131d252af2574d63
Kasper Garnæs <kasperg@users.noreply.github.com>
2025-04-25 20:06:51 +0200

    Use non-deprecated version of API to retrieve patron information
    useGetPatronInformationByPatronIdV2() is deprecated so we replace it
    with useGetPersonTypePatronInformationV2().
    
    The two API calls use different response formats so to preserve
    compatibility we convert responses from the new service to match
    responses from the old.

--- src/core/utils/helpers/usePatronData.ts
+++ src/core/utils/helpers/usePatronData.ts
@@ -1,18 +1,40 @@
-import { useGetPatronInformationByPatronIdV2 } from "../../fbs/fbs";
+import { useGetPersonTypePatronInformationV2 } from "../../fbs/fbs";
 import BlockedTypes from "../types/BlockedTypes";
 import { Patron } from "../types/entities";
 import { isAnonymous } from "./user";
 import type { QueryKey, UseQueryResult } from "react-query";
 import type { ErrorType } from "../../fbs/mutator/fetcher";
-import { AuthenticatedPatronV6 } from "../../fbs/model";
+import {
+  AuthenticatedPatronV6,
+  PatronV5,
+  PersonPatronV2
+} from "../../fbs/model";
+import { first } from "lodash";
 
+// Explicitly return the data structure of previous versions of the underlying
+// generated hook from FBS.
 export const usePatronData = (): UseQueryResult<
   Awaited<Promise<AuthenticatedPatronV6 | null>>,
   ErrorType<void>
 > & { queryKey: QueryKey } => {
-  return useGetPatronInformationByPatronIdV2({
-    enabled: !isAnonymous()
+  const result = useGetPersonTypePatronInformationV2({
+    enabled: !isAnonymous(),
+    // Transform the response data structure to match previous versions
+    select: (data: PersonPatronV2 | null): AuthenticatedPatronV6 | null => {
+      return data
+        ? {
+            // As we use the FBS adapter where we are only able to retrieve
+            // the patron matching the current authentication token it seems
+            // safe to assume that authentication is valid if we can retrieve
+            // data.
+            authenticateStatus: "VALID",
+            patron: convertPersonPatronV2ToPatronV5(data)
+          }
+        : null;
+    }
   });
+
+  return result;
 };
 
 export const getBlockedStatus = (patron?: Patron) => {
@@ -23,6 +45,19 @@ export const getBlockedStatus = (patron?: Patron) => {
   }
   // We cannot resolve the block status so we return unknown
   return BlockedTypes.unknown;
+};
+
+const convertPersonPatronV2ToPatronV5 = (person: PersonPatronV2): PatronV5 => {
+  return {
+    // PersonPatronV2 supports multiple email addresses and phone numbers with
+    // individual notifications. Reduce this to the first one to match the
+    // data structure of PatronV5.
+    emailAddress: first(person.emailAddresses)?.emailAddress,
+    receiveEmail: first(person.emailAddresses)?.receiveNotification ?? false,
+    phoneNumber: first(person.phoneNumbers)?.phoneNumber,
+    receiveSms: first(person.phoneNumbers)?.receiveNotification ?? false,
+    ...person
+  };
 };
 
 export default {};
