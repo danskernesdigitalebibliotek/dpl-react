f2ddaf6fe9e1120892f1a675d13d2a0b81ab45dd
LasseStaus <l.stausgaard@gmail.com>
2025-04-30 13:21:29 +0200

    Update `MaterialGridRelated` to have filters for switching displayed materials.
    The options are Recommendations, in Series, By author.
    
    The components for the filters are added in a separate commit.
    
    DDFSAL-1
    DDFSAL-4

---
+++ src/components/material-grid-related/helper.ts
@@ -0,0 +1,56 @@
+import {
+  WorkRecommendationsQuery,
+  ComplexSearchWithPaginationQuery
+} from "../../core/dbc-gateway/generated/graphql";
+import { WorkId } from "../../core/utils/types/ids";
+import { MaterialGridItemProps } from "../material-grid/MaterialGrid";
+import {
+  MaterialGridFilterOption,
+  MaterialGridFilterType
+} from "./MaterialGridRelated.types";
+
+export function prepareCreatorCql(
+  creators: string[] | undefined | null
+): string {
+  if (!creators || creators.length === 0) return "";
+
+  return creators.map((name) => `term.creator='${name}'`).join(" OR ");
+}
+
+export function extractRecommendationMaterials(
+  data?: WorkRecommendationsQuery
+): MaterialGridItemProps[] {
+  if (!data?.recommend?.result) return [];
+  return data.recommend.result.map(({ work }) => ({
+    wid: work.workId as WorkId
+  }));
+}
+
+export function extractSeriesMaterials(
+  data?: ComplexSearchWithPaginationQuery
+): MaterialGridItemProps[] {
+  if (!data?.complexSearch?.works) return [];
+  return data.complexSearch.works.map((work) => ({
+    wid: work.workId as WorkId
+  }));
+}
+
+export function extractAuthorMaterials(
+  data?: ComplexSearchWithPaginationQuery
+): MaterialGridItemProps[] {
+  if (!data?.complexSearch?.works) return [];
+  return data.complexSearch.works.map((work) => ({
+    wid: work.workId as WorkId
+  }));
+}
+
+export function getPreferredFallback(
+  options: MaterialGridFilterOption[]
+): MaterialGridFilterType | undefined {
+  const preferred: MaterialGridFilterType[] = [
+    "recommendation",
+    "series",
+    "author"
+  ];
+  return preferred.find((f) => options.some((o) => o.value === f));
+}
