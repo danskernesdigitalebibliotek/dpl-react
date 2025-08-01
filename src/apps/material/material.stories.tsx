9bb486610554a50820df5844ab6c344478b8dc65
Kasper Birch <kasperbirch1@gmail.com>
2025-08-01 13:38:43 +0200

    Add agencyIdConfig to MaterialEntryTextProps and stories

--- src/apps/material/material.stories.tsx
+++ src/apps/material/material.stories.tsx
@@ -51,6 +51,10 @@ const meta: Meta<typeof MaterialEntry> = {
       description: "Work ID",
       control: { type: "text" }
     },
+    agencyIdConfig: {
+      description: "Agency ID from OpenID Connect configuration",
+      control: { type: "text" }
+    },
     smsNotificationsForReservationsEnabledConfig: {
       description: "SMS notifications for reservations is enabled",
       control: { type: "text" }
@@ -844,6 +848,7 @@ const meta: Meta<typeof MaterialEntry> = {
     searchUrl: "/search",
     materialUrl: "/work/:workid",
     wid: "work-of:870970-basis:52557240",
+    agencyIdConfig: "710100",
     smsNotificationsForReservationsEnabledConfig: "1",
     blacklistedPickupBranchesConfig:
       "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024,DK-775164",
