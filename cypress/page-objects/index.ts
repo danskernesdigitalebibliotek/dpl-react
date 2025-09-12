2ea4ce195e1bfa8e93c7335d85561334f8c8c604
Kasper Birch <kasperbirch1@gmail.com>
2025-09-12 14:29:39 +0200

    Add POC for cypress page object
    - Added a 1-1 copy of demo modal test to show concept of cypress page object
    - Demonstrates proper separation of concerns:
      * Page object contains element locators
      * Test contains assertions and simple actions
    - Provides foundation for incremental migration to page object pattern

---
+++ cypress/page-objects/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Page Objects Index
+ *
+ * Central export point for all page objects used in Cypress tests.
+ * This provides a clean import path for consuming page objects in tests.
+ */
+
+export { DemoModalPage } from "./demo-modal.page";
+
+// Future page objects can be exported here following the same pattern:
+// export { SomeOtherPage } from "./some-other.page";
