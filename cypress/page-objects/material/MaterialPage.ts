a8388451184e87cc5f1fe47df8d027918bd963b4
Kasper Birch <kasperbirch1@gmail.com>
2025-10-23 15:45:04 +0200

    Update MaterialPage to use reorganized components
    Update imports and component references to match the new component structure. Simplify constructor by removing unused materialType parameter.
    
    Why:
    
    - Aligns with new component naming
    
    - Simpler interface (removed unnecessary parameter)
    
    - Clearer element selectors

--- cypress/page-objects/material/MaterialPage.ts
+++ cypress/page-objects/material/MaterialPage.ts
@@ -3,45 +3,85 @@ import {
   Elements,
   NestedComponents
 } from "@hammzj/cypress-page-object";
-import { EditionsComponent } from "./components/editions";
-import { FindOnShelfComponent } from "./components/find-on-shelf";
+import { DisclosureEditionsComponent } from "./components/disclosure-editions";
+import { ModalReservationComponent } from "./components/modal-reservation";
+import { ModalEditionsSwitchComponent } from "./components/modal-editions-switch";
+import { ModalFindOnShelfComponent } from "./components/modal-find-on-shelf";
 
 export class MaterialPage extends PageObject {
   public elements!: Elements;
   public components!: NestedComponents;
 
-  constructor(materialType: string = "bog") {
+  constructor() {
     super({
-      path: `/iframe.html?id=apps-material--default&viewMode=story&type=${materialType}`
+      path: `/iframe.html?id=apps-material--default&viewMode=story`
     });
     this.elements = {
       title: () => cy.get(".text-header-h1")
     };
 
     this.addElements = {
-      // Get all availability labels in the material header
-      // First scroll into view to activate lazy loading, then get the labels
       headerAvailabilityLabels: () => {
-        cy.get("[data-cy='material-header-content']").scrollIntoView();
         return cy.get(
           ".material-header__availability-label .availability-label"
         );
-      }
+      },
+
+      mainReservationButton: () =>
+        cy.getBySel("material-header-buttons-physical"),
+
+      // FindOnShelf button in the material header
+      findOnShelfButton: () =>
+        cy.getBySel("material-header-buttons-find-on-shelf")
     };
 
     this.addNestedComponents = {
-      Editions: (fn) =>
-        this.performWithin(this.container(), new EditionsComponent(), fn),
-      FindOnShelf: (fn) =>
-        this.performWithin(this.container(), new FindOnShelfComponent(), fn)
+      DisclosureEditions: (fn) =>
+        this.performWithin(
+          this.container(),
+          new DisclosureEditionsComponent(),
+          fn
+        ),
+      ModalReservation: (fn) =>
+        this.performWithin(
+          this.container(),
+          new ModalReservationComponent(),
+          fn
+        ),
+      ModalFindOnShelf: (fn) =>
+        this.performWithin(
+          this.container(),
+          new ModalFindOnShelfComponent(),
+          fn
+        ),
+      ModalEditionsSwitch: (fn) =>
+        this.performWithin(
+          this.container(),
+          new ModalEditionsSwitchComponent(),
+          fn
+        )
     };
   }
 
+  openModalReservation() {
+    this.elements.mainReservationButton().should("be.visible").click();
+  }
+
   getHeaderAvailabilityLabel(labelIndex: number) {
-    // Scroll into view first, then get the specific label
-    cy.get("[data-cy='material-header-content']").scrollIntoView();
     return cy
       .get(".material-header__availability-label .availability-label")
       .eq(labelIndex);
+  }
+
+  openFindOnShelf() {
+    this.elements
+      .findOnShelfButton()
+      .scrollIntoView()
+      .should("be.visible")
+      .click();
+
+    // Wait for the modal to appear
+    cy.get(".modal-find-on-shelf").should("be.visible");
+    return this;
   }
 }
