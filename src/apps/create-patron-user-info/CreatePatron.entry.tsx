5603040bc4fcfcca8d2a6ab99dea8b2ab5368912
thomasgross <thomasgrossrasmussen@gmail.com>
2025-10-29 12:08:06 +0100

    feat: add branch address search configuration and update FindLibraryDialog with new text translations

--- src/apps/create-patron-user-info/CreatePatron.entry.tsx
+++ src/apps/create-patron-user-info/CreatePatron.entry.tsx
@@ -14,6 +14,7 @@ interface CreatePatronConfigProps {
   blacklistedPickupBranchesConfig: string;
   branchesConfig: string;
   textNotificationsEnabledConfig: string;
+  branchAddressSearchEnabledConfig: string;
 }
 interface CreatePatronUrlProps {
   authUrl: string;
@@ -52,6 +53,12 @@ interface CreatePatronTextProps {
   postRegisterRedirectInfoTopText: string;
   postRegisterRedirectInfoBottomText: string;
   postRegisterRedirectButtonText: string;
+
+  librarySelectEmptyStateText: string;
+  findLibraryDialogTitleText: string;
+  findLibraryDialogDawaInputLabelText: string;
+  findLibraryDialogDawaInputPlaceholderText: string;
+  findLibraryDialogGeoLocationButtonText: string;
 }
 
 export interface CreatePatronProps
--- src/apps/create-patron-user-info/CreatePatron.stories.tsx
+++ src/apps/create-patron-user-info/CreatePatron.stories.tsx
@@ -221,6 +221,43 @@ export const Primary: Story = {
         defaultValue: { summary: "Log in again" }
       },
       control: { type: "text" }
+    },
+    librarySelectEmptyStateText: {
+      description: "Library select empty state text",
+      table: {
+        type: { summary: "text" },
+        defaultValue: { summary: "Choose library" }
+      },
+      control: { type: "text" }
+    },
+    findLibraryDialogTitleText: {
+      description: "Find library dialog title text",
+      table: {
+        type: { summary: "text" },
+        defaultValue: { summary: "Find nearest library" }
+      },
+      control: { type: "text" }
+    },
+    findLibraryDialogDawaInputLabelText: {
+      table: {
+        type: { summary: "text" },
+        defaultValue: { summary: "Enter your address" }
+      },
+      control: { type: "text" }
+    },
+    findLibraryDialogDawaInputPlaceholderText: {
+      table: {
+        type: { summary: "text" },
+        defaultValue: { summary: "Fx Torvegade 1, 1401 København K" }
+      },
+      control: { type: "text" }
+    },
+    findLibraryDialogGeoLocationButtonText: {
+      table: {
+        type: { summary: "text" },
+        defaultValue: { summary: "Find nearest library using your location" }
+      },
+      control: { type: "text" }
     }
   },
   args: {
@@ -233,7 +270,8 @@ export const Primary: Story = {
     blacklistedPickupBranchesConfig:
       "FBS-751032,FBS-751031,FBS-751009,FBS-751027,FBS-751024",
     minAgeConfig: "18",
-    branchesConfig: `[{"branchId": "DK-710111","title": "Nørrebro","location": {"address": "Sædding Strandvej 2","city": "6710 Esbjerg V","value": "Sædding Strandvej 2 6710 Esbjerg V","lat": "55.4871","lng": "8.41239"}},{"branchId": "DK-710110","title": "Biblioteket Rentemestervej","location": {"address": "Suomisvej 7 st. 2","city": "1927 Frederiksberg C","value": "Suomisvej 7 st. 2 1927 Frederiksberg C","lat": "55.6795","lng": "12.5563"}},{"branchId": "DK-710113","title": "Sundby","location": {"address": "Baaringvadvænget 2","city": "5580 Nørre Aaby","value": "Baaringvadvænget 2 5580 Nørre Aaby","lat": "55.4534","lng": "9.89213"}},{"branchId": "DK-710112","title": "Solvang"},{"branchId": "DK-710111","title": "Nørrebro","location": {"address": "Sædding Strandvej 2","city": "6710 Esbjerg V","value": "Sædding Strandvej 2 6710 Esbjerg V","lat": "55.4871","lng": "8.41239"}},{"branchId": "DK-710110","title": "Biblioteket Rentemestervej","location": {"address": "Suomisvej 7 st. 2","city": "1927 Frederiksberg C","value": "Suomisvej 7 st. 2 1927 Frederiksberg C","lat": "55.6795","lng": "12.5563"}},{"branchId": "DK-710113","title": "Sundby","location": {"address": "Baaringvadvænget 2","city": "5580 Nørre Aaby","value": "Baaringvadvænget 2 5580 Nørre Aaby","lat": "55.4534","lng": "9.89213"}},{"branchId": "DK-710112","title": "Solvang"}]`,
+    branchesConfig: `[{"branchId":"DK-710111","title":"Nørrebro","location":{"address":"Sædding Strandvej 2","city":"6710 Esbjerg V","value":"Sædding Strandvej 2 6710 Esbjerg V","lat":"55.4871","lng":"8.41239"}},{"branchId":"DK-710110","title":"Biblioteket Rentemestervej","location":{"address":"Suomisvej 7 st. 2","city":"1927 Frederiksberg C","value":"Suomisvej 7 st. 2 1927 Frederiksberg C","lat":"55.6795","lng":"12.5563"}},{"branchId":"DK-710113","title":"Sundby","location":{"address":"Baaringvadvænget 2","city":"5580 Nørre Aaby","value":"Baaringvadvænget 2 5580 Nørre Aaby","lat":"55.4534","lng":"9.89213"}},{"branchId":"DK-710112","title":"Solvang","location":{"address":"Banestien 2 ","city":"6310 Broager","value":"Banestien 2 6310 Broager ","lat":"54.8862","lng":"9.67579"}},{"branchId":"DK-710115","title":"Sydhavn"},{"branchId":"DK-710114","title":"Bibliotekshuset"},{"branchId":"DK-710117","title":"Valby"},{"branchId":"DK-710116","title":"Tingbjerg"},{"branchId":"DK-710119","title":"Vesterbro"},{"branchId":"DK-710118","title":"Vanløse"},{"branchId":"DK-710100","title":"Hovedbiblioteket"},{"branchId":"FBS-101011","title":"Fjernlager 4"},{"branchId":"FBS-101012","title":"Fjernlager 5"},{"branchId":"FBS-101017","title":".MÅ ALDRIG BRUGES"},{"branchId":"DK-710120","title":"Vigerslev"},{"branchId":"FBS-101010","title":"Fjernlager 3"},{"branchId":"DK-710122","title":"Ørestad"},{"branchId":"DK-710121","title":"Østerbro"},{"branchId":"DK-710104","title":"Blågården"},{"branchId":"DK-710106","title":"Christianshavn"},{"branchId":"DK-710105","title":"Brønshøj"},{"branchId":"DK-710108","title":"Islands Brygge"},{"branchId":"DK-710107","title":"Husum"},{"branchId":"DK-710109","title":"Øbro Jagtvej"},{"branchId":"FBS-101008","title":"Fjernlager 1"},{"branchId":"FBS-101009","title":"Fjernlager 2"},{"branchId":"FBS-101004","title":"HB Københavns fængsler"},{"branchId":"FBS-101006","title":"Bestilt - ikke modtaget"},{"branchId":"FBS-101007","title":"HB Rigshospitalet"},{"branchId":"FBS-101002","title":"FjernUDlån (afh.sted)"},{"branchId":"FBS-101003","title":"Fjernlager (cso)"}]`,
+    branchAddressSearchEnabledConfig: "1",
     dashboardUrl: "/user/me/dashboard",
     agencyConfig: "999999",
     textNotificationsEnabledConfig: "1",
@@ -270,7 +308,15 @@ export const Primary: Story = {
       "You are now registered as a user and need to log in again to be able to use the application.",
     postRegisterRedirectInfoBottomText:
       "You will be sent to the Adgangsplatformen to log in again in @seconds seconds.",
-    postRegisterRedirectButtonText: "Log in again"
+    postRegisterRedirectButtonText: "Log in again",
+
+    librarySelectEmptyStateText: "Choose library",
+    findLibraryDialogTitleText: "Find nearest library",
+    findLibraryDialogDawaInputLabelText: "Enter your address",
+    findLibraryDialogDawaInputPlaceholderText:
+      "Ex. Torvegade 1, 1401 København K",
+    findLibraryDialogGeoLocationButtonText:
+      "Find nearest library using your location"
   },
   decorators: [
     (Story) => {
