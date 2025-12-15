46d8e3da9d2d31a620a8cfcdafa4cb312c89a837
Benjamin Rasmussen <rasben@users.noreply.github.com>
2025-12-15 13:07:08 +0100

    Only return branches for search if blacklist has effect  DDFSAL-449
    If the supplied blacklist has no affect for the search branches, we only
    want to send an empty array to FBI, rather than a full list of branches.
    It results in the same, but by doing it like this, we both use less
    bandwith, but also minimises issues with BNF/Delingstjenesten.

--- src/core/utils/branches.tsx
+++ src/core/utils/branches.tsx
@@ -34,23 +34,46 @@ export const cleanBranchesId = (branches: TBranch[]): string[] => {
   );
 };
 
-export const useGetBranches = (key: string): TBranch[] => {
+/**
+ * Getting branches that are not blacklisted.
+ *
+ * @param blacklistKey
+ *  The config key that contains the ID of the blacklist.
+ *  Different lists (search, pickup etc.) might have different types.
+ * @param preferEmptyResult
+ *  If set, return an empty array if blacklist filtering has no effect.
+ *  This is useful for filters, where an empty array already means all filters.
+ */
+export const useGetBranches = (
+  blacklistKey: string,
+  preferEmptyResult: boolean = false
+): TBranch[] => {
   const config = useConfig();
   const branches = config<TBranch[]>("branchesConfig", {
     transformer: "jsonParse"
   });
-  const blacklistBranches = config(key, {
+  const blacklistBranches = config(blacklistKey, {
     transformer: "stringToArray"
   });
+
+  if (preferEmptyResult && !blacklistBranches.length) {
+    return [];
+  }
+
   const whitelistBranches = excludeBlacklistedBranches(
     branches,
     blacklistBranches
   );
+
+  if (preferEmptyResult && whitelistBranches.length == branches.length) {
+    return [];
+  }
+
   return whitelistBranches;
 };
 
 const useGetSearchBranches = () => {
-  const branches = useGetBranches("blacklistedSearchBranchesConfig");
+  const branches = useGetBranches("blacklistedSearchBranchesConfig", true);
   const cleanBranches = cleanBranchesId(branches);
   return cleanBranches;
 };
