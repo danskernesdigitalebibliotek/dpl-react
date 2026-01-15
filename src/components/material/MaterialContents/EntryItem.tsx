70b1155601e9b2bc1dcdc9cd297ddf3458a4656f
Kasper Birch <kasperbirch1@gmail.com>
2026-01-15 11:06:12 +0100

    Remove accordion logic from content entry and sublevel

--- src/components/material/MaterialContents/EntryItem.tsx
+++ src/components/material/MaterialContents/EntryItem.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import ExpandIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/ExpandMore.svg";
+import React from "react";
 import { ContentEntry } from "./types";
 import { EntryDetails } from "./EntryDetails";
 import { SubLevel1 } from "./SubLevel1";
@@ -9,64 +8,33 @@ interface EntryItemProps {
 }
 
 export const EntryItem: React.FC<EntryItemProps> = ({ entry }) => {
-  const [isOpen, setIsOpen] = useState(false);
   const hasSublevels = entry.sublevel && entry.sublevel.length > 0;
-
-  if (!hasSublevels) {
-    return (
-      <li className="material-content__item material-content__item--level-1">
-        <div className="material-content__item-content">
-          <div className="material-content__main-line">
-            <span className="material-content__title">
-              {entry.title.display}
-            </span>
-            <EntryDetails
-              creators={entry.creators}
-              playingTime={entry.playingTime}
-            />
-          </div>
-          {entry.contributors && entry.contributors.length > 0 && (
-            <div className="material-content__secondary-line">
-              <span className="material-content__contributors">
-                ({entry.contributors.join(", ")})
-              </span>
-            </div>
-          )}
-        </div>
-      </li>
-    );
-  }
 
   return (
     <li className="material-content__item material-content__item--level-1">
-      <details open={isOpen} onToggle={(e) => setIsOpen(e.currentTarget.open)}>
-        <summary className="material-content__summary">
-          <div className="material-content__summary-content">
-            <div className="material-content__main-line">
-              <span className="material-content__title">
-                {entry.title.display}
-              </span>
-              <EntryDetails
-                creators={entry.creators}
-                playingTime={entry.playingTime}
-              />
-            </div>
-            {entry.contributors && entry.contributors.length > 0 && (
-              <div className="material-content__secondary-line">
-                <span className="material-content__contributors">
-                  ({entry.contributors.join(", ")})
-                </span>
-              </div>
-            )}
+      <div className="material-content__item-content">
+        <div className="material-content__main-line">
+          <span className="material-content__title">{entry.title.display}</span>
+          <EntryDetails
+            creators={entry.creators}
+            playingTime={entry.playingTime}
+          />
+        </div>
+        {entry.contributors && entry.contributors.length > 0 && (
+          <div className="material-content__secondary-line">
+            <span className="material-content__contributors">
+              ({entry.contributors.join(", ")})
+            </span>
           </div>
-          <img src={ExpandIcon} alt="" className="material-content__icon" />
-        </summary>
-        <ol className="material-content__list material-content__list--level-2">
-          {entry.sublevel?.map((sublevel1, idx) => (
-            <SubLevel1 key={idx} item={sublevel1} />
-          ))}
-        </ol>
-      </details>
+        )}
+        {hasSublevels && (
+          <ol className="material-content__list material-content__list--level-2">
+            {entry.sublevel?.map((sublevel1, idx) => (
+              <SubLevel1 key={idx} item={sublevel1} />
+            ))}
+          </ol>
+        )}
+      </div>
     </li>
   );
 };
--- src/components/material/MaterialContents/SubLevel1.tsx
+++ src/components/material/MaterialContents/SubLevel1.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import ExpandIcon from "@danskernesdigitalebibliotek/dpl-design-system/build/icons/collection/ExpandMore.svg";
+import React from "react";
 import { ContentSublevel } from "./types";
 import { EntryDetails } from "./EntryDetails";
 import { SubLevel2 } from "./SubLevel2";
@@ -9,46 +8,30 @@ interface SubLevel1Props {
 }
 
 export const SubLevel1: React.FC<SubLevel1Props> = ({ item }) => {
-  const [isOpen, setIsOpen] = useState(false);
   const hasSublevels = item.sublevel && item.sublevel.length > 0;
-
-  const itemContent = (
-    <>
-      <div className="material-content__main-line">
-        <span className="material-content__title">{item.title.display}</span>
-        <EntryDetails playingTime={item.playingTime} />
-      </div>
-      {item.contributors && item.contributors.length > 0 && (
-        <div className="material-content__secondary-line">
-          <span className="material-content__contributors">
-            ({item.contributors.join(", ")})
-          </span>
-        </div>
-      )}
-    </>
-  );
-
-  if (!hasSublevels) {
-    return (
-      <li className="material-content__item material-content__item--level-2">
-        <div className="material-content__item-content">{itemContent}</div>
-      </li>
-    );
-  }
 
   return (
     <li className="material-content__item material-content__item--level-2">
-      <details open={isOpen} onToggle={(e) => setIsOpen(e.currentTarget.open)}>
-        <summary className="material-content__summary">
-          <div className="material-content__summary-content">{itemContent}</div>
-          <img src={ExpandIcon} alt="" className="material-content__icon" />
-        </summary>
-        <ol className="material-content__list material-content__list--level-3">
-          {item.sublevel?.map((sublevel2, idx) => (
-            <SubLevel2 key={idx} item={sublevel2} />
-          ))}
-        </ol>
-      </details>
+      <div className="material-content__item-content">
+        <div className="material-content__main-line">
+          <span className="material-content__title">{item.title.display}</span>
+          <EntryDetails playingTime={item.playingTime} />
+        </div>
+        {item.contributors && item.contributors.length > 0 && (
+          <div className="material-content__secondary-line">
+            <span className="material-content__contributors">
+              ({item.contributors.join(", ")})
+            </span>
+          </div>
+        )}
+        {hasSublevels && (
+          <ol className="material-content__list material-content__list--level-3">
+            {item.sublevel?.map((sublevel2, idx) => (
+              <SubLevel2 key={idx} item={sublevel2} />
+            ))}
+          </ol>
+        )}
+      </div>
     </li>
   );
 };
