373262a510c6736649358ad54fc8af78742473dd
Kasper Birch <kasperbirch1@gmail.com>
2026-02-05 14:40:22 +0100

    Add integration test for shelfmark display
    Add test to verify shelfmark and postfix are displayed with middle dot
    separator in Find on Shelf modal.

--- cypress/factories/manifestation/manifestation.factory.ts
+++ cypress/factories/manifestation/manifestation.factory.ts
@@ -103,7 +103,10 @@ export const manifestationFactory = Factory.define<ManifestationForWorkMedium>(
         loanIsPossible: true
       }
     ],
-    shelfmark: null,
+    shelfmark: {
+      shelfmark: "shelfmark",
+      postfix: "postfix"
+    },
     workYear: null,
     catalogueCodes: {
       nationalBibliography: ["DBF201835"],
