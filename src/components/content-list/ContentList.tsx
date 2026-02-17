9f44edcf9dff68cbeb453d684286e7c56986568d
Kasper Birch <kasperbirch1@gmail.com>
2026-02-17 13:20:36 +0100

    Extend ContentListPage and ContentList for reuse
    Add optional props to support variations across the codebase

--- src/components/content-list/ContentList.tsx
+++ src/components/content-list/ContentList.tsx
@@ -1,11 +1,16 @@
 import React, { FC, ReactNode } from "react";
+import clsx from "clsx";
 
 type ContentListProps = {
   children: ReactNode;
+  className?: string;
+  dataCy?: string;
 };
 
-const ContentList: FC<ContentListProps> = ({ children }) => (
-  <ul className="content-list">{children}</ul>
+const ContentList: FC<ContentListProps> = ({ children, className, dataCy }) => (
+  <ul className={clsx("content-list", className)} data-cy={dataCy}>
+    {children}
+  </ul>
 );
 
 export default ContentList;
--- src/components/content-list/ContentListPage.tsx
+++ src/components/content-list/ContentListPage.tsx
@@ -1,13 +1,36 @@
 import React, { FC, ReactNode } from "react";
+import clsx from "clsx";
 
 type ContentListPageProps = {
-  title: string;
-  children: ReactNode;
+  title: ReactNode;
+  children?: ReactNode;
+  headingLevel?: "h1" | "h2";
+  headingClassName?: string;
+  headingId?: string;
+  headingDataCy?: string;
+  headingAriaLive?: "polite" | "assertive" | "off";
+  dataCy?: string;
 };
 
-const ContentListPage: FC<ContentListPageProps> = ({ title, children }) => (
-  <section className="content-list-page">
-    <h1 className="content-list-page__heading">{title}</h1>
+const ContentListPage: FC<ContentListPageProps> = ({
+  title,
+  children,
+  headingLevel: Heading = "h1",
+  headingClassName,
+  headingId,
+  headingDataCy,
+  headingAriaLive,
+  dataCy
+}) => (
+  <section className="content-list-page" data-cy={dataCy}>
+    <Heading
+      className={clsx("content-list-page__heading", headingClassName)}
+      id={headingId}
+      data-cy={headingDataCy}
+      aria-live={headingAriaLive}
+    >
+      {title}
+    </Heading>
     {children}
   </section>
 );
